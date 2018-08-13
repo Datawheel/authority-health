c7b90dcff8fa68e8e20141b9d2a8450c19ed28b5
Rashmi <baheti.rash123@gmail.com>
2018-08-13 14:49:52 -0400

    removes unwanted comments

--- cache/populations.js
+++ cache/populations.js
@@ -5,7 +5,6 @@ module.exports = async function() {
 
   const client = new Client("https://canon-api.datausa.io/");
 
-  // get geoid:total population
   const levels = ["Zip", "Place", "Tract"];
   const popQueries = levels
     .map(level => client.cube("acs_yg_total_population_5")
@@ -29,7 +28,6 @@ module.exports = async function() {
   const rawPops = await Promise.all(popQueries);
   const pops = rawPops.reduce((obj, d) => (obj = Object.assign(obj, d), obj), {});
 
-  // get geoid:place
   const geoserviceData = await axios.get("http://ah-aspen.datawheel.us/geoservice-api/relations/intersects/05000US26163?targetLevels=zip,place,tract")
     .then(d => d.data);
 

bfa06441ea3474bf0584e7a74a4bf55a9ddd92fa
Rashmi <baheti.rash123@gmail.com>
2018-08-30 14:36:11 -0400

    adds logiclayer for authority health cube

--- app/routes.jsx
+++ app/routes.jsx
@@ -4,14 +4,12 @@ import {Route, IndexRoute, browserHistory} from "react-router";
 import App from "./App";
 import Home from "./pages/Home";
 import Profile from "pages/Profile";
-import Visualize from "components/Visualize";
 
 export default function RouteCreate() {
   return (
     <Route path="/" component={App} history={browserHistory}>
       <IndexRoute component={Home} />
       <Route path="profile(/:id)" component={Profile} />
-      <Route path="visualize" component={Visualize} />
     </Route>
   );
 }

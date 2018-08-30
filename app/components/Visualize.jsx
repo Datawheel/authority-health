55166064af469f9bc8ad959f5294e11f8e0035e2
Rashmi <baheti.rash123@gmail.com>
2018-08-30 13:58:11 -0400

    installs vizbuilder

---
+++ app/components/Visualize.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Vizbuilder from "@datawheel/canon-vizbuilder";
+
+export default class Visualize extends React.Component {
+  render() {
+    return <Vizbuilder src="http://ah-aspen.datawheel.us/" />;
+  }
+}

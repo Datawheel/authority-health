7bbd17d91ee81788c5d231a328916f40ac0c318f
Rashmi <baheti.rash123@gmail.com>
2018-09-27 14:53:58 -0400

    adds restaurant types to Treemap in addition to store types

--- app/pages/Profile/components/FoodAccess.jsx
+++ app/pages/Profile/components/FoodAccess.jsx
@@ -21,23 +21,31 @@ class FoodAccess extends SectionColumns {
           {/* <BarChart  /> */}
         </article>
         <Treemap config={{
-          data: "/api/data?measures=Farmers%27%20markets,Grocery%20stores,Supercenters%20and%20club%20stores,Convenience%20stores&County=05000US26163&Year=all",
-          groupBy: "StoreType",
-          height: 300,
-          width: 400,
-          label: d => d.StoreType,
+          data: "/api/data?measures=Farmers%27%20markets,Grocery%20stores,Supercenters%20and%20club%20stores,Convenience%20stores,Fast-food%20restaurants,Full-service%20restaurants&County=05000US26163&Year=all",
+          groupBy: ["Group", "FoodServiceType"],
+          height: 400,
+          width: 600,
           legend: true,
-          sum: d => d[d.StoreType]
+          sum: d => d[d.FoodServiceType]
         }}
         dataFormat={resp => {
           const storeTypes = [];
-          resp.source[0].measures.forEach(storeType => storeTypes.push(storeType));
-
+          const restaurantTypes = [];
+          resp.source[0].measures.forEach(foodServiceType => foodServiceType.toLowerCase().includes("restaurant") ? restaurantTypes.push(foodServiceType) : storeTypes.push(foodServiceType));
           const data = [];
           storeTypes.map(storeType => {
             const result = resp.data.reduce((acc, currentValue) => {
               if (acc === null && currentValue[storeType] !== null) {
-                return Object.assign({}, currentValue, {StoreType: storeType});
+                return Object.assign({}, currentValue, {FoodServiceType: storeType, Group: "Stores"});
+              }
+              return acc;
+            }, null);
+            data.push(result);
+          });
+          restaurantTypes.map(restaurantType => {
+            const result = resp.data.reduce((acc, currentValue) => {
+              if (acc === null && currentValue[restaurantType] !== null) {
+                return Object.assign({}, currentValue, {FoodServiceType: restaurantType, Group: "Restaurants"});
               }
               return acc;
             }, null);

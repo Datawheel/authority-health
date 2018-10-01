6141d5a68803a65a84105592cb7ba1489844643d
Rashmi <baheti.rash123@gmail.com>
2018-10-01 11:47:57 -0400

    adds stat for largest race group

--- app/pages/Profile/components/DemographicFoodAccess.jsx
+++ app/pages/Profile/components/DemographicFoodAccess.jsx
@@ -1,20 +1,36 @@
 import React from "react";
-// import {connect} from "react-redux";
+import {connect} from "react-redux";
 import {BarChart} from "d3plus-react";
-// import {formatAbbreviate} from "d3plus-format";
+import {formatAbbreviate} from "d3plus-format";
+import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
-import {SectionColumns, SectionTitle} from "@datawheel/canon-core";
-
-// import Stat from "./Stat";
+import Stat from "./Stat";
 
 class DemographicFoodAccess extends SectionColumns {
 
   render() {
+
+    const {foodAccessByRace} = this.props;
+    const races = [];
+    foodAccessByRace.source[0].measures.forEach(race => races.push(race));
+    let largestRaceGroupPercentage = foodAccessByRace.data[0][races[0]];
+    let largestRaceGroup = races[0];
+    races.forEach(race => {
+      if (foodAccessByRace.data[0][race] > largestRaceGroupPercentage) {
+        largestRaceGroupPercentage = foodAccessByRace.data[0][race];
+        largestRaceGroup = race;
+      }
+    });
+
     return (
       <SectionColumns>
         <SectionTitle>Low Access to Food store</SectionTitle>
         <article>
-          Some text about low access to food
+          <Stat
+            title={`${largestRaceGroup} in the Wayne County:`}
+            value={`${formatAbbreviate(largestRaceGroupPercentage)}%`}
+          />
+          <br/><br/>
           <BarChart config={{
             data: "/api/data?measures=Children%2C%20low%20access%20to%20store%20(%25),Seniors%2C%20low%20access%20to%20store%20(%25)&County=05000US26163&Year=all",
             discrete: "y",
@@ -27,7 +43,6 @@ class DemographicFoodAccess extends SectionColumns {
             tooltipConfig: {tbody: [["Value", d => d[d.AgeType]]]}
           }}
           dataFormat={resp => {
-            console.log("resp: ", resp);
             const ages = [];
             resp.source[0].measures.forEach(ageType => ages.push(ageType));
             const data = [];
@@ -78,4 +93,12 @@ class DemographicFoodAccess extends SectionColumns {
   }
 }
 
-export default DemographicFoodAccess;
+DemographicFoodAccess.need = [
+  fetchData("foodAccessByRace", "/api/data?measures=White%2C%20low%20access%20to%20store%20(%25),Black%2C%20low%20access%20to%20store%20(%25),Hispanic%20ethnicity%2C%20low%20access%20to%20store%20(%25),Asian%2C%20low%20access%20to%20store%20(%25),American%20Indian%20or%20Alaska%20Native%2C%20low%20access%20to%20store%20(%25),Hawaiian%20or%20Pacific%20Islander%2C%20low%20access%20to%20store%20(%25),Multiracial%2C%20low%20access%20to%20store%20(%25)&County=05000US26163&Year=all")
+];
+
+const mapStateToProps = state => ({
+  foodAccessByRace: state.data.foodAccessByRace
+});
+  
+export default connect(mapStateToProps)(DemographicFoodAccess);

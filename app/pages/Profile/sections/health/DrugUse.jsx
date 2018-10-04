7c72e9905e1b4933f0d4f79803090b7fb0110444
Rashmi <baheti.rash123@gmail.com>
2018-10-04 14:00:47 -0400

    adding dropdown UI (WIP)

--- app/pages/Profile/sections/health/DrugUse.jsx
+++ app/pages/Profile/sections/health/DrugUse.jsx
@@ -1,23 +1,53 @@
 import React from "react";
 import {connect} from "react-redux";
-// import {Treemap} from "d3plus-react";
+// import {Geomap} from "d3plus-react";
 // import {formatAbbreviate} from "d3plus-format";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
-// import Stat from "../../components/Stat";
+import Stat from "../../components/Stat";
 
 class DrugUse extends SectionColumns {
 
+  constructor(props) {
+    super(props);
+    this.state = {dropdownValue: "Binge Drinking Data Value"};
+  }
+
+  // Handler for dropdown onChange function.
+  handleChange = event => this.setState({dropdownValue: event.target.value});
 
   render() {
 
-    console.log("this.props.smokingDrinkingData: ", this.props.smokingDrinkingData);
+    const {smokingDrinkingData} = this.props;
+
+    // Create individual data object for each type of age and race.
+    // Add AgeRaceType key in each object.
+    const data = smokingDrinkingData.source[0].measures.map(d => {
+      const result = smokingDrinkingData.data.reduce((acc, currentValue) => {
+        if (acc === null && currentValue[d] !== null) {
+          return Object.assign({}, currentValue, {DrupType: d});
+        }
+        return acc;
+      }, null);
+      return result;
+    });
+
+    console.log("smokingDrinkingData: ", smokingDrinkingData);
+    console.log("data: ", data);
+
+    // const dropdownValues = item => <option value={item}>{item}</option>;
 
     return (
       <SectionColumns>
         <SectionTitle>Drug Use</SectionTitle>
         <article>
+          {/* Create a dropdown using raceAndAgeTypes array. */}
+          {/* <select onChange={this.handleChange}>{raceAndAgeTypes.map(dropdownValues)}</select> */}
+          <Stat
+            // title={`Access in ${currentRaceAndAgeData.County} County`}
+            // value={`${formatAbbreviate(currentRaceAndAgeData[this.state.dropdownValue])}%`}
+          />
 
         </article>
 

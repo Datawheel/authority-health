df17de4bdfaa8ab188369b81cd7a2fe19f1c28d4
Rashmi <baheti.rash123@gmail.com>
2018-10-09 15:16:44 -0400

    creates Dentists component

---
+++ app/pages/Profile/sections/health/Dentists.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
+
+class Dentists extends SectionColumns {
+
+  render() {
+    return (
+      <SectionTitle>
+        <article>
+            
+        </article>
+
+      </SectionTitle>
+    );
+
+  }
+}
+
+export default Dentists;

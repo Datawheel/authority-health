e3b373a45ce09b17f94033c94bb6aa2bbf67e1cc
Rashmi <baheti.rash123@gmail.com>
2018-10-11 11:21:51 -0400

    adds all age ranges to the age BarChart

--- app/pages/Profile/sections/health/Dentists.jsx
+++ app/pages/Profile/sections/health/Dentists.jsx
@@ -40,17 +40,18 @@ class Dentists extends SectionColumns {
           />
           <p></p>
           <BarChart config={{
-            data: "http://localhost:3300/api/data?measures=%23%20Active%20Dentists%20less%20than%2035&Year=all",
+            data: "/api/data?measures=%23%20Active%20Dentists%20less%20than%2035,%23%20Active%20Dentists%2035,%23%20Active%20Dentists%2045,%23%20Active%20Dentists%2055,%23%20Active%20Dentists%2065%20or%20more,%23%20Active%20Dentists%20Age%20Unknown&Year=all",
             discrete: "x",
             height: 400,
             groupBy: "Age",
             x: d => d.Age,
             y: d => d[d.Age],
             time: "ID Year",
+            legend: false,
             xSort: (a, b) => a["ID Age"] - b["ID Age"],
             xConfig: {
               labelRotation: false,
-              tickFormat: d => formatAge(d)
+              tickFormat: d => formatAge(d) === "None" ? "Unkn" : formatAge(d)
             },
             shapeConfig: {label: false},
             tooltipConfig: {tbody: [["Value", d => d[d.Age]]]}
@@ -103,7 +104,7 @@ Dentists.defaultProps = {
 };
 
 Dentists.need = [
-  fetchData("dentistsTypes", "http://localhost:3300/api/data?measures=Dentists%2C%20Male%20w%2FNPI,Dentists%2CFemale%20w%2FNPI,Dentists%2C%20Total%2C%20Priv%20Pract%2C%20FT,Dentists%2C%20Total%2C%20Priv%20Pract%2C%20PT,Dent%2C%20Total%20Male%2C%20Priv%20Pract,Dent%2C%20Total%20Female%2C%20Priv%20Pract,Dentists%2C%20No%20Longer%20in%20Practice,Dentists%2C%20State%20or%20Local%20Govt,Dentists%2C%20Grad%20Student%2FResident&Year=all")
+  fetchData("dentistsTypes", "/api/data?measures=Dentists%2C%20Male%20w%2FNPI,Dentists%2CFemale%20w%2FNPI,Dentists%2C%20Total%2C%20Priv%20Pract%2C%20FT,Dentists%2C%20Total%2C%20Priv%20Pract%2C%20PT,Dent%2C%20Total%20Male%2C%20Priv%20Pract,Dent%2C%20Total%20Female%2C%20Priv%20Pract,Dentists%2C%20No%20Longer%20in%20Practice,Dentists%2C%20State%20or%20Local%20Govt,Dentists%2C%20Grad%20Student%2FResident&Year=all")
 ];
 
 const mapStateToProps = state => ({

b4a61e9f73506e34a487d0517dce17124964eef4
Rashmi <baheti.rash123@gmail.com>
2018-10-12 16:57:55 -0400

    addresses all the comments in PR

--- app/pages/Profile/sections/health/DrugUse.jsx
+++ app/pages/Profile/sections/health/DrugUse.jsx
@@ -23,17 +23,33 @@ class DrugUse extends SectionColumns {
   handleChange = event => this.setState({dropdownValue: event.target.value});
 
   render() {
+    const {dropdownValue} = this.state;
 
     const {allTractSmokingDrinkingData} = this.props;
     const drugTypes = allTractSmokingDrinkingData.source[0].measures;
 
     const allTractSmokingData = allTractSmokingDrinkingData.data.slice(0);
-    allTractSmokingData.sort((a, b) => b[drugTypes[0]] - a[drugTypes[0]]);
+    allTractSmokingData.sort((a, b) => b[dropdownValue] - a[dropdownValue]);
     const topTractSmokingData = allTractSmokingData[0];
 
     const allTractDrinkingData = allTractSmokingDrinkingData.data.slice(0);
-    allTractDrinkingData.sort((a, b) => b[drugTypes[1]] - a[drugTypes[1]]);
+    allTractDrinkingData.sort((a, b) => b[dropdownValue] - a[dropdownValue]);
     const topTractDrinkingData = allTractDrinkingData[0];
+
+    let topTractNum = topTractSmokingData.Tract;
+    let year = topTractSmokingData["ID Year"];
+    let topTractRate = topTractSmokingData[dropdownValue];
+    
+    if (dropdownValue === drugTypes[0]) { // Assign all Smoking data here
+      topTractNum = topTractSmokingData.Tract;
+      year = topTractSmokingData["ID Year"];
+      topTractRate = topTractSmokingData[dropdownValue];
+    }
+    else { // Assign all Drinking data here
+      topTractNum = topTractDrinkingData.Tract;
+      year = topTractDrinkingData["ID Year"];
+      topTractRate = topTractDrinkingData[dropdownValue];
+    }
 
     return (
       <SectionColumns>
@@ -43,32 +59,21 @@ class DrugUse extends SectionColumns {
           <select onChange={this.handleChange}>
             {drugTypes.map(item => <option key={item} value={item}>{formatName(item)}</option>)}
           </select>
-          {this.state.dropdownValue === drugTypes[0]
-            ? <Stat
-              title={`${topTractSmokingData.Tract}`}
-              value={`${formatAbbreviate(topTractSmokingData[this.state.dropdownValue])}%`}
-            />
-            : <Stat
-              title={`${topTractDrinkingData.Tract}`}
-              value={`${formatAbbreviate(topTractDrinkingData[this.state.dropdownValue])}%`}
-            />
-          }
-
-          {this.state.dropdownValue === drugTypes[0]
-            ? <p>{topTractSmokingData.Tract} had highest smoking rate of {topTractSmokingData[drugTypes[0]]}% in the year {topTractSmokingData["ID Year"]}</p>
-            : <p>{topTractDrinkingData.Tract} had highest drinking rate of {topTractDrinkingData[drugTypes[1]]}% in the year {topTractDrinkingData["ID Year"]}</p>
-          }
-
+          <Stat
+            title={"Tract with highest prevalence"}
+            value={`${topTractNum} ${formatAbbreviate(topTractRate)}%`}
+          />
+          <p>{topTractNum} had the highest {formatName(dropdownValue.toLowerCase())} rate of {topTractRate}% in the year {year}</p>
         </article>
 
         {/* Create a Geomap based on the dropdown choice. */}
         <Geomap config={{
-          data: `/api/data?measures=${this.state.dropdownValue.replace(/\s/g, "%20")}&drilldowns=Tract&Year=latest`,
+          data: `/api/data?measures=${dropdownValue.replace(/\s/g, "%20")}&drilldowns=Tract&Year=latest`,
           groupBy: "ID Tract",
-          colorScale: this.state.dropdownValue,
+          colorScale: dropdownValue,
           label: d => d.Tract,
           height: 400,
-          tooltipConfig: {tbody: [["Value", d => formatAbbreviate(d[this.state.dropdownValue])]]},
+          tooltipConfig: {tbody: [["Value", d => formatAbbreviate(d[dropdownValue])]]},
           topojson: "/topojson/tract.json",
           topojsonFilter: d => d.id.startsWith("14000US26163")
         }}

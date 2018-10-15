ad65e839c4b5c8c232b2f203ab2f8851ea905c7c
Rashmi <baheti.rash123@gmail.com>
2018-10-15 12:12:30 -0400

    adds stats and Geomap - still WIP

--- app/pages/Profile/sections/health/HealthCenters.jsx
+++ app/pages/Profile/sections/health/HealthCenters.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import {connect} from "react-redux";
+import {connect} from "react-redux";
 import {nest} from "d3-collection";
-import {BarChart} from "d3plus-react";
+import {BarChart, Geomap} from "d3plus-react";
 import {formatAbbreviate} from "d3plus-format";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
-// import Stat from "../../components/Stat";
+import Stat from "../../components/Stat";
 
 const formatName = d => {
   const nameArr = d.split(" ");
@@ -18,11 +18,36 @@ const formatPercentage = d => `${formatAbbreviate(d * 100)}%`;
 class HealthCenters extends SectionColumns {
 
   render() {
+    const {healthCenterData} = this.props;
+    console.log("healthCenterData: ", healthCenterData);
+    const healthCenterLatestYearData = healthCenterData.data[0];
+    console.log("Penetration of Uninsured Population: ", healthCenterLatestYearData["Penetration of Uninsured Population"]);
 
     return (
       <SectionColumns>
         <SectionTitle>Health Centers</SectionTitle>
         <article>
+          {healthCenterLatestYearData["Penetration of Total Population"] !== null && healthCenterLatestYearData["Penetration of Total Population"] !== undefined
+            ? <Stat
+              title={`Total Population visited health center in ${healthCenterLatestYearData.Year}`}
+              value={`${formatPercentage(healthCenterLatestYearData["Penetration of Total Population"])}`}
+            />
+            : null
+          }
+          {healthCenterLatestYearData["Penetration of Low-Income"] !== null && healthCenterLatestYearData["Penetration of Low-Income"] !== undefined
+            ? <Stat 
+              title={`Low-Income Population visited health center in ${healthCenterLatestYearData.Year}`}
+              value={`${formatPercentage(healthCenterLatestYearData["Penetration of Low-Income"])}`}
+            />
+            : null
+          }
+          {healthCenterLatestYearData["Penetration of Uninsured Population"] !== null && healthCenterLatestYearData["Penetration of Uninsured Population"] !== undefined
+            ? <Stat
+              title={`Uninsured Population visited health center in ${healthCenterLatestYearData.Year}`}
+              value={`${formatPercentage(healthCenterLatestYearData["Penetration of Uninsured Population"])}`}
+            />
+            : null
+          }
           <BarChart config={{
             data: "/api/data?measures=%25%20Non-white,%25%20Hispanic,%25%20Black,%25%20Asian,%25%20American%20Indian%2FAlaska%20Native&Year=all",
             discrete: "y",
@@ -47,6 +72,11 @@ class HealthCenters extends SectionColumns {
                 resp.source[0].measures.map(d => {
                   const result = group.values.reduce((acc, currentValue) => {
                     if (acc === null && currentValue[d] !== null) {
+                      if (d === "% Non-white") {
+                        d = "% White";
+                        currentValue[d] = 1 - currentValue["% Non-white"];
+                        return Object.assign({}, currentValue, {RaceType: d});
+                      }
                       return Object.assign({}, currentValue, {RaceType: d});
                     }
                     return acc;
@@ -54,11 +84,32 @@ class HealthCenters extends SectionColumns {
                   data.push(result);
                 });
               });
+            console.log("data: ", data);
             return data;
           }}
           />
-
         </article>
+
+        <Geomap config={{
+          data: "http://localhost:3300/api/data?measures=Health%20Centers&drilldowns=Zip%20Code&Year=all",
+          groupBy: "ID Zip Code",
+          colorScale: "Health Centers",
+          label: d => d["Zip Code"],
+          height: 400,
+          // time: "Year",
+          tooltipConfig: {tbody: [["Value", d => formatAbbreviate(d["Health Centers"])]]},
+          topojson: "/topojson/zipcodes.json",
+          topojsonFilter: d => {
+            // console.log("d: ", d);
+            console.log("topojsonFilter:");
+            return d.properties.GEOID10;
+          }
+        }}
+        // dataFormat={resp => {
+        //   console.log("resp: ", resp);
+        //   return resp.data;
+        // }}
+        />
       </SectionColumns>
     );
   }
@@ -67,5 +118,14 @@ class HealthCenters extends SectionColumns {
 HealthCenters.defaultProps = {
   slug: "health-centers"
 };
-  
-export default HealthCenters;
+
+HealthCenters.need = [
+  fetchData("healthCenterData", "/api/data?measures=Penetration%20of%20Total%20Population,Penetration%20of%20Low-Income,Penetration%20of%20Uninsured%20Population&Zip%20Code=48101&Year=all")
+];
+
+const mapStateToProps = state => ({
+  healthCenterData: state.data.healthCenterData
+});
+
+export default connect(mapStateToProps)(HealthCenters);
+

18a82b816426608771f0e18e66f889e107e0bfa5
Rashmi <baheti.rash123@gmail.com>
2018-10-19 16:25:48 -0400

    adds mini barchart for smoking status: current, former & never

--- app/pages/Profile/sections/health/DrugUse.jsx
+++ app/pages/Profile/sections/health/DrugUse.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
-import {Geomap} from "d3plus-react";
+import {BarChart, Geomap} from "d3plus-react";
 import {formatAbbreviate} from "d3plus-format";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
@@ -11,6 +11,7 @@ const formatName = name => {
   const nameArr = name.split(" ");
   return `${nameArr[0]} ${nameArr[1]}`;
 };
+const formatPercentage = d => `${formatAbbreviate(d)}%`;
 
 class DrugUse extends SectionColumns {
 
@@ -64,6 +65,43 @@ class DrugUse extends SectionColumns {
             value={`${topTractNum} ${formatAbbreviate(topTractRate)}%`}
           />
           <p>{topTractNum} had the highest {formatName(dropdownValue.toLowerCase())} rate of {topTractRate}% in the year {year}</p>
+
+          {/* Draw a mini bar chart to show smoking status: former, current & never. */}
+          {dropdownValue === drugTypes[0]
+            ? <BarChart config={{
+              data: "/api/data?measures=Smoking%20Status%20Current%20Weighted%20Percent,Smoking%20Status%20Former%20Weighted%20Percent,Smoking%20Status%20Never%20Weighted%20Percent&Year=all",
+              discrete: "y",
+              height: 250,
+              groupBy: "SmokingType",
+              label: d => {
+                const wordsArr = d.SmokingType.split(" ");
+                return `${wordsArr[0]} ${wordsArr[1]}: ${wordsArr[2]}`;
+              },
+              legend: false,
+              y: "SmokingType",
+              x: d => d[d.SmokingType],
+              time: "ID Start Year",
+              xConfig: {
+                labelRotation: false,
+                tickFormat: d => formatPercentage(d)
+              },
+              yConfig: {ticks: []},
+              shapeConfig: {label: false},
+              tooltipConfig: {tbody: [["Value", d => formatPercentage(d[d.SmokingType])]]}
+            }}
+            dataFormat={resp => {
+              const data = resp.source[0].measures.map(smokingType => {
+                const result = resp.data.reduce((acc, currentValue) => {
+                  if (acc === null && currentValue[smokingType] !== null) {
+                    return Object.assign({}, currentValue, {SmokingType: smokingType});
+                  }
+                  return acc;
+                }, null);
+                return result;
+              });
+              return data;
+            }}
+            /> : null }
         </article>
 
         {/* Create a Geomap based on the dropdown choice. */}

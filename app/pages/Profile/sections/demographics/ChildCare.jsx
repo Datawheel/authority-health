056564fc04918fbfe7735f26608df6a8a84aea41
Rashmi <baheti.rash123@gmail.com>
2018-10-19 10:06:41 -0400

    adds Stat to the ChildCare component

--- app/pages/Profile/sections/demographics/ChildCare.jsx
+++ app/pages/Profile/sections/demographics/ChildCare.jsx
@@ -1,44 +1,68 @@
 import React from "react";
-// import {connect} from "react-redux";
+import {sum} from "d3-array";
+import {nest} from "d3-collection";
+import {connect} from "react-redux";
 import {BarChart} from "d3plus-react";
-// import {formatAbbreviate} from "d3plus-format";
+import {formatAbbreviate} from "d3plus-format";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
-// import Stat from "../../components/Stat";
+import Stat from "../../components/Stat";
 
 import rangeFormatter from "../../../../utils/rangeFormatter";
+
+const formatPercentage = d => `${formatAbbreviate(d)}%`;
 
 class ChildCare extends SectionColumns {
 
   render() {
+
+    const {responsibilityData} = this.props;
+    const recentYearData = {};
+    nest()
+      .key(d => d.Year)
+      .entries(responsibilityData.data)
+      .forEach(group => {
+        const total = sum(group.values, d => d.Population);
+        group.values.forEach(d => d.share = d.Population / total * 100);
+        group.key >= responsibilityData.data[0].Year ? Object.assign(recentYearData, group) : {};
+      });
+
+    const recentYearFilteredData = recentYearData.values.filter(d => d["ID Responsibility Length"] === 4);
+    const data = responsibilityData.data.filter(d => d["ID Responsibility Length"] !== 5 && d["ID Responsibility Length"] !== 6);
+
     return (
       <SectionColumns>
         <SectionTitle>Child Care</SectionTitle>
         <article>
+          <Stat
+            title={`Grandparents responsible for their 5+ years grandchildren in ${recentYearFilteredData[0].Year}, `}
+            value={`${formatAbbreviate(recentYearFilteredData[0].Population)}`}
+          />
+          <p>Short paragraph here</p>
         </article>
 
         <BarChart config={{
-          data: "/api/data?measures=Population&drilldowns=Responsibility%20Length&Year=all",
+          data,
           discrete: "x",
           height: 400,
-          //   stacked: true,
           groupBy: "Responsibility Length",
+          legend: false,
           x: d => d["Responsibility Length"],
           y: "Population",
           time: "ID Year",
           xSort: (a, b) => a["ID Responsibility Length"] - b["ID Responsibility Length"],
           xConfig: {
             labelRotation: false,
-            tickFormat: d => rangeFormatter(d)
+            tickFormat: d => {
+              if (rangeFormatter(d) === "< 6") return "< 6 months";
+              if (rangeFormatter(d) === "6 - 11") return "6 - 11 months";
+              return rangeFormatter(d);
+            }
           },
-          //   yConfig: {tickFormat: d => formatPopulation(d)},
-          shapeConfig: {label: false}
-        //   tooltipConfig: {tbody: [["Value", d => formatAbbreviate(d.Population)]]}
-        }}
-        dataFormat={resp => {
-          console.log("resp.data: ", resp.data);
-          return resp.data;
+          yConfig: {tickFormat: d => formatAbbreviate(d)},
+          shapeConfig: {label: false},
+          tooltipConfig: {tbody: [["Value", d => formatPercentage(d.share)]]}
         }}
         />
       </SectionColumns>
@@ -50,4 +74,13 @@ ChildCare.defaultProps = {
   slug: "child-care"
 };
 
-export default ChildCare;
+ChildCare.need = [
+  fetchData("responsibilityData", "/api/data?measures=Population&drilldowns=Responsibility%20Length&Year=all")
+];
+
+const mapStateToProps = state => ({
+  responsibilityData: state.data.responsibilityData
+});
+  
+export default connect(mapStateToProps)(ChildCare);
+

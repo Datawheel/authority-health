a43c3e67777d43eb6300fecbd9ee5fd96ef2206d
Rashmi <baheti.rash123@gmail.com>
2018-10-22 15:07:08 -0400

    dispaly stats and a short paragraph based on the dropdown choice

--- app/pages/Profile/sections/demographics/Immigrants.jsx
+++ app/pages/Profile/sections/demographics/Immigrants.jsx
@@ -49,6 +49,21 @@ class Immigrants extends SectionColumns {
 
     const nativityArr = ["Native", "Foreign Born"];
 
+    let topNativityYear = topNativePopulation.Year;
+    let topNativityPlace = topNativePopulation.Place;
+    let topNativityShare = topNativePopulation.share;
+
+    if (dropdownValue === nativityArr[0]) {
+      topNativityYear = topNativePopulation.Year;
+      topNativityPlace = topNativePopulation.Place;
+      topNativityShare = topNativePopulation.share;
+    }
+    else {
+      topNativityYear = topForeignBornPopulation.Year;
+      topNativityPlace = topForeignBornPopulation.Place;
+      topNativityShare = topForeignBornPopulation.share;
+    }
+
     return (
       <SectionColumns>
         <SectionTitle>Immigrants</SectionTitle>
@@ -58,17 +73,12 @@ class Immigrants extends SectionColumns {
             {nativityArr.map((item, i) => <option key={i} value={item}>{item}</option>)}
           </select>
 
-          {/* Show top stats for each Nativity. */}
+          {/* Show top stats and a short paragraph about it for each Nativity. */}
           <Stat
-            title={`Top Native Population in ${topNativePopulation.Year}`}
-            value={`${topNativePopulation.Place} ${formatPopulation(topNativePopulation.share)}`}
+            title={`Top ${dropdownValue} Population in ${topNativityYear}`}
+            value={`${topNativityPlace} ${formatPopulation(topNativityShare)}`}
           />
-          <Stat
-            title={`Top Foreign Born Population in ${topForeignBornPopulation.Year}`}
-            value={`${topForeignBornPopulation.Place} ${formatPopulation(topForeignBornPopulation.share)}`}
-          />
-          <p>In {topNativePopulation.Year}, the highest Native population was {formatPopulation(topNativePopulation.share)} in the {topNativePopulation.Place} city</p>
-          <p>In {topForeignBornPopulation.Year}, the highest Foreign Born population was {formatPopulation(topForeignBornPopulation.share)} in the {topNativePopulation.Place} city</p>
+          <p>In {topNativityYear}, the highest {dropdownValue} population was {formatPopulation(topNativityShare)} in the {topNativityPlace} city</p>
         </article>
 
         {/* Create a Geomap based on the dropdown choice. */}

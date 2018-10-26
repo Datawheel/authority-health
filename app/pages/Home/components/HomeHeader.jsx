9008d904699c945dbc7f615c65a4318d95ccdcec
James Ferrell <hello@jamesferrell.me>
2018-10-26 15:21:34 -0400

    styles home header intro text

--- app/pages/Home/components/HomeHeader.jsx
+++ app/pages/Home/components/HomeHeader.jsx
@@ -11,8 +11,14 @@ export default class Home extends Component {
         <div className="home-header-inner">
           {/* intro text & search */}
           <div className="home-header-intro">
-            <h1 className="home-header-headline">Get to know  Wayne County</h1>
-            <p className="home-header-intro-text">View our challenges, embrace our strengths, and engage with our opportunities. <Link to="/about">learn more</Link></p>
+            <h1 className="home-header-headline font-xxl">Get to know  Wayne County</h1>
+            <p className="home-header-intro-text font-lg">
+              View our challenges, embrace our strengths, and engage with our opportunities.
+              <Link className="home-header-intro-link font-sm u-uppercase" to="/about">
+                learn more<span className="pt-icon pt-icon-chevron-right" />
+                <span className="u-visually-hidden"> about data in Wayne County, Michigan</span>
+              </Link>
+            </p>
             <Search
               className="home-search"
               placeholder="ex. Woodhaven, Romulus, Wayne"

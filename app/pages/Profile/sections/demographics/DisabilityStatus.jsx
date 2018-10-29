9ab9195364be0a2e911b15891ac50f744a8ff571
Rashmi <baheti.rash123@gmail.com>
2018-10-29 12:46:41 -0400

    removes dropdown and displayes all types of coverage in single barchart

--- app/pages/Profile/sections/demographics/DisabilityStatus.jsx
+++ app/pages/Profile/sections/demographics/DisabilityStatus.jsx
@@ -13,152 +13,52 @@ import rangeFormatter from "../../../../utils/rangeFormatter";
 const formatPopulation = d => `${formatAbbreviate(d)}%`;
 
 class DisabilityStatus extends SectionColumns {
-  constructor(props) {
-    super(props);
-    this.state = {dropdownValue: "Disability Status"};
-  }
-
-  // Handler function for dropdown onChange.
-  handleChange = event => this.setState({dropdownValue: event.target.value});
 
   render() {
-    const {dropdownValue} = this.state;
-    const dropdownList = ["Disability Status", "No Health Insurance Coverage", "With Health Insurance Coverage", "With Private Health Insurance Coverage", "With Public Health Insurance Coverage"];
 
-    const {disabilityStatus, healthInsuranceStatus, healthInsuranceType} = this.props;
-
-    // Read and transform Disability Status data into desired format.
-    const recentYearDisabilityStatus = {};
-    nest()
-      .key(d => d.Year)
-      .entries(disabilityStatus)
-      .forEach(group => {
-        nest()
-          .key(d => d["ID Place"])
-          .entries(group.values)
-          .forEach(place => {
-            nest()
-              .key(d => d["ID Age"])
-              .entries(place.values)
-              .forEach(ageType => {
-                const total = sum(ageType.values, d => d.Population);
-                ageType.values.forEach(d => d.share = d.Population / total * 100);
-              });
-          });
-        group.key >= disabilityStatus[0].Year ? Object.assign(recentYearDisabilityStatus, group) : {};
-      });
-    // Set data for Disability Status.
-    let data = disabilityStatus.filter(d => d["ID Disability Status"] === 0);
-    // Find top Disability Status Data for the most recent year.
-    const filteredRecentDisabilityStatus = recentYearDisabilityStatus.values.filter(d => d["ID Disability Status"] === 0);
-    filteredRecentDisabilityStatus.sort((a, b) => b.share - a.share);
-    let topData = filteredRecentDisabilityStatus[0];
+    const {healthCoverageType} = this.props;
 
     // Read and transform Health Insurance Status data into desired format.
-    const recentYearHealthInsuranceStatus = {};
+    const recentYearHealthCoverageType = {};
     nest()
       .key(d => d.Year)
-      .entries(healthInsuranceStatus)
+      .entries(healthCoverageType)
       .forEach(group => {
         nest()
-          .key(d => d["ID Place"])
+          .key(d => d["ID Age"])
           .entries(group.values)
-          .forEach(place => {
-            nest()
-              .key(d => d["ID Age"])
-              .entries(place.values)
-              .forEach(ageType => {
-                const total = sum(ageType.values, d => d.Population);
-                ageType.values.forEach(d => d.share = d.Population / total * 100);
-              });
+          .forEach(ageType => {
+            const total = sum(ageType.values, d => d.Population);
+            ageType.values.forEach(d => d.share = d.Population / total * 100);
           });
-        group.key >= healthInsuranceStatus[0].Year ? Object.assign(recentYearHealthInsuranceStatus, group) : {};
+        group.key >= healthCoverageType[0].Year ? Object.assign(recentYearHealthCoverageType, group) : {};
       });
-    // Filter data for each Health Insurance Status.
-    const noInsuranceData = healthInsuranceStatus.filter(d => d["ID Health Insurance coverage:status"] === 1);
-    const withInsuranceData = healthInsuranceStatus.filter(d => d["ID Health Insurance coverage:status"] === 0);
 
-    // Read and transform Health Insurance Type data into desired format.
-    const recentYearHealthInsuranceType = {};
-    nest()
-      .key(d => d.Year)
-      .entries(healthInsuranceType)
-      .forEach(group => {
-        nest()
-          .key(d => d["ID Place"])
-          .entries(group.values)
-          .forEach(place => {
-            nest()
-              .key(d => d["ID Age"])
-              .entries(place.values)
-              .forEach(ageType => {
-                const total = sum(ageType.values, d => d.Population);
-                ageType.values.forEach(d => d.share = d.Population / total * 100);
-              });
-          });
-        group.key >= healthInsuranceType[0].Year ? Object.assign(recentYearHealthInsuranceType, group) : {};
-      });
-    // Filter data for each health insurance type.
-    const privateInsuranceData = healthInsuranceType.filter(d => d["ID Health Insurance coverage:type"] === 0);
-    const publicInsuranceData = healthInsuranceType.filter(d => d["ID Health Insurance coverage:type"] === 1);
-
-    // Set data for No Health Insurance Coverage.
-    if (dropdownValue === dropdownList[1]) {
-      data = noInsuranceData;
-      // Find top With Health Insurance data for the most recent year.
-      const noInsuranceRecentData = recentYearHealthInsuranceStatus.values.filter(d => d["ID Health Insurance coverage:status"] === 1);
-      noInsuranceRecentData.sort((a, b) => b.share - a.share);
-      topData = noInsuranceRecentData[0];
-    }
-    // Set data for With Health Insurance Coverage.
-    else if (dropdownValue === dropdownList[2]) {
-      data = withInsuranceData;
-      // Find top With Health Insurance data for the most recent year.
-      const withInsuranceRecentData = recentYearHealthInsuranceStatus.values.filter(d => d["ID Health Insurance coverage:status"] === 0);
-      withInsuranceRecentData.sort((a, b) => b.share - a.share);
-      topData = withInsuranceRecentData[0];
-    }
-    // Set data for Private Health Insurance Coverage.
-    else if (dropdownValue === dropdownList[3]) {
-      data = privateInsuranceData;
-      // Find top Private Health Insurance data for the most recent year.
-      const privateHealthInsuranceRecentData = recentYearHealthInsuranceType.values.filter(d => d["ID Health Insurance coverage:type"] === 0);
-      privateHealthInsuranceRecentData.sort((a, b) => b.share - a.share);
-      topData = privateHealthInsuranceRecentData[0];
-    }
-    // Set data for Public Health Insurance Coverage.
-    else if (dropdownValue === dropdownList[4]) {
-      data = publicInsuranceData;
-      // Find top Public Health Insurance data for the most recent year.
-      const publicHealthInsuranceRecentData = recentYearHealthInsuranceType.values.filter(d => d["ID Health Insurance coverage:type"] === 1);
-      publicHealthInsuranceRecentData.sort((a, b) => b.share - a.share);
-      topData = publicHealthInsuranceRecentData[0];
-    }
+    const sortedRecentYearData = recentYearHealthCoverageType.values.sort((a, b) => b.share - a.share);
+    const topRecentYearData = sortedRecentYearData[0];
 
     return (
       <SectionColumns>
         <SectionTitle>Disability Status</SectionTitle>
         <article>
           {/* Create a dropdown list. */}
-          <select onChange={this.handleChange}>
-            {dropdownList.map(item => <option key={item} value={item}>{item}</option>)}
-          </select>
           <Stat 
-            title={`Majority age group in ${topData.Year}`}
-            value={`${topData.Age} ${formatPopulation(topData.share)}`}
+            title={`Majority age group in ${topRecentYearData.Year}`}
+            value={`${rangeFormatter(topRecentYearData.Age)} ${formatPopulation(topRecentYearData.share)}`}
           />
-          <p>In {topData.Year}, the majority age group with disability was {topData.Age} with {formatPopulation(topData.share)} in the {topData.County} county.</p>
-          <p>The Bar Chart here shows the percentage of disabled population in the {topData.County} county.</p>
+          <p>In {topRecentYearData.Year}, the majority age group with disability was {rangeFormatter(topRecentYearData.Age)} years with {formatPopulation(topRecentYearData.share)} in the {topRecentYearData.County} county.</p>
+          <p>The Bar Chart here shows the percentage of population with public, private and no health insurance in the {topRecentYearData.County} county.</p>
         </article>
         <BarChart config={{
-          data,
+          data: healthCoverageType,
           discrete: "x",
           height: 400,
-          groupBy: "Age",
+          groupBy: "Health Insurance coverage:type",
           legend: false,
           x: d => d.Age,
           y: "share",
           time: "ID Year",
+          groupPadding: 40,
           xConfig: {
             labelRotation: false,
             tickFormat: d => rangeFormatter(d)
@@ -179,15 +79,11 @@ DisabilityStatus.defaultProps = {
 };
 
 DisabilityStatus.need = [
-  fetchData("disabilityStatus", "/api/data?measures=Population&drilldowns=Disability%20Status,Age&County=05000US26163&Year=all", d => d.data),
-  fetchData("healthInsuranceStatus", "/api/data?measures=Population&drilldowns=Health%20Insurance%20coverage%3Astatus,Age&County=05000US26163&Year=all", d => d.data),
-  fetchData("healthInsuranceType", "/api/data?measures=Population&drilldowns=Health%20Insurance%20coverage%3Atype,Age&County=05000US26163&Year=all", d => d.data)
+  fetchData("healthCoverageType", "/api/data?measures=Population&drilldowns=Health%20Insurance%20coverage%3Astatus,Health%20Insurance%20coverage%3Atype,Age&County=<id>&Year=all", d => d.data)
 ];
 
 const mapStateToProps = state => ({
-  disabilityStatus: state.data.disabilityStatus,
-  healthInsuranceStatus: state.data.healthInsuranceStatus,
-  healthInsuranceType: state.data.healthInsuranceType
+  healthCoverageType: state.data.healthCoverageType
 });
 
 export default connect(mapStateToProps)(DisabilityStatus);

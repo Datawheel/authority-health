7b692ccfaf65867c79781ada1f0d1d13b2146468
Rashmi <baheti.rash123@gmail.com>
2018-11-01 17:07:51 -0400

    adds LinePlot for rental amount

--- app/pages/Profile/sections/housingAndLiving/HouseRentals.jsx
+++ app/pages/Profile/sections/housingAndLiving/HouseRentals.jsx
@@ -1,20 +1,50 @@
 import React from "react";
-// import {connect} from "react-redux";
-// import {BarChart} from "d3plus-react";
-// import {formatAbbreviate} from "d3plus-format";
+import {connect} from "react-redux";
+import {LinePlot} from "d3plus-react";
+import {formatAbbreviate} from "d3plus-format";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
 // import Stat from "../../components/Stat";
+// import places from "../../../../utils/places";
 
 class HouseRentals extends SectionColumns {
 
+  constructor(props) {
+    super(props);
+    this.state = {dropdownValue: "Child"};
+  }
+
   render() {
+
+    const {rentAmountData} = this.props;
+    console.log("rentAmountData: ", rentAmountData);
+
     return (
       <SectionColumns>
         <SectionTitle>House Rentals</SectionTitle>
         <article>
         </article>
+        {/* Create a Geomap based on the dropdown choice. */}
+        <LinePlot config={{
+          // data: "/api/data?measures=Construction%20Date&drilldowns=Place&Year=all",
+          data: rentAmountData,
+          discrete: "x",
+          height: 400,
+          groupBy: "ID County",
+          label: d => d.Year,
+          x: "Year",
+          xConfig: {
+            title: "Year"
+          },
+          y: "Rent Amount",
+          yConfig: {
+            tickFormat: d => formatAbbreviate(d),
+            title: "Rent amount"
+          },
+          tooltipConfig: {tbody: [["Value", d => `$${formatAbbreviate(d["Rent Amount"])}`]]}
+        }}
+        />
       </SectionColumns>
     );
   }
@@ -24,4 +54,13 @@ HouseRentals.defaultProps = {
   slug: "house-rentals"
 };
 
-export default HouseRentals;
+HouseRentals.need = [
+  fetchData("rentAmountData", "/api/data?measures=Rent%20Amount&County=<id>&Year=all", d => d.data)
+];
+
+const mapStateToProps = state => ({
+  rentAmountData: state.data.rentAmountData
+});
+  
+export default connect(mapStateToProps)(HouseRentals);
+

fa857b110f2bc4c18a29502aee9612287b02314e
James Ferrell <hello@jamesferrell.me>
2018-11-14 15:29:26 -0500

    implements footer

--- app/App.jsx
+++ app/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 import MainNav from "components/MainNav";
+import Footer from "components/Footer";
 import "./App.css";
 
 export default class App extends Component {
@@ -16,18 +17,11 @@ export default class App extends Component {
 
     return (
       <div className="outer-container">
-
         <MainNav />
-
-        {/* main content */}
         <main className="main-container" role="main">
           { children }
         </main>
-
-        {/* footer */}
-        {/*<footer role="contentinfo">
-          Footer
-        </footer>*/}
+        <Footer />
       </div>
     );
   }

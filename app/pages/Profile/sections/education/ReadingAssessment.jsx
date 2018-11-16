6e3e25b8ed38ca50fb91c7baa11ed7817a483673
Rashmi <baheti.rash123@gmail.com>
2018-11-15 21:59:36 -0800

    adds LinePlot for average reading assessment score by gender

--- app/pages/Profile/sections/education/ReadingAssessment.jsx
+++ app/pages/Profile/sections/education/ReadingAssessment.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-// import {connect} from "react-redux";
-// import {BarChart} from "d3plus-react";
-// import {formatAbbreviate} from "d3plus-format";
+import {connect} from "react-redux";
+import {LinePlot} from "d3plus-react";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
@@ -10,11 +9,34 @@ import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 class ReadingAssessment extends SectionColumns {
 
   render() {
+
+    const {readingScoresByGender} = this.props;
+    console.log("readingScoresByGender: ", readingScoresByGender);
+
     return (
       <SectionColumns>
         <SectionTitle>Reading Assessment</SectionTitle>
         <article>
         </article>
+
+        {/* Lineplot to show the Reading assessment for different years in the Detroit City. */}
+        <LinePlot config={{
+          data: readingScoresByGender,
+          discrete: "x",
+          height: 250,
+          groupBy: d => `${d.Grade} ${d.Gender}`,
+          label: d => `${d.Grade}th Grade ${d.Gender}`,
+          x: "Year",
+          xConfig: {
+            title: "Year"
+          },
+          y: "Average Reading Score",
+          yConfig: {
+            title: "Average Reading Score"
+          },
+          tooltipConfig: {tbody: [["Score", d => d["Average Reading Score"]]]}
+        }}
+        />
       </SectionColumns>
     );
   }
@@ -24,4 +46,13 @@ ReadingAssessment.defaultProps = {
   slug: "reading-assessment"
 };
 
-export default ReadingAssessment;
+
+ReadingAssessment.need = [
+  fetchData("readingScoresByGender", "/api/data?measures=Average%20Reading%20Score&drilldowns=Grade,Gender,City&Year=all", d => d.data)
+];
+
+const mapStateToProps = state => ({
+  readingScoresByGender: state.data.readingScoresByGender
+});
+  
+export default connect(mapStateToProps)(ReadingAssessment);

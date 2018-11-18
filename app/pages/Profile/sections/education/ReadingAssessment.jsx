2840a0e9399d8907e95b62109cdc32a3c2619ff0
Rashmi <baheti.rash123@gmail.com>
2018-11-18 15:01:49 -0800

    adds stats and Lineplot for reading assessment by Gender, ELL and Disability

--- app/pages/Profile/sections/education/ReadingAssessment.jsx
+++ app/pages/Profile/sections/education/ReadingAssessment.jsx
@@ -11,7 +11,7 @@ class ReadingAssessment extends SectionColumns {
 
   constructor(props) {
     super(props);
-    this.state = {dropdownValue: "Gender"};
+    this.state = {dropdownValue: "Disability"};
   }
 
   // Handler function for dropdown onChange.
@@ -21,79 +21,234 @@ class ReadingAssessment extends SectionColumns {
     const {dropdownValue} = this.state;
     console.log("dropdownValue: ", dropdownValue);
 
-    const {readingScoresByGender} = this.props;
+    const {readingScoresByGender, readingScoresByELL, readingScoresByDisability} = this.props;
     console.log("readingScoresByGender: ", readingScoresByGender);
+    console.log("readingScoresByELL: ", readingScoresByELL);
+    console.log("readingScoresByDisability: ", readingScoresByDisability);
 
-    // Get the recent year male and female 4th and 8th grade data.
-    const recentYearReadingScoresByGender = {};
-    nest()
-      .key(d => d.Year)
-      .entries(readingScoresByGender)
-      .forEach(group => {
-        group.key >= readingScoresByGender[0].Year ? Object.assign(recentYearReadingScoresByGender, group) : {};
-      });
+    const readingAssessmentChoices = ["Gender", "ELL", "Disability"];
 
-    // Find top male 4th Grade data.
-    const maleReadingScores = recentYearReadingScoresByGender.values.filter(d => d.Gender === "Male");
-    const maleFourthGradeReadingScores = maleReadingScores.filter(d => d.Grade === "4");
+    // const drawVisualization = (vizData) => {
 
-    // Find top male 8th grade data.
-    const maleEighthGradeReadingScores = maleReadingScores.filter(d => d.Grade === "8");
+    // }
 
-    // Find top female 4th Grade data.
-    const femaleReadingScores = recentYearReadingScoresByGender.values.filter(d => d.Gender === "Female");
-    const femaleFourthGradeReadingScores = femaleReadingScores.filter(d => d.Grade === "4");
+    if (dropdownValue === "Gender") {
+      // Get the recent year male and female 4th and 8th grade data.
+      const recentYearReadingScoresByGender = {};
+      nest()
+        .key(d => d.Year)
+        .entries(readingScoresByGender)
+        .forEach(group => {
+          group.key >= readingScoresByGender[0].Year ? Object.assign(recentYearReadingScoresByGender, group) : {};
+        });
 
-    // Find top female 8th grade data.
-    const femaleEighthGradeReadingScores = femaleReadingScores.filter(d => d.Grade === "8");
+      // Find top male 4th Grade data.
+      const maleReadingScores = recentYearReadingScoresByGender.values.filter(d => d.Gender === "Male");
+      const maleFourthGradeReadingScores = maleReadingScores.filter(d => d.Grade === "4");
 
-    const readingAssessmentChoices = ["Gender"];
+      // Find top male 8th grade data.
+      const maleEighthGradeReadingScores = maleReadingScores.filter(d => d.Grade === "8");
 
+      // Find top female 4th Grade data.
+      const femaleReadingScores = recentYearReadingScoresByGender.values.filter(d => d.Gender === "Female");
+      const femaleFourthGradeReadingScores = femaleReadingScores.filter(d => d.Grade === "4");
+
+      // Find top female 8th grade data.
+      const femaleEighthGradeReadingScores = femaleReadingScores.filter(d => d.Grade === "8");
+
+      return (
+        <SectionColumns>
+          <SectionTitle>Reading Assessment</SectionTitle>
+          <article>
+            <select onChange={this.handleChange}>
+              {readingAssessmentChoices.map((item, i) => <option key={i} value={item}>{item}</option>)}
+            </select>
+            <Stat
+              title={`Average 4th Grade Score in ${maleFourthGradeReadingScores[0].Year}`}
+              value={`${maleFourthGradeReadingScores[0].Gender} ${maleFourthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+            <Stat
+              title={`Average 8th Grade Score in ${maleEighthGradeReadingScores[0].Year}`}
+              value={`${maleEighthGradeReadingScores[0].Gender} ${maleEighthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+            <Stat
+              title={`Average 4th Grade Score in ${femaleFourthGradeReadingScores[0].Year}`}
+              value={`${femaleFourthGradeReadingScores[0].Gender} ${femaleFourthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+            <Stat
+              title={`Average 8th Grade Score in ${femaleEighthGradeReadingScores[0].Year}`}
+              value={`${femaleEighthGradeReadingScores[0].Gender} ${femaleEighthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+          </article>
+  
+          {/* Lineplot to show the Reading assessment for different years in the Detroit City. */}
+          <LinePlot config={{
+            data: readingScoresByGender,
+            discrete: "x",
+            height: 250,
+            groupBy: d => `${d.Grade} ${d.Gender}`,
+            label: d => `${d.Grade}th Grade ${d.Gender}`,
+            x: "Year",
+            xConfig: {
+              title: "Year"
+            },
+            y: "Average Reading Score",
+            yConfig: {
+              title: "Average Reading Score"
+            },
+            // shapeConfig: {strokeDasharray: "4 1"},
+            tooltipConfig: {tbody: [["Score", d => d["Average Reading Score"]]]}
+          }}
+          />
+        </SectionColumns>
+      );
+    }
+    else if (dropdownValue === "ELL") {
+      // Get the recent year ELL scores for 4th and 8th grade.
+      const recentYearReadingScoresByELL = {};
+      nest()
+        .key(d => d.Year)
+        .entries(readingScoresByELL)
+        .forEach(group => {
+          group.key >= readingScoresByELL[0].Year ? Object.assign(recentYearReadingScoresByELL, group) : {};
+        });
+
+      // Find top with ELL 4th Grade data.
+      const withELLReadingScores = recentYearReadingScoresByELL.values.filter(d => d.ELL === "Yes");
+      const withELLFourthGradeReadingScores = withELLReadingScores.filter(d => d.Grade === "4");
+
+      // Find top male 8th grade data.
+      const withELLEighthGradeReadingScores = withELLReadingScores.filter(d => d.Grade === "8");
+
+      // Find top female 4th Grade data.
+      const noELLReadingScores = recentYearReadingScoresByELL.values.filter(d => d.ELL === "No");
+      const noELLFourthGradeReadingScores = noELLReadingScores.filter(d => d.Grade === "4");
+
+      // Find top female 8th grade data.
+      const noELLEighthGradeReadingScores = noELLReadingScores.filter(d => d.Grade === "8");
+
+      return (
+        <SectionColumns>
+          <SectionTitle>Reading Assessment</SectionTitle>
+          <article>
+            <select onChange={this.handleChange}>
+              {readingAssessmentChoices.map((item, i) => <option key={i} value={item}>{item}</option>)}
+            </select>
+            <Stat
+              title={`Average 4th Grade Score in ${withELLFourthGradeReadingScores[0].Year}`}
+              value={`With ELL ${withELLFourthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+            <Stat
+              title={`Average 8th Grade Score in ${withELLEighthGradeReadingScores[0].Year}`}
+              value={`With ELL ${withELLEighthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+            <Stat
+              title={`Average 4th Grade Score in ${noELLFourthGradeReadingScores[0].Year}`}
+              value={`No ELL ${noELLFourthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+            <Stat
+              title={`Average 8th Grade Score in ${noELLEighthGradeReadingScores[0].Year}`}
+              value={`No ELL ${noELLEighthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+          </article>
+    
+          {/* Lineplot to show the Reading assessment for different years in the Detroit City. */}
+          <LinePlot config={{
+            data: readingScoresByELL,
+            discrete: "x",
+            height: 250,
+            groupBy: d => `${d.Grade} ${d.ELL}`,
+            label: d => `${d.Grade}th Grade ${d.ELL === "Yes" ? "With ELL" : "No ELL"}`,
+            x: "Year",
+            xConfig: {
+              title: "Year"
+            },
+            y: "Average Reading Score",
+            yConfig: {
+              title: "Average Reading Score"
+            },
+            tooltipConfig: {tbody: [["Score", d => d["Average Reading Score"]]]}
+          }}
+          />
+        </SectionColumns>
+      );
+    }
+    else if (dropdownValue === "Disability") {
+      // Get the recent year ELL scores for 4th and 8th grade.
+      const recentYearReadingScoresByDisability = {};
+      nest()
+        .key(d => d.Year)
+        .entries(readingScoresByDisability)
+        .forEach(group => {
+          group.key >= readingScoresByDisability[0].Year ? Object.assign(recentYearReadingScoresByDisability, group) : {};
+        });
+
+      // Find top with Disability 4th Grade data.
+      const withDisabilityReadingScores = recentYearReadingScoresByDisability.values.filter(d => d.Disability === "Yes");
+      const withDisabilityFourthGradeReadingScores = withDisabilityReadingScores.filter(d => d.Grade === "4");
+
+      // Find top with Disability 8th grade data.
+      const withDisabilityEighthGradeReadingScores = withDisabilityReadingScores.filter(d => d.Grade === "8");
+
+      // Find top no Disability 4th Grade data.
+      const noDisabilityReadingScores = recentYearReadingScoresByDisability.values.filter(d => d.Disability === "No");
+      const noDisabilityFourthGradeReadingScores = noDisabilityReadingScores.filter(d => d.Grade === "4");
+
+      // Find top no Disability 8th grade data.
+      const noDisabilityEighthGradeReadingScores = noDisabilityReadingScores.filter(d => d.Grade === "8");
+
+      return (
+        <SectionColumns>
+          <SectionTitle>Reading Assessment</SectionTitle>
+          <article>
+            <select onChange={this.handleChange}>
+              {readingAssessmentChoices.map((item, i) => <option key={i} value={item}>{item}</option>)}
+            </select>
+            <Stat
+              title={`Average 4th Grade Score in ${withDisabilityFourthGradeReadingScores[0].Year}`}
+              value={`With Disability ${withDisabilityFourthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+            <Stat
+              title={`Average 8th Grade Score in ${withDisabilityEighthGradeReadingScores[0].Year}`}
+              value={`With Disability ${withDisabilityEighthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+            <Stat
+              title={`Average 4th Grade Score in ${noDisabilityFourthGradeReadingScores[0].Year}`}
+              value={`No Disability ${noDisabilityFourthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+            <Stat
+              title={`Average 8th Grade Score in ${noDisabilityEighthGradeReadingScores[0].Year}`}
+              value={`No Disability ${noDisabilityEighthGradeReadingScores[0]["Average Reading Score"]}`}
+            />
+          </article>
+    
+          {/* Lineplot to show the Reading assessment with and without disability for different years in the Detroit City. */}
+          <LinePlot config={{
+            data: readingScoresByDisability,
+            discrete: "x",
+            height: 250,
+            groupBy: d => `${d.Grade} ${d.Disability}`,
+            label: d => `${d.Grade}th Grade ${d.Disability === "Yes" ? "With Disability" : "No Disability"}`,
+            x: "Year",
+            xConfig: {
+              title: "Year"
+            },
+            y: "Average Reading Score",
+            yConfig: {
+              title: "Average Reading Score"
+            },
+            tooltipConfig: {tbody: [["Score", d => d["Average Reading Score"]]]}
+          }}
+          />
+        </SectionColumns>
+      );
+    }
+
+    
     return (
-      <SectionColumns>
-        <SectionTitle>Reading Assessment</SectionTitle>
-        <article>
-          <select onChange={this.handleChange}>
-            {readingAssessmentChoices.map((item, i) => <option key={i} value={item}>{item}</option>)}
-          </select>
-          <Stat
-            title={`Average 4th Grade Score in ${maleFourthGradeReadingScores[0].Year}`}
-            value={`${maleFourthGradeReadingScores[0].Gender} ${maleFourthGradeReadingScores[0]["Average Reading Score"]}`}
-          />
-          <Stat
-            title={`Average 8th Grade Score in ${maleEighthGradeReadingScores[0].Year}`}
-            value={`${maleEighthGradeReadingScores[0].Gender} ${maleEighthGradeReadingScores[0]["Average Reading Score"]}`}
-          />
-          <Stat
-            title={`Average 4th Grade Score in ${femaleFourthGradeReadingScores[0].Year}`}
-            value={`${femaleFourthGradeReadingScores[0].Gender} ${femaleFourthGradeReadingScores[0]["Average Reading Score"]}`}
-          />
-          <Stat
-            title={`Average 8th Grade Score in ${femaleEighthGradeReadingScores[0].Year}`}
-            value={`${femaleEighthGradeReadingScores[0].Gender} ${femaleEighthGradeReadingScores[0]["Average Reading Score"]}`}
-          />
-        </article>
-
-        {/* Lineplot to show the Reading assessment for different years in the Detroit City. */}
-        <LinePlot config={{
-          data: readingScoresByGender,
-          discrete: "x",
-          height: 250,
-          groupBy: d => `${d.Grade} ${d.Gender}`,
-          label: d => `${d.Grade}th Grade ${d.Gender}`,
-          x: "Year",
-          xConfig: {
-            title: "Year"
-          },
-          y: "Average Reading Score",
-          yConfig: {
-            title: "Average Reading Score"
-          },
-          tooltipConfig: {tbody: [["Score", d => d["Average Reading Score"]]]}
-        }}
-        />
-      </SectionColumns>
+      <div>No dropdown selected</div>
     );
+    
   }
 }
 
@@ -103,11 +258,15 @@ ReadingAssessment.defaultProps = {
 
 
 ReadingAssessment.need = [
-  fetchData("readingScoresByGender", "/api/data?measures=Average%20Reading%20Score&drilldowns=Grade,Gender,City&Year=all", d => d.data)
+  fetchData("readingScoresByGender", "/api/data?measures=Average%20Reading%20Score&drilldowns=Grade,Gender,City&Year=all", d => d.data),
+  fetchData("readingScoresByELL", "/api/data?measures=Average%20Reading%20Score&drilldowns=Grade,ELL,City&Year=all", d => d.data),
+  fetchData("readingScoresByDisability", "/api/data?measures=Average%20Reading%20Score&drilldowns=Grade,Disability,City&Year=all", d => d.data)
 ];
 
 const mapStateToProps = state => ({
-  readingScoresByGender: state.data.readingScoresByGender
+  readingScoresByGender: state.data.readingScoresByGender,
+  readingScoresByELL: state.data.readingScoresByELL,
+  readingScoresByDisability: state.data.readingScoresByDisability
 });
   
 export default connect(mapStateToProps)(ReadingAssessment);

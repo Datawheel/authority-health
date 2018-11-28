2fadb9cd954209512b82c642993e78c83a8f3e0f
Rashmi <baheti.rash123@gmail.com>
2018-11-28 17:21:44 -0500

    adds dropdown list, Geomap, stats and short paragraph

--- app/pages/Profile/sections/healthBehaviors/PhysicalActivity.jsx
+++ app/pages/Profile/sections/healthBehaviors/PhysicalActivity.jsx
@@ -1,20 +1,77 @@
 import React from "react";
 import {connect} from "react-redux";
-// import {BarChart} from "d3plus-react";
-// import {formatAbbreviate} from "d3plus-format";
+import {Geomap} from "d3plus-react";
+import {nest} from "d3-collection";
+import {formatAbbreviate} from "d3plus-format";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
-// import Stat from "../../components/Stat";
+import Stat from "../../components/Stat";
+
+const formatPercentage = d => `${formatAbbreviate(d)}%`;
+const formatDropdownChoiceName = d => {
+  const wordsList = d.split(" ");
+  return `${wordsList[0]} ${wordsList[1]}`;
+};
 
 class PhysicalActivity extends SectionColumns {
 
+  constructor(props) {
+    super(props);
+    this.state = {dropdownValue: "Physical Health Data Value"};
+  }
+
+  // Handler function for dropdown onChange event.
+  handleChange = event => this.setState({dropdownValue: event.target.value});
+
   render() {
+
+    const {physicalActivity} = this.props;
+    console.log("physicalActivity: ", physicalActivity);
+
+    const {dropdownValue} = this.state;
+    const dropdownList = ["Physical Health Data Value", "Physical Inactivity Data Value"];
+    // const physicalHealthSelected = dropdownValue === "Physical Health Data Value";
+
+    const recentYearData = {};
+    nest()
+      .key(d => d.Year)
+      .entries(physicalActivity)
+      .forEach(group => {
+        group.key >= physicalActivity[0].Year ? Object.assign(recentYearData, group) : {};
+      });
+
+    const topRecentYearData = recentYearData.values.sort((a, b) => b[dropdownValue] - a[dropdownValue])[0];
+    console.log("topRecentYearData: ", topRecentYearData);
+
     return (
       <SectionColumns>
         <SectionTitle>Physical Activity</SectionTitle>
         <article>
+          {/* Create a dropdown list for Physical Health and Physical Activity options. */}
+          <select onChange={this.handleChange}>
+            {dropdownList.map((item, i) => <option key={i} value={item}>{formatDropdownChoiceName(item)}</option>)}
+          </select>
+
+          <Stat 
+            title={`Top ${dropdownValue} in ${topRecentYearData.Year}`}
+            value={`${topRecentYearData.Tract} ${formatPercentage(topRecentYearData[dropdownValue])}`}
+          />
+          <p>The Geomap here shows the {dropdownValue} for Tracts in Wayne County, MI.</p>
         </article>
+
+        {/* Geomap to show Physical health and physical Inactivity for tracts in the Wayne County. */}
+        <Geomap config={{
+          data: physicalActivity,
+          groupBy: "ID Tract",
+          colorScale: d => d[dropdownValue],
+          height: 400,
+          time: "Year",
+          tooltipConfig: {tbody: [["Value", d => `${formatPercentage(d[dropdownValue])}`]]},
+          topojson: "/topojson/tract.json",
+          topojsonFilter: d => d.id.startsWith("14000US26163")
+        }}
+        />
       </SectionColumns>
     );
   }
@@ -30,7 +87,7 @@ PhysicalActivity.need = [
 ];
 
 const mapStateToProps = state => ({
-  occupancyData: state.data.occupancyData
+  physicalActivity: state.data.physicalActivity
 });
 
 export default connect(mapStateToProps)(PhysicalActivity);

474b72d86e1cc735edd4b1222c3fdbf224ff9c9c
Rashmi <baheti.rash123@gmail.com>
2018-12-06 16:03:42 -0500

    adds the list of all types of cancer
    - adds stats for cancerByGender and cancerByRaceAndEthnicity

--- app/pages/Profile/sections/healthBehaviors/Cancer.jsx
+++ app/pages/Profile/sections/healthBehaviors/Cancer.jsx
@@ -7,7 +7,7 @@ import {formatAbbreviate} from "d3plus-format";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
-// import Stat from "../../../../components/Stat";
+import Stat from "../../../../components/Stat";
 
 const formatPercentage = d => `${formatAbbreviate(d)}%`;
 
@@ -15,7 +15,7 @@ class Cancer extends SectionColumns {
 
   constructor(props) {
     super(props);
-    this.state = {dropdownValue: "Brain"};
+    this.state = {dropdownValue: "All Invasive Cancer Sites Combined"};
   }
 
   // Handler function for dropdown onChange event.
@@ -23,39 +23,71 @@ class Cancer extends SectionColumns {
   
   render() {
 
-    const {cancerOccuranceRate, cancerByGender, cancerByRaceAndEthnicity} = this.props;
-    console.log("cancerOccuranceRate: ", cancerOccuranceRate);
-    console.log("cancerByGender: ", cancerByGender);
-    console.log("cancerByRaceAndEthnicity: ", cancerByRaceAndEthnicity);
+    const {cancerOccuranceRate, cancerByGender, cancerByRaceAndEthnicity, getSortedCancerTypes} = this.props;
+
+    const {dropdownValue} = this.state;
+    // const dropdownList = ["Acute Lymphocytic Leukemia", "Acute Myeloid Leukemia", "Aleukemic, Subleukemic and NOS", "All Invasive Cancer Sites Combined", "Anus, Anal Canal and Anorectum", "Appendix", "Ascending Colon", "Bones and Joints", "Brain", "Brain and Other Nervous System", "Cecum", "Cervix Uteri", "Chronic Lymphocytic Leukemia", "Chronic Myeloid Leukemia", "Colon and Rectum", "Colon excluding Rectum", "Corpus Uteri", "Descending Colon", "Digestive System", "Endocrine System", "Esophagus", "Eye and Orbit", "Female Breast", "Female Breast, In Situ", "Female Genital System", "Floor of Mouth", "Gallbladder", "Gum and Other Mouth", "Hepatic Flexure", "Hodgkin - Nodal", "Hodgkin Lymphoma", "Hypopharynx", "Intrahepatic Bile Duct", "Kaposi Sarcoma", "Kidney and Renal Pelvis", "Larynx", "Leukemias", "Lip", "Liver"];
+
+    // Filter data based on the dropdown Value.
+    const filteredOccuranceRateData = cancerOccuranceRate.filter(d => d["Cancer Site"] === dropdownValue);
+    const filteredGenderData = cancerByGender.filter(d => d["Cancer Site"] === dropdownValue);
+    const filteredRaceAndEthnicityData = cancerByRaceAndEthnicity.filter(d => d["Cancer Site"] === dropdownValue);
+
+    console.log("filteredGenderData: ", filteredGenderData);
+    const dropdownList = [];
+    nest()
+      .key(d => d["Cancer Site"])
+      .entries(getSortedCancerTypes)
+      .forEach(group => dropdownList.push(group.key));
 
     const recentYearCancerByGender = {};
     nest()
       .key(d => d.Year)
-      .entries(cancerByGender)
+      .entries(filteredGenderData)
       .forEach(group => {
         const total = sum(group.values, d => d.Count);
         group.values.forEach(d => d.share = d.Count / total * 100);
-        group.key >= cancerByGender[0].Year ? Object.assign(recentYearCancerByGender, group) : {};
+        group.key >= filteredGenderData[0].Year ? Object.assign(recentYearCancerByGender, group) : {};
       });
+    const topFemaleCancerData = recentYearCancerByGender.values[0];
+    const topMaleCancerData = recentYearCancerByGender.values[1];
 
     const recentYearCancerByRaceAndEthnicity = {};
     nest()
       .key(d => d.Year)
-      .entries(cancerByRaceAndEthnicity)
+      .entries(filteredRaceAndEthnicityData)
       .forEach(group => {
         const total = sum(group.values, d => d.Count);
         group.values.forEach(d => d.share = d.Count / total * 100);
-        group.key >= cancerByGender[0].Year ? Object.assign(recentYearCancerByRaceAndEthnicity, group) : {};
+        group.key >= filteredRaceAndEthnicityData[0].Year ? Object.assign(recentYearCancerByRaceAndEthnicity, group) : {};
       });
+    recentYearCancerByRaceAndEthnicity.values.sort((a, b) => b.share - a.share);
+    const topCancerByRaceAndEthnicity = recentYearCancerByRaceAndEthnicity.values[0];
 
     return (
       <SectionColumns>
         <SectionTitle>Cancer</SectionTitle>
         <article>
+          {/* Create a dropdown for sheltered and unsheltered choices. */}
+          <select onChange={this.handleChange}>
+            {dropdownList.map(item => <option key={item} value={item}>{item}</option>)}
+          </select>
+          <Stat
+            title={`Top Female ${dropdownValue} in ${topFemaleCancerData.Year}`}
+            value={`${formatPercentage(topFemaleCancerData.share)}`}
+          />
+          <Stat
+            title={`Top Male ${dropdownValue} in ${topMaleCancerData.Year}`}
+            value={`${formatPercentage(topMaleCancerData.share)}`}
+          />
+          <Stat
+            title={`Top Race & Ethnicity Cancer in ${topCancerByRaceAndEthnicity.Year}`}
+            value={`${topCancerByRaceAndEthnicity.Ethnicity} ${topCancerByRaceAndEthnicity.Race} ${formatPercentage(topCancerByRaceAndEthnicity.share)}`}
+          />
 
           {/* Draw a BarChart to show Cancer by Sex for selected cancer type. */}
           <BarChart config={{
-            data: cancerByGender,
+            data: filteredGenderData,
             discrete: "y",
             height: 250,
             legend: false,
@@ -70,8 +102,7 @@ class Cancer extends SectionColumns {
               labelRotation: false
             },
             yConfig: {
-              tickFormat: d => d,
-              labelRotation: false
+              tickFormat: d => d
             },
             tooltipConfig: {tbody: [["Value", d => formatPercentage(d.share)]]}
           }}
@@ -79,7 +110,7 @@ class Cancer extends SectionColumns {
           
           {/* Draw a BarChart to show Cancer by Race and Ethnicity for selected cancer type. */}
           <BarChart config={{
-            data: cancerByRaceAndEthnicity,
+            data: filteredRaceAndEthnicityData,
             discrete: "y",
             height: 250,
             legend: false,
@@ -98,9 +129,9 @@ class Cancer extends SectionColumns {
           }}
           />
         </article>
-
+        
         <LinePlot config={{
-          data: cancerOccuranceRate,
+          data: filteredOccuranceRateData,
           discrete: "x",
           height: 400,
           groupBy: "Cancer Site",
@@ -132,15 +163,17 @@ Cancer.defaultProps = {
 };
 
 Cancer.need = [
-  fetchData("cancerOccuranceRate", "/api/data?measures=Age-Adjusted%20Rate,Age-Adjusted%20Rate%20Lower%2095%20Percent%20Confidence%20Interval,Age-Adjusted%20Rate%20Upper%2095%20Percent%20Confidence%20Interval&drilldowns=MSA&Year=all&Cancer%20Site=Brain", d => d.data),
-  fetchData("cancerByGender", "/api/data?measures=Count&drilldowns=MSA,Sex&Year=all&Cancer%20Site=Brain", d => d.data),
-  fetchData("cancerByRaceAndEthnicity", "/api/data?measures=Count&drilldowns=MSA,Race,Ethnicity&Year=all&Cancer%20Site=Brain", d => d.data)
+  fetchData("cancerOccuranceRate", "/api/data?measures=Age-Adjusted%20Rate,Age-Adjusted%20Rate%20Lower%2095%20Percent%20Confidence%20Interval,Age-Adjusted%20Rate%20Upper%2095%20Percent%20Confidence%20Interval&drilldowns=Cancer%20Site&Year=all", d => d.data),
+  fetchData("cancerByGender", "/api/data?measures=Count&drilldowns=Cancer%20Site,Sex&Year=all", d => d.data),
+  fetchData("cancerByRaceAndEthnicity", "/api/data?measures=Count&drilldowns=Cancer%20Site,Race,Ethnicity&Year=all", d => d.data),
+  fetchData("getSortedCancerTypes", "/api/data?measures=Count&drilldowns=Cancer%20Site&Year=all&order=Count&sort=desc", d => d.data)
 ];
 
 const mapStateToProps = state => ({
   cancerOccuranceRate: state.data.cancerOccuranceRate,
   cancerByGender: state.data.cancerByGender,
-  cancerByRaceAndEthnicity: state.data.cancerByRaceAndEthnicity
+  cancerByRaceAndEthnicity: state.data.cancerByRaceAndEthnicity,
+  getSortedCancerTypes: state.data.getSortedCancerTypes
 });
 
 export default connect(mapStateToProps)(Cancer);

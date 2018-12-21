17268d7fc12c431eea3692be505a546a98753ffb
Rashmi <baheti.rash123@gmail.com>
2018-12-21 11:07:16 -0500

    adds dropdown for lead and mercury level
    - updates map config
    - removes unwanted code

--- app/pages/Profile/sections/naturalEnvironment/WaterQuality.jsx
+++ app/pages/Profile/sections/naturalEnvironment/WaterQuality.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import {connect} from "react-redux";
-import {Geomap, LinePlot} from "d3plus-react";
-// import {formatAbbreviate} from "d3plus-format";
+import {Geomap} from "d3plus-react";
+import {titleCase} from "d3plus-text";
+
+import styles from "style.yml";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
 import Stat from "../../../../components/Stat";
 
 const formatData = d => {
-  console.log("d: ", d);
-  if (d === "unavailable") return 0;
-  if (d === "normal") return 1;
-  if (d === "excessive") return 2;
-  return -1;
+  let value = -1;
+  if (d === "unavailable") value = 0;
+  if (d === "normal") value = 1;
+  if (d === "excessive") value = 2;
+  return value;
+};
+
+const formatLabel = d => {
+  if (d === 0) return "Unavailable";
+  else if (d === 1) return "Normal";
+  else if (d === 2) return "Excessive";
+  return d;
 };
 
 const getLeadStats = excessiveLeadData => {
@@ -37,64 +46,60 @@ const getMercuryStats = excessiveMercuryData => {
 
 class WaterQuality extends SectionColumns {
 
+  constructor(props) {
+    super(props);
+    this.state = {dropdownValue: "Lead Level"};
+  }
+
+  // Handler function for dropdown onChange event.
+  handleChange = event => this.setState({dropdownValue: event.target.value});
+
   render() {
-
     const {waterQualityData} = this.props;
-    // console.log("waterQualityData: ", waterQualityData);
-
-
     const filteredData = waterQualityData.data.filter(d => d["ID Tract"].startsWith("14000US26163"));
-    // console.log("filteredData: ", filteredData);
+    
+    const {dropdownValue} = this.state;
+    const dropdownList = ["Lead Level", "Mercury Level"];
+    const leadLevelSelected = dropdownValue === "Lead Level";
 
     const excessiveLeadData = filteredData.filter(d => d["Lead Level"] === "excessive");
-    // console.log("excessiveLeadData: ", excessiveLeadData);
-
     const excessiveMercuryData = filteredData.filter(d => d["Mercury Level"] === "excessive");
-    // console.log("excessiveMercuryData: ", excessiveMercuryData);
 
     const leadStats = getLeadStats(excessiveLeadData);
     const mercuryStats = getMercuryStats(excessiveMercuryData);
+
+    const chartData = waterQualityData.data.filter(d => formatData(d["Lead Level"]) !== -1);
 
     return (
       <SectionColumns>
         <SectionTitle>Water Quality</SectionTitle>
         <article>
-          {leadStats}
-          {mercuryStats}
+          {/* Create a dropdown for lead and mercury level in water. */}
+          <div className="pt-select pt-fill">
+            <select onChange={this.handleChange}>
+              {dropdownList.map(item => <option key={item} value={item}>{item}</option>)}
+            </select>
+          </div>
+
+          {leadLevelSelected ? leadStats : mercuryStats}
         </article>
 
         {/* Geomap to show Lead and Mercury level in water for all tracts in the Wayne County. */}
         <Geomap config={{
-          data: waterQualityData.data,
+          data: chartData,
           groupBy: "ID Tract",
-          // colorScale: d => formatData(d["Lead Level"]),
-          shapeConfig: {Path: {fill: d => d["Lead Level"] === "excessive" ? "red" : "transparent"}},
+          colorScale: leadLevelSelected ? d => formatData(d["Lead Level"]) : d => formatData(d["Mercury Level"]),
+          colorScaleConfig: {
+            axisConfig: {tickFormat: d => formatLabel(d)},
+            color: styles["terra-cotta"]
+          },
           height: 400,
           time: "Year",
-          tooltipConfig: {tbody: [["Value", d => d["Lead Level"]]]},
-          topojson: "/topojson/tract.json"
-          // topojsonFilter: d => d.id.startsWith("14000US26163")
+          tooltipConfig: leadLevelSelected ? {tbody: [["Year", d => d.Year], ["Lead Level", d => titleCase(d["Lead Level"])]]} : {tbody: [["Year", d => d.Year], ["Mercury Level", d => titleCase(d["Mercury Level"])]]},
+          topojson: "/topojson/tract.json",
+          topojsonFilter: d => chartData.find(c => c["ID Tract"] === d.id)
         }}
         />
-
-        {/* <LinePlot config={{
-          data: filteredData,
-          discrete: "x",
-          height: 250,
-          groupBy: "ID Tract",
-          legend: false,
-          baseline: 0,
-          x: "Year",
-          xConfig: {
-            title: "Year"
-          },
-          y: d => formatData(d["Lead Level"]),
-          yConfig: {
-            title: "Lead Level"
-          },
-          tooltipConfig: {tbody: [["Value", d => d["Lead Level"]]]}
-        }}
-        /> */}
       </SectionColumns>
     );
   }

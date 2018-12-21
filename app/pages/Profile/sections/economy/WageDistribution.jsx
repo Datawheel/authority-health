84d85b0521398b56a528e99caa6b03008f3545ef
Rashmi <baheti.rash123@gmail.com>
2018-12-21 14:07:56 -0500

    shows wage GINI for profile
    removes wage distribution stats

--- app/pages/Profile/sections/economy/WageDistribution.jsx
+++ app/pages/Profile/sections/economy/WageDistribution.jsx
@@ -18,44 +18,26 @@ class WageDistribution extends SectionColumns {
 
     const {wageDistributionData, wageGinidata} = this.props;
 
-    // Find the top data for the most recent year.
-    const recentYearWageDistribution = {};
     nest()
       .key(d => d.Year)
       .entries(wageDistributionData)
       .forEach(group => {
         const total = sum(group.values, d => d["Household Income"]);
         group.values.forEach(d => d.share = d["Household Income"] / total * 100);
-        group.key >= wageDistributionData[0].Year ? Object.assign(recentYearWageDistribution, group) : {};
       });
-    recentYearWageDistribution.values.sort((a, b) => b.share - a.share);
-    const topWageDistribution = recentYearWageDistribution.values[0];
-
-    // Find all places and wage GINI in Michigan state.
-    const wageGiniDataForMichigan = wageGinidata.filter(d => d["ID Place"].startsWith("16000US26"));
-    wageGiniDataForMichigan.sort((a, b) => b["Wage GINI"] - a["Wage GINI"]);
-    const topWageGini = wageGiniDataForMichigan[0];
 
     return (
       <SectionColumns>
         <SectionTitle>Wage Distribution</SectionTitle>
         <article>
-          {/* Top stats and short paragraph about Wage distribution. */}
-          <Stat
-            title={`Top Wage Distribution in ${topWageDistribution.Geography}`}
-            year={topWageDistribution.Year}
-            value={topWageDistribution["Household Income Bucket"]}
-            qualifier={formatPopulation(topWageDistribution.share)}
-          />
           {/* Top stats and short paragraph about Wage Gini. */}
           <Stat
-            title="Top Wage GINI"
-            year={topWageGini.Year}
-            value={topWageGini.Place}
-            qualifier={formatAbbreviate(topWageGini["Wage GINI"])}
+            title="Wage GINI"
+            year={wageGinidata[0].Year}
+            value={wageGinidata[0]["Wage GINI"]}
           />
-          <p>This Barchart shows the number of workers in various wage buckets in {topWageDistribution.Geography}. In {topWageDistribution.Year}, {topWageDistribution.Geography} had the top Wage Distribution of {topWageDistribution["Household Income Bucket"]} with {formatPopulation(topWageDistribution.share)} share.</p>
-          <p>In {topWageGini.Year}, the highest income inequality in Michigan, was in {topWageGini.Place} with Gini Index of {formatAbbreviate(topWageGini["Wage GINI"])}.</p>
+          <p>In {wageGinidata[0].Year}, the income inequality in {wageGinidata[0].Geography} was {wageGinidata[0]["Wage GINI"]}. The GINI coefficient is a measure of statistical dispersion intended to represent the equality of a distribution, and is the most commonly used measure of inequality. Values range from 0 to 1, with 0 being perfect equality.</p>
+          <p>The following chart shows the number of workers in various wage buckets in {wageDistributionData[0].Geography}.</p>
         </article>
 
         {/* Draw Geomap to show wage distribution for each place in the Wayne county. */}
@@ -73,11 +55,14 @@ class WageDistribution extends SectionColumns {
             labelRotation: false,
             tickFormat: d => rangeFormatter(d)
           },
-          yConfig: {tickFormat: d => formatPopulation(d)},
+          yConfig: {
+            tickFormat: d => formatPopulation(d),
+            title: "Share"
+          },
           shapeConfig: {
             label: false
           },
-          tooltipConfig: {tbody: [["Value", d => formatPopulation(d.share)]]}
+          tooltipConfig: {tbody: [["Share", d => formatPopulation(d.share)]]}
         }}
         />
       </SectionColumns>
@@ -91,7 +76,7 @@ WageDistribution.defaultProps = {
 
 WageDistribution.need = [
   fetchData("wageDistributionData", "https://mammoth.datausa.io/api/data?measures=Household Income&drilldowns=Household Income Bucket&Geography=<id>&Year=all", d => d.data),
-  fetchData("wageGinidata", "https://mammoth.datausa.io/api/data?measures=Wage GINI&drilldowns=Place&Year=latest", d => d.data)
+  fetchData("wageGinidata", "https://mammoth.datausa.io/api/data?measures=Wage GINI&Geography=<id>&Year=latest", d => d.data)
 ];
 
 const mapStateToProps = state => ({

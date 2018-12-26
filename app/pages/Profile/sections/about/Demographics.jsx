6f9d9905f0c45e91682935aa044f0e19492a05e7
Rashmi <baheti.rash123@gmail.com>
2018-12-26 11:59:49 -0800

    re-arranges visualizations and adds socioeconomic ranking stats

--- app/pages/Profile/sections/about/Demographics.jsx
+++ app/pages/Profile/sections/about/Demographics.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {connect} from "react-redux";
 import {sum} from "d3-array";
 import {nest} from "d3-collection";
+import {format} from "d3-format";
 import {BarChart, LinePlot} from "d3plus-react";
 import {formatAbbreviate} from "d3plus-format";
 
@@ -17,11 +18,12 @@ const formatWorkHours = d => {
   return d;
 };
 
+const commas = format(",d");
+
 class Demographics extends SectionColumns {
 
   render() {
-    const {population, populationByAgeAndGender, unemploymentRate, lifeExpectancy, employmentStatus, workExperience} = this.props;
-    console.log("employmentStatus: ", employmentStatus);
+    const {population, populationByAgeAndGender, unemploymentRate, lifeExpectancy, employmentStatus, workExperience, socioeconomicRanking} = this.props;
 
     // Find share for population by Age and Gender.
     nest()
@@ -51,20 +53,29 @@ class Demographics extends SectionColumns {
       });
 
     return (
-      <SectionColumns>
-        <SectionTitle>Demographics</SectionTitle>
-        <article>
+      <div>
+        <SectionColumns>
+          <SectionTitle>Demographics</SectionTitle>
+
           <Stat
             title="Life Expectancy"
             year={""}
             value={`${formatAbbreviate(lifeExpectancy[0]["Life Expectancy"])} Years`}
           />
 
+          <Stat
+            title="Socioeconomic Ranking"
+            year={socioeconomicRanking[0].Year}
+            value={commas(socioeconomicRanking[0]["Socioeconomic Ranking"])}
+          />
+        </SectionColumns>
+
+        <SectionColumns>
           {/* Lineplot to show total population over the years for selected geography. */}
           <LinePlot config={{
             data: population.data,
             discrete: "x",
-            height: 200,
+            height: 300,
             title: "Population Over Years",
             legend: false,
             groupBy: "Geography",
@@ -74,31 +85,45 @@ class Demographics extends SectionColumns {
           }}
           />
 
-          {/* Lineplot to show total population over the years for selected geography. */}
-          <LinePlot config={{
-            data: unemploymentRate,
+          {/* Barchart to show population by age and gender over the years for selected geography. */}
+          <BarChart config={{
+            data: populationByAgeAndGender,
             discrete: "x",
-            height: 200,
-            title: "Unemplyment Rate Over Years",
-            legend: false,
-            groupBy: "Geography",
-            x: "Year",
-            y: "Unemployment Rate",
-            yConfig: {tickFormat: d => formatPercentage(d)},
-            tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPercentage(d["Unemployment Rate"])]]}
+            height: 300,
+            groupBy: "Sex",
+            x: "Age",
+            y: "share",
+            time: "Year",
+            xSort: (a, b) => a["ID Age"] - b["ID Age"],
+            xConfig: {
+              tickFormat: d => rangeFormatter(d),
+              title: "Population by Age and Gender"
+            },
+            yConfig: {
+              tickFormat: d => formatPercentage(d),
+              title: "Share"
+            },
+            shapeConfig: {
+              label: false
+            },
+            tooltipConfig: {tbody: [["Year", d => d.Year], ["Age", d => rangeFormatter(d.Age)], ["Share", d => formatPercentage(d.share)]]}
           }}
           />
 
+        </SectionColumns>
+
+        <SectionColumns>
           {/* Barchart to show work experience by gender over the years for selected geography. */}
           <BarChart config={{
             data: workExperience,
             discrete: "y",
-            height: 200,
+            height: 300,
             groupBy: ["Work Experience", "Sex"],
             stacked: true,
             x: "share",
             y: "Work Experience",
             time: "Year",
+            title: "Work Experience by Gender",
             xConfig: {
               tickFormat: d => formatPercentage(d),
               labelRotation: false,
@@ -114,59 +139,49 @@ class Demographics extends SectionColumns {
             tooltipConfig: {tbody: [["Year", d => d.Year], ["Work Hours", d => formatWorkHours(d["Work Experience"])], ["Share", d => formatPercentage(d.share)]]}
           }}
           />
-        </article>
+          
+          {/* Barchart to show population by age and gender over the years for selected geography. */}
+          <BarChart config={{
+            data: employmentStatus,
+            discrete: "x",
+            height: 300,
+            groupBy: ["Sex", "Employment Status"],
+            x: "Age",
+            stacked: true,
+            y: "share",
+            time: "Year",
+            xSort: (a, b) => a["ID Age"] - b["ID Age"],
+            xConfig: {
+              tickFormat: d => rangeFormatter(d),
+              title: "Employment Status by Age and Gender"
+            },
+            yConfig: {
+              tickFormat: d => formatPercentage(d),
+              title: "Share"
+            },
+            shapeConfig: {
+              label: false
+            },
+            tooltipConfig: {tbody: [["Year", d => d.Year], ["Age", d => rangeFormatter(d.Age)], ["Share", d => formatPercentage(d.share)]]}
+          }}
+          />
 
-        {/* Barchart to show population by age and gender over the years for selected geography. */}
-        <BarChart config={{
-          data: populationByAgeAndGender,
-          discrete: "x",
-          height: 300,
-          groupBy: "Sex",
-          x: "Age",
-          y: "share",
-          time: "Year",
-          xSort: (a, b) => a["ID Age"] - b["ID Age"],
-          xConfig: {
-            tickFormat: d => rangeFormatter(d),
-            title: "Population by Age and Gender"
-          },
-          yConfig: {
-            tickFormat: d => formatPercentage(d),
-            title: "Share"
-          },
-          shapeConfig: {
-            label: false
-          },
-          tooltipConfig: {tbody: [["Year", d => d.Year], ["Age", d => rangeFormatter(d.Age)], ["Share", d => formatPercentage(d.share)]]}
-        }}
-        />
-        <br />
-        {/* Barchart to show population by age and gender over the years for selected geography. */}
-        <BarChart config={{
-          data: employmentStatus,
-          discrete: "x",
-          height: 300,
-          groupBy: ["Sex", "Employment Status"],
-          x: "Age",
-          stacked: true,
-          y: "share",
-          time: "Year",
-          xSort: (a, b) => a["ID Age"] - b["ID Age"],
-          xConfig: {
-            tickFormat: d => rangeFormatter(d),
-            title: "Employment Status by Age and Gender"
-          },
-          yConfig: {
-            tickFormat: d => formatPercentage(d),
-            title: "Share"
-          },
-          shapeConfig: {
-            label: false
-          },
-          tooltipConfig: {tbody: [["Year", d => d.Year], ["Age", d => rangeFormatter(d.Age)], ["Share", d => formatPercentage(d.share)]]}
-        }}
-        />
-      </SectionColumns>
+          {/* Lineplot to show total population over the years for selected geography. */}
+          <LinePlot config={{
+            data: unemploymentRate,
+            discrete: "x",
+            height: 300,
+            title: "Unemplyment Rate Over Years",
+            legend: false,
+            groupBy: "Geography",
+            x: "Year",
+            y: "Unemployment Rate",
+            yConfig: {tickFormat: d => formatPercentage(d)},
+            tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPercentage(d["Unemployment Rate"])]]}
+          }}
+          />
+        </SectionColumns>
+      </div>
     );
   }
 }
@@ -181,7 +196,8 @@ Demographics.need = [
   fetchData("unemploymentRate", "/api/data?measures=Unemployment Rate&Geography=<id>&Year=all", d => d.data),
   fetchData("lifeExpectancy", "/api/data?measures=Life Expectancy&Geography=<id>", d => d.data), // Year data not available
   fetchData("employmentStatus", "/api/data?measures=Population&drilldowns=Employment Status,Age,Sex&Geography=<id>&Year=all", d => d.data),
-  fetchData("workExperience", "/api/data?measures=Population&drilldowns=Work Experience,Sex&Geography=<id>&Year=all", d => d.data)
+  fetchData("workExperience", "/api/data?measures=Population&drilldowns=Work Experience,Sex&Geography=<id>&Year=all", d => d.data),
+  fetchData("socioeconomicRanking", "/api/data?measures=Socioeconomic Ranking&Geography=<id>&Year=latest", d => d.data)
 ];
 
 const mapStateToProps = state => ({
@@ -190,7 +206,8 @@ const mapStateToProps = state => ({
   unemploymentRate: state.data.unemploymentRate,
   lifeExpectancy: state.data.lifeExpectancy,
   employmentStatus: state.data.employmentStatus,
-  workExperience: state.data.workExperience
+  workExperience: state.data.workExperience,
+  socioeconomicRanking: state.data.socioeconomicRanking
 });
 
 export default connect(mapStateToProps)(Demographics);

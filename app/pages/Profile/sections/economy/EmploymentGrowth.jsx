9c7c21ad225dfa4f0e756a3fa3ecd2938a228ea7
Rashmi <baheti.rash123@gmail.com>
2019-01-03 10:10:10 -0500

    adds zip codes geomap for wayne county

--- app/pages/Profile/sections/economy/EmploymentGrowth.jsx
+++ app/pages/Profile/sections/economy/EmploymentGrowth.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import {connect} from "react-redux";
-// import {BarChart} from "d3plus-react";
-// import {formatAbbreviate} from "d3plus-format";
+import {Geomap} from "d3plus-react";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
 // import Stat from "../../../../components/Stat";
+import zipcodes from "../../../../utils/zipcodes";
 
 class EmploymentGrowth extends SectionColumns {
 
   render() {
 
     const {percentChangeInEmploymemt} = this.props;
-    console.log("percentChangeInEmploymemt: ", percentChangeInEmploymemt);
 
     return (
       <SectionColumns>
         <SectionTitle>Employment Growth</SectionTitle>
         <article>
         </article>
+
+        {/* Draw Geomap to show health center count for each zip code in the Wayne county */}
+        <Geomap config={{
+          data: percentChangeInEmploymemt,
+          groupBy: "Zip Code",
+          colorScale: "Percent Change in Employment",
+          colorScaleConfig: {
+            axisConfig: {tickFormat: d => `${d}%`}
+          },
+          label: d => d["Zip Code"],
+          height: 400,
+          time: "Year",
+          tooltipConfig: {tbody: [["Year", d => d.Year], ["Employment Growth", d => `${d["Percent Change in Employment"]}%`]]},
+          topojson: "/topojson/zipcodes.json",
+          topojsonFilter: d => zipcodes.includes(d.properties.ZCTA5CE10),
+          topojsonId: d => d.properties.ZCTA5CE10
+        }}
+        />
       </SectionColumns>
     );
   }

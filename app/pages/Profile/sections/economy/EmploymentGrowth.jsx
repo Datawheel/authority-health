c17bcdca7d660cdae7c0a5b9ea5365820623652a
Rashmi <baheti.rash123@gmail.com>
2019-01-03 10:49:29 -0500

    adds top stats and text

--- app/pages/Profile/sections/economy/EmploymentGrowth.jsx
+++ app/pages/Profile/sections/economy/EmploymentGrowth.jsx
@@ -4,7 +4,7 @@ import {Geomap} from "d3plus-react";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
-// import Stat from "../../../../components/Stat";
+import Stat from "../../../../components/Stat";
 import zipcodes from "../../../../utils/zipcodes";
 
 class EmploymentGrowth extends SectionColumns {
@@ -12,11 +12,21 @@ class EmploymentGrowth extends SectionColumns {
   render() {
 
     const {percentChangeInEmploymemt} = this.props;
+    const topEmploymentRateData = percentChangeInEmploymemt.sort((a, b) => b["Percent Change in Employment"] - a["Percent Change in Employment"])[0];
 
     return (
       <SectionColumns>
         <SectionTitle>Employment Growth</SectionTitle>
         <article>
+          <Stat
+            title={"Zip Code with the most employment growth"}
+            year={topEmploymentRateData.Year}
+            value={topEmploymentRateData["Zip Code"]}
+            qualifier={`${topEmploymentRateData["Percent Change in Employment"]}%`}
+          />
+
+          <p>In {topEmploymentRateData.Year}, the zip code in Wayne County with the most employment growth was {topEmploymentRateData["Zip Code"]} ({topEmploymentRateData["Percent Change in Employment"]}%).</p>
+          <p>The following map shows the employment growth for all zip codes in Wayne County.</p>
         </article>
 
         {/* Draw Geomap to show health center count for each zip code in the Wayne county */}

1286209d2015ba38cde28c58526aa0f0cf8ba3bf
Rashmi <baheti.rash123@gmail.com>
2019-01-07 15:02:56 -0500

    adds test for race and ethnicity data

--- app/pages/Profile/sections/about/Introduction.jsx
+++ app/pages/Profile/sections/about/Introduction.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import {nest} from "d3-collection";
-
+import {sum} from "d3-array";
 import {SectionColumns, SectionTitle} from "@datawheel/canon-core";
 import {formatAbbreviate} from "d3plus-format";
 import growthCalculator from "../../../../utils/growthCalculator";
 
-// import Stat from "../../../../components/Stat";
+const formatRaceName = d => d.replace("Alone", "");
+const formatEthnicityName = d => d.replace("Not Hispanic or Latino", "Non Hispanic");
 
 class Introduction extends SectionColumns {
 
   render() {
-    const {population, populationByAgeAndGender, lifeExpectancy} = this.props;
+    const {population, populationByAgeAndGender, populationByRaceAndEthnicity, lifeExpectancy} = this.props;
 
     const populationGrowth = formatAbbreviate(growthCalculator(population[0].Population, population[1].Population));
 
@@ -27,6 +28,18 @@ class Introduction extends SectionColumns {
     const getFemaleData = populationByAgeAndGender.filter(d => d.Sex === "Female");
     const getTopFemaleData = getFemaleData.sort((a, b) => b.Population - a.Population)[0];
 
+    // Get recent year race and ethnicity population data.
+    const recentYearPopulationByRaceAndEthnicity = {};
+    nest()
+      .key(d => d.Year)
+      .entries(populationByRaceAndEthnicity)
+      .forEach(group => {
+        const total = sum(group.values, d => d["Hispanic Population"]);
+        group.values.forEach(d => d.share = d["Hispanic Population"] / total * 100);
+        group.key >= populationByRaceAndEthnicity[0].Year ? Object.assign(recentYearPopulationByRaceAndEthnicity, group) : {};
+      });
+    recentYearPopulationByRaceAndEthnicity.values.sort((a, b) => b.share - a.share);
+
     return (
       <SectionColumns>
         <SectionTitle>Introduction</SectionTitle>
@@ -36,6 +49,15 @@ class Introduction extends SectionColumns {
             The most common age group for male is {getTopMaleData.Age.toLowerCase()} and for female it is {getTopFemaleData.Age.toLowerCase()}. 
             Between 2015 and 2016 the population of {population[0].Geography} {populationGrowth < 0 ? "reduced" : "increased"} from {formatAbbreviate(population[1].Population)} to {formatAbbreviate(population[0].Population)}, 
             a {populationGrowth < 0 ? "decline" : "increase"} of {populationGrowth < 0 ? populationGrowth * -1 : populationGrowth}%.
+          </p>
+        </article>
+
+        <article>
+          <p>
+            Most of the population in {recentYearPopulationByRaceAndEthnicity.values[0].Geography} is {formatEthnicityName(recentYearPopulationByRaceAndEthnicity.values[0].Ethnicity).toLowerCase()} { }
+            {formatRaceName(recentYearPopulationByRaceAndEthnicity.values[0].Race).toLowerCase()} ({formatAbbreviate(recentYearPopulationByRaceAndEthnicity.values[0].share)}%) and { }
+            {formatEthnicityName(recentYearPopulationByRaceAndEthnicity.values[1].Ethnicity).toLowerCase()} {formatRaceName(recentYearPopulationByRaceAndEthnicity.values[1].Race).toLowerCase()} 
+            ({formatAbbreviate(recentYearPopulationByRaceAndEthnicity.values[1].share)}%).
           </p>
         </article>
 

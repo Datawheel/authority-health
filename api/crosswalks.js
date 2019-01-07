52f15306a4f2813cee61644c65e072e35d473116
Dave Landry <landry.dave@gmail.com>
2019-01-07 17:02:31 -0500

    adds initial geography crosswalks and relations

---
+++ api/crosswalks.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+
+const {CANON_LOGICLAYER_CUBE} = process.env;
+
+const prefixMap = {
+  "050": "County",
+  "160": "Place",
+  "140": "Tract",
+  "860": "Zip"
+};
+
+module.exports = function(app) {
+
+  app.get("/api/geo/children/:id/", async(req, res) => {
+
+    const {id} = req.params;
+    const {level} = req.query;
+    const targetLevel = level ? level.replace(/^[A-Z]{1}/g, chr => chr.toLowerCase()) : false;
+
+    const prefix = prefixMap[id.slice(0, 3)];
+
+    if (level === "Region") {
+      res.json({cut: "05000US26163", drilldown: level});
+    }
+    else if (level === "Tract" && prefix === "County") {
+      res.json({cut: "05000US26163", drilldown: level});
+    }
+    else {
+      if (prefix === level) res.json({cut: "05000US26163", drilldown: level});
+      else {
+        const cuts = await axios.get(`${CANON_LOGICLAYER_CUBE}/geoservice-api/relations/intersects/${id}?targetLevels=${targetLevel}`)
+          .then(resp => resp.data)
+          .then(arr => arr.map(d => d.geoid));
+        res.json(cuts || []);
+      }
+    }
+
+  });
+
+};
--- canon.js
+++ canon.js
@@ -1,5 +1,42 @@
+const {CANON_API, CANON_LOGICLAYER_CUBE} = process.env;
+
 /**
     The object that this file exports is used to set configurations for canon
     and it's sub-modules.
 */
-module.exports = {};
+module.exports = {
+  logiclayer: {
+    relations: {
+      Geography: {
+        tracts: {
+          url: id => `${CANON_API}/api/geo/children/${id}/?level=Tract`
+        },
+        regions: {
+          url: id => `${CANON_API}/api/geo/children/${id}/?level=Region`
+        },
+        zips: {
+          url: id => `${CANON_API}/api/geo/children/${id}/?level=Zip`
+        }
+      }
+    },
+    substitutions: {
+      Geography: {
+        levels: {
+          County: ["County"],
+          Place: ["County"],
+          Tract: ["Zip", "Place", "County"],
+          Zip: ["Tract", "Place", "County"]
+        },
+        url: (id, level) => {
+          const targetLevel = level.replace(/^[A-Z]{1}/g, chr => chr.toLowerCase());
+          return `${CANON_LOGICLAYER_CUBE}/geoservice-api/relations/intersects/${id}?targetLevels=${targetLevel}&overlapSize=true`;
+        },
+        callback: arr => {
+          arr = arr.filter(d => d.level !== "county");
+          if (!arr.length) return "05000US26163";
+          return arr.sort((a, b) => b.overlap_size - a.overlap_size)[0].geoid;
+        }
+      }
+    }
+  }
+};

9932ecda3e91c662c141910c92b0fc5bcc61c614
Rashmi <baheti.rash123@gmail.com>
2019-01-07 13:17:07 -0500

    adds lineplot, top stats and short paragraph

--- app/pages/Profile/sections/economy/ConsumerPriceIndex.jsx
+++ app/pages/Profile/sections/economy/ConsumerPriceIndex.jsx
@@ -1,20 +1,81 @@
 import React from "react";
-// import {connect} from "react-redux";
-// import {BarChart} from "d3plus-react";
-// import {formatAbbreviate} from "d3plus-format";
+import {connect} from "react-redux";
+import {format} from "d3-format";
+import {LinePlot} from "d3plus-react";
+import {formatAbbreviate} from "d3plus-format";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
-// import Stat from "../../../../components/Stat";
+import styles from "style.yml";
+import Stat from "../../../../components/Stat";
+import growthCalculator from "../../../../utils/growthCalculator";
+
+const commas = format(".2f");
 
 class ConsumerPriceIndex extends SectionColumns {
 
   render() {
+
+    const {consumerPriceIndexData} = this.props;
+
+    const data = [];
+    consumerPriceIndexData.data.forEach(d => {
+      consumerPriceIndexData.source[0].measures.forEach(category => {
+        if (d[category] !== null) {
+          data.push(Object.assign({}, d, {Category: category}));
+        }
+      });
+    });
+
+    // Calculate the percentage growth CPI for current and the previous year.
+    const cpiGrowth = formatAbbreviate(growthCalculator(data[0]["Average CPI"], data[3]["Average CPI"]));
+
     return (
       <SectionColumns>
         <SectionTitle>Consumer Price Index</SectionTitle>
         <article>
+          <Stat
+            title="Average CPI"
+            year={data[0].Year}
+            value={formatAbbreviate(data[0]["Average CPI"])}
+          />
+          <Stat
+            title="Midwest CPI"
+            year={data[0].Year}
+            value={formatAbbreviate(data[0]["Average Midwest CPI"])}
+          />
+          <Stat
+            title="National CPI"
+            year={data[0].Year}
+            value={formatAbbreviate(data[0]["Average USA CPI"])}
+          />
+          <p>
+            In {data[0].Year}, the average CPI for Detroit-Warren-Dearborn, MI metro area was {formatAbbreviate(data[0]["Average CPI"])}, 
+            which was less than the midwest CPI ({formatAbbreviate(data[0]["Average Midwest CPI"])}) and also less than the national CPI ({formatAbbreviate(data[0]["Average USA CPI"])}).
+            Between {data[3].Year} and {data[0].Year} the average CPI of Detroit-Warren-Dearborn, MI metro area {cpiGrowth < 0 ? "reduced" : "increased"} from {formatAbbreviate(data[3]["Average CPI"])} to {formatAbbreviate(data[0]["Average CPI"])},
+            {cpiGrowth < 0 ? " a decline" : " an increase"} of {cpiGrowth < 0 ? cpiGrowth * -1 : cpiGrowth}%.
+          </p>
+          <p>The following chart shows the average consumer price index for USA, Midwest and Detroit-Warren-Dearborn, MI metro area.</p>
         </article>
+
+        {/* Create a LinePlot. */}
+        <LinePlot config={{
+          data,
+          discrete: "x",
+          height: 400,
+          legend: false,
+          groupBy: "Category",
+          x: "Year",
+          y: d => d[d.Category],
+          yConfig: {
+            tickFormat: d => formatAbbreviate(d)
+          },
+          shapeConfig: {
+            Line: {stroke: d => d.Category === "Average CPI" ? styles["majorelle-light"] : styles["majorelle-dark"]}
+          },
+          tooltipConfig: {tbody: [["Year", d => d.Year], ["Consumer Price Index", d => commas(d[d.Category])]]}
+        }}
+        />
       </SectionColumns>
     );
   }
@@ -24,4 +85,12 @@ ConsumerPriceIndex.defaultProps = {
   slug: "consumer-price-index"
 };
 
-export default ConsumerPriceIndex;
+ConsumerPriceIndex.need = [
+  fetchData("consumerPriceIndexData", "/api/data?measures=Average CPI,Average USA CPI,Average Midwest CPI&drilldowns=Year")
+];
+
+const mapStateToProps = state => ({
+  consumerPriceIndexData: state.data.consumerPriceIndexData
+});
+
+export default connect(mapStateToProps)(ConsumerPriceIndex);

7dccf2b84cfdfbe30959d51dca26efd15f9f7e3c
Rashmi <baheti.rash123@gmail.com>
2019-01-24 16:37:13 -0500

    updates code with new cube annotations
    - fetches latest data from need and passes all year data url to the geomap

--- app/pages/Profile/sections/accessToCare/HealthCenters.jsx
+++ app/pages/Profile/sections/accessToCare/HealthCenters.jsx
@@ -9,23 +9,13 @@ import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 import Stat from "../../../../components/Stat";
 import zipcodes from "../../../../utils/zipcodes";
 
-const formatName = d => {
-  const nameArr = d.split(" ");
-  return nameArr.reduce((acc, currValue, i) => i === 0 ? "" : acc.concat(currValue), "").trim();
-};
+const formatName = d => d.replace("Health Center Patients", "");
 
-const formatRaceNames = d => d.split(" ").slice(1).join();
+const formatRaceNames = d => d;
 
 const formatPercentage = d => `${formatAbbreviate(d * 100)}%`;
 
-const formatMeasureName = d => {
-  if (d === "Health Centers") return d;
-  const nameArr = d.split(" ");
-  return `${nameArr[2]} Population`;
-};
-
 class HealthCenters extends SectionColumns {
-
   constructor(props) {
     super(props);
     this.state = {dropdownValue: "Health Centers"};
@@ -40,14 +30,7 @@ class HealthCenters extends SectionColumns {
     const dropdownList = healthCenterData.source[0].measures;
 
     // Get the current dropdown value data for latest year.
-    const recentYearHealthCenterData = {};
-    nest()
-      .key(d => d.Year)
-      .entries(healthCenterData.data)
-      .forEach(group => {
-        group.key >= healthCenterData.data[0].Year ? Object.assign(recentYearHealthCenterData, group) : {};
-      });
-    const topRecentYearDropdownValueData = recentYearHealthCenterData.values.sort((a, b) => b[dropdownValue] - a[dropdownValue])[0];
+    const topRecentYearDropdownValueData = healthCenterData.data.sort((a, b) => b[dropdownValue] - a[dropdownValue])[0];
 
     // Get most recent year data for race and ethnicity.
     // Add RaceType property to raceAndEthnicityData so that each race type can have individual object.
@@ -61,9 +44,9 @@ class HealthCenters extends SectionColumns {
           const result = group.values.reduce((acc, currentValue) => {
             if (acc === null && currentValue[d] !== null) {
               // Non-white race population is the sum of all the race population minus White population.
-              if (d === "Percent Non-white") {
-                d = "Percent White";
-                currentValue[d] = 1 - currentValue["Percent Non-white"];
+              if (d === "Non-white Health Center Patients") {
+                d = "White Health Center Patients";
+                currentValue[d] = 1 - currentValue["Non-white Health Center Patients"];
                 return Object.assign({}, currentValue, {RaceType: d});
               }
               return Object.assign({}, currentValue, {RaceType: d});
@@ -96,14 +79,14 @@ class HealthCenters extends SectionColumns {
           {/* Create a dropdown list. */}
           <div className="pt-select">
             <select onChange={this.handleChange}>
-              {dropdownList.map(item => <option key={item} value={item}>{formatMeasureName(item)}</option>)}
+              {dropdownList.map(item => <option key={item} value={item}>{item}</option>)}
             </select>
           </div>
           {/* Show top stats for each dropdown choice. */}
           {isHealthCentersSelected
             ? <div>
               <Stat
-                title={`Zip Code with the most ${formatMeasureName(dropdownValue)}`}
+                title={`Zip Code with the most ${dropdownValue}`}
                 year={`${topRecentYearDropdownValueData.Year}`}
                 value={topRecentYearDropdownValueData.Zip}
                 qualifier={`${topRecentYearDropdownValueData[dropdownValue]} Health Centers`}
@@ -118,8 +101,8 @@ class HealthCenters extends SectionColumns {
                 value={topRecentYearDropdownValueData.Zip}
                 qualifier={formatPercentage(topRecentYearDropdownValueData[dropdownValue])}
               />
-              <p>In {topRecentYearDropdownValueData.Year}, the zip code in Wayne County with the most {formatMeasureName(dropdownValue).toLowerCase()} visiting health centers was {topRecentYearDropdownValueData.Zip} ({formatPercentage(topRecentYearDropdownValueData[dropdownValue])}).</p>
-              <p>The following map shows the share of {formatMeasureName(dropdownValue).toLowerCase()} visiting health centers for all zip codes in Wayne County, MI.</p>
+              <p>In {topRecentYearDropdownValueData.Year}, the zip code in Wayne County with the most {dropdownValue.toLowerCase()} visiting health centers was {topRecentYearDropdownValueData.Zip} ({formatPercentage(topRecentYearDropdownValueData[dropdownValue])}).</p>
+              <p>The following map shows the share of {dropdownValue.toLowerCase()} visiting health centers for all zip codes in Wayne County, MI.</p>
             </div>
           }
 
@@ -149,8 +132,8 @@ class HealthCenters extends SectionColumns {
 
         {/* Draw Geomap to show health center count for each zip code in the Wayne county */}
         <Geomap config={{
-          data: healthCenterData.data,
-          groupBy: "Zip",
+          data: "/api/data?measures=Health Centers,Health Center Penetration,Low-Income Health Center Penetration,Uninsured Health Center Penetration&drilldowns=Zip&Year=all",
+          groupBy: d => d["ID Zip"].slice(7),
           colorScale: dropdownValue,
           colorScaleConfig: {
             axisConfig: {tickFormat: isHealthCentersSelected ? d => d : d => formatPercentage(d)}
@@ -158,11 +141,12 @@ class HealthCenters extends SectionColumns {
           label: d => d.Zip,
           height: 400,
           time: "Year",
-          tooltipConfig: isHealthCentersSelected ? {tbody: [["Year", d => d.Year], ["Health Centers", d => d[dropdownValue]]]} : {tbody: [["Year", d => d.Year], ["Visitor", formatMeasureName(dropdownValue)], ["Share", d => formatPercentage(d[dropdownValue])]]},
+          tooltipConfig: isHealthCentersSelected ? {tbody: [["Year", d => d.Year], ["Health Centers", d => d[dropdownValue]]]} : {tbody: [["Year", d => d.Year], ["Visitor", dropdownValue], ["Share", d => formatPercentage(d[dropdownValue])]]},
           topojson: "/topojson/zipcodes.json",
           topojsonFilter: d => zipcodes.includes(d.properties.ZCTA5CE10),
           topojsonId: d => d.properties.ZCTA5CE10
         }}
+        dataFormat={resp => resp.data}
         />
       </SectionColumns>
     );
@@ -174,8 +158,8 @@ HealthCenters.defaultProps = {
 };
 
 HealthCenters.need = [
-  fetchData("healthCenterData", "/api/data?measures=Health Centers,Penetration of Total Population,Penetration of Low-Income,Penetration of Uninsured Population&drilldowns=Zip&Year=all"),
-  fetchData("raceAndEthnicityData", "/api/data?measures=Percent Non-white,Percent Hispanic,Percent Black,Percent Asian,Percent American Indian/Alaska Native&Year=all")
+  fetchData("healthCenterData", "/api/data?measures=Health Centers,Health Center Penetration,Low-Income Health Center Penetration,Uninsured Health Center Penetration&drilldowns=Zip&Year=latest"),
+  fetchData("raceAndEthnicityData", "/api/data?measures=Non-white Health Center Patients,Hispanic Health Center Patients,Black Health Center Patients,Asian Health Center Patients,American Indian/Alaska Native Health Center Patients&Year=all")
 ];
 
 const mapStateToProps = state => ({

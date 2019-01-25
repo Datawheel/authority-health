981acdb8126a7224c5f845b5f32e7187ee3d8478
Rashmi <baheti.rash123@gmail.com>
2019-01-25 17:07:53 -0500

    handles data for any tract/zip level that might not be available

--- app/pages/Profile/sections/builtSocialEnvironment/Rentals.jsx
+++ app/pages/Profile/sections/builtSocialEnvironment/Rentals.jsx
@@ -18,23 +18,33 @@ class Rentals extends SectionColumns {
   render() {
 
     const {rentAmountData, utilitiesData, rentersByIncomePercentage} = this.props;
-    const totalUtilitiesData = utilitiesData[0]["Renter-Occupied Housing Units"] + utilitiesData[1]["Renter-Occupied Housing Units"];
-    const recentYearNoExtraUtilitiesPercentage = utilitiesData[1]["Renter-Occupied Housing Units"] / totalUtilitiesData * 100;
+
+    const rentAmountDataAvailable = rentAmountData.length !== 0;
+    const utilitiesDataAvailable = utilitiesData.length !== 0;
+    const rentersByIncomePercentageAvailable = rentersByIncomePercentage.length !== 0;
+
+    let recentYearNoExtraUtilitiesPercentage; 
+    if (utilitiesDataAvailable) {
+      const totalUtilitiesData = utilitiesData[0]["Renter-Occupied Housing Units"] + utilitiesData[1]["Renter-Occupied Housing Units"];
+      recentYearNoExtraUtilitiesPercentage = utilitiesData[1]["Renter-Occupied Housing Units"] / totalUtilitiesData * 100;
+    }
 
     const recentYearRentersByIncomePercentage = {};
-    nest()
-      .key(d => d.Year)
-      .entries(rentersByIncomePercentage)
-      .forEach(group => {
-        const total = sum(group.values, d => d["Renters by Income Percentage"]);
-        group.values.forEach(d => d.share = d["Renters by Income Percentage"] / total * 100);
-        group.key >= rentersByIncomePercentage[0].Year ? Object.assign(recentYearRentersByIncomePercentage, group) : {};
-      });
+    let topIncomeToPayMostRent;
+    if (rentersByIncomePercentageAvailable) {
+      nest()
+        .key(d => d.Year)
+        .entries(rentersByIncomePercentage)
+        .forEach(group => {
+          const total = sum(group.values, d => d["Renters by Income Percentage"]);
+          group.values.forEach(d => d.share = d["Renters by Income Percentage"] / total * 100);
+          group.key >= rentersByIncomePercentage[0].Year ? Object.assign(recentYearRentersByIncomePercentage, group) : {};
+        });
+      topIncomeToPayMostRent = recentYearRentersByIncomePercentage.values.sort((a, b) => b.share - a.share)[0];
+    }
 
-    let topIncomeToPayMostRent = recentYearRentersByIncomePercentage.values.sort((a, b) => b.share - a.share);
-    topIncomeToPayMostRent = topIncomeToPayMostRent[0];
-
-    const growthRate = growthCalculator(rentAmountData[0]["Rent Amount"], rentAmountData[1]["Rent Amount"]);
+    let growthRate;
+    if (rentAmountDataAvailable) growthRate = growthCalculator(rentAmountData[0]["Rent Amount"], rentAmountData[1]["Rent Amount"]);
 
     return (
       <SectionColumns>
@@ -43,59 +53,61 @@ class Rentals extends SectionColumns {
           {/* Show recent Year Rent amount. */}
           <Stat
             title="Median Rent"
-            year={rentAmountData[0].Year}
-            value={`$${formatAbbreviate(rentAmountData[0]["Rent Amount"])}/month`}
+            year={rentAmountDataAvailable ? rentAmountData[0].Year : ""}
+            value={rentAmountDataAvailable ? `$${formatAbbreviate(rentAmountData[0]["Rent Amount"])}/month` : "N/A"}
           />
           {/* Show stats for Renter-Occupied Housing Units with Extra Pay on Utilities for most recent year. */}
           <Stat
             title="Units with utilities included"
-            year={utilitiesData[0].Year}
-            value={`${formatPercentage(recentYearNoExtraUtilitiesPercentage)}`}
+            year={utilitiesDataAvailable ? utilitiesData[0].Year : ""}
+            value={utilitiesDataAvailable ? `${formatPercentage(recentYearNoExtraUtilitiesPercentage)}` : "N/A"}
           />
 
-          <p>In {rentAmountData[0].Year}, the median price for a rental unit in {rentAmountData[0].Geography} was ${formatAbbreviate(rentAmountData[0]["Rent Amount"])}/month. This is a {growthRate < 0 ? formatPercentage(growthRate * -1) : formatPercentage(growthRate)} {growthRate < 0 ? "decline" : "increase"} from the previous year (${formatAbbreviate(rentAmountData[1]["Rent Amount"])}/month).</p>
-          <p>{formatPercentage(recentYearNoExtraUtilitiesPercentage)} of the rental properties in {utilitiesData[0].Geography} include utilities with the price of rent.</p>
-          <p>The average income bracket for renters in {topIncomeToPayMostRent.Geography} is {rangeFormatter(topIncomeToPayMostRent["Household Income"])} and the following bar chart shows the renter distribution across all income levels.</p>
+          <p>{rentAmountDataAvailable ? <span>In {rentAmountData[0].Year}, the median price for a rental unit in {rentAmountData[0].Geography} was ${formatAbbreviate(rentAmountData[0]["Rent Amount"])}/month. This is a {growthRate < 0 ? formatPercentage(growthRate * -1) : formatPercentage(growthRate)} {growthRate < 0 ? "decline" : "increase"} from the previous year (${formatAbbreviate(rentAmountData[1]["Rent Amount"])}/month).</span> : ""}</p>
+          <p>{utilitiesDataAvailable ? <span>{formatPercentage(recentYearNoExtraUtilitiesPercentage)} of the rental properties in {utilitiesData[0].Geography} include utilities with the price of rent.</span> : ""}</p>
+          <p>{rentersByIncomePercentageAvailable ? <span> The average income bracket for renters in {topIncomeToPayMostRent.Geography} is {rangeFormatter(topIncomeToPayMostRent["Household Income"])} and the following bar chart shows the renter distribution across all income levels.</span> : ""}</p>
           
           {/* Create a LinePlot. */}
-          <LinePlot config={{
-            data: rentAmountData,
-            discrete: "x",
-            height: 175,
-            groupBy: "Geography",
-            x: "Year",
-            y: "Rent Amount",
-            yConfig: {
-              tickFormat: d => `$${formatAbbreviate(d)}`,
-              title: "Rent Per Month"
-            },
-            tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => `$${formatAbbreviate(d["Rent Amount"])}`]]}
-          }}
-          />
+          {rentAmountDataAvailable
+            ? <LinePlot config={{
+              data: rentAmountData,
+              discrete: "x",
+              height: 175,
+              groupBy: "Geography",
+              x: "Year",
+              y: "Rent Amount",
+              yConfig: {
+                tickFormat: d => `$${formatAbbreviate(d)}`,
+                title: "Rent Per Month"
+              },
+              tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => `$${formatAbbreviate(d["Rent Amount"])}`]]}
+            }}
+            /> : <div></div>}
         </article>
 
-        <BarChart config={{
-          data: rentersByIncomePercentage,
-          discrete: "x",
-          height: 400,
-          legend: false,
-          groupBy: "Household Income",
-          x: d => d["Household Income"],
-          y: "share",
-          time: "Year",
-          xSort: (a, b) => a["ID Household Income"] - b["ID Household Income"],
-          xConfig: {
-            labelRotation: false,
-            tickFormat: d => rangeFormatter(d),
-            title: "Household Income"
-          },
-          yConfig: {
-            tickFormat: d => formatPercentage(d),
-            title: "Share of Renters"},
-          shapeConfig: {label: false},
-          tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPercentage(d.share)], ["Location", d => d.Geography]]}
-        }}
-        />
+        {rentersByIncomePercentageAvailable 
+          ? <BarChart config={{
+            data: rentersByIncomePercentage,
+            discrete: "x",
+            height: 400,
+            legend: false,
+            groupBy: "Household Income",
+            x: d => d["Household Income"],
+            y: "share",
+            time: "Year",
+            xSort: (a, b) => a["ID Household Income"] - b["ID Household Income"],
+            xConfig: {
+              labelRotation: false,
+              tickFormat: d => rangeFormatter(d),
+              title: "Household Income"
+            },
+            yConfig: {
+              tickFormat: d => formatPercentage(d),
+              title: "Share of Renters"},
+            shapeConfig: {label: false},
+            tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPercentage(d.share)], ["Location", d => d.Geography]]}
+          }}
+          /> : <div></div>}
       </SectionColumns>
     );
   }

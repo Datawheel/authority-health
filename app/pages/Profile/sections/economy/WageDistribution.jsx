e9b571f1ca38dfdbe34afeb5c637b692d2f2523d
Rashmi <baheti.rash123@gmail.com>
2019-01-28 16:42:48 -0500

    Wage Distribution component: handles data for any tract/zip level that might not be available

--- app/pages/Profile/sections/economy/WageDistribution.jsx
+++ app/pages/Profile/sections/economy/WageDistribution.jsx
@@ -18,13 +18,18 @@ class WageDistribution extends SectionColumns {
 
     const {wageDistributionData, wageGinidata} = this.props;
 
-    nest()
-      .key(d => d.Year)
-      .entries(wageDistributionData)
-      .forEach(group => {
-        const total = sum(group.values, d => d["Household Income"]);
-        group.values.forEach(d => d.share = d["Household Income"] / total * 100);
-      });
+    const wageDistributionDataAvailable = wageDistributionData.length !== 0;
+    const wageGinidataAvailable = wageGinidata.length !== 0;
+
+    if (wageDistributionDataAvailable) {
+      nest()
+        .key(d => d.Year)
+        .entries(wageDistributionData)
+        .forEach(group => {
+          const total = sum(group.values, d => d["Household Income"]);
+          group.values.forEach(d => d.share = d["Household Income"] / total * 100);
+        });
+    }
 
     return (
       <SectionColumns>
@@ -33,38 +38,39 @@ class WageDistribution extends SectionColumns {
           {/* Top stats and short paragraph about Wage Gini. */}
           <Stat
             title="Wage GINI"
-            year={wageGinidata[0].Year}
-            value={wageGinidata[0]["Wage GINI"]}
+            year={wageGinidataAvailable ? wageGinidata[0].Year : ""}
+            value={wageGinidataAvailable ? wageGinidata[0]["Wage GINI"] : "N/A"}
           />
-          <p>In {wageGinidata[0].Year}, the income inequality in {wageGinidata[0].Geography} was {wageGinidata[0]["Wage GINI"]}. The GINI coefficient is a measure of statistical dispersion intended to represent the equality of a distribution, and is the most commonly used measure of inequality. Values range from 0 to 1, with 0 being perfect equality.</p>
-          <p>The following chart shows the household income bucket and share for each bucket in {wageDistributionData[0].Geography}.</p>
+          {wageGinidataAvailable ? <p>In {wageGinidata[0].Year}, the income inequality in {wageGinidata[0].Geography} was {wageGinidata[0]["Wage GINI"]}. The GINI coefficient is a measure of statistical dispersion intended to represent the equality of a distribution, and is the most commonly used measure of inequality. Values range from 0 to 1, with 0 being perfect equality.</p> : ""}
+          {wageDistributionDataAvailable ? <p>The following chart shows the household income bucket and share for each bucket in {wageDistributionData[0].Geography}.</p> : ""}
         </article>
 
         {/* Draw Geomap to show wage distribution for each place in the Wayne county. */}
-        <BarChart config={{
-          data: wageDistributionData,
-          discrete: "x",
-          height: 400,
-          legend: false,
-          groupBy: "Household Income Bucket",
-          x: "Household Income Bucket",
-          y: "share",
-          time: "Year",
-          xSort: (a, b) => a["ID Household Income Bucket"] - b["ID Household Income Bucket"],
-          xConfig: {
-            tickFormat: d => rangeFormatter(d),
-            title: "Household Income Bucket"
-          },
-          yConfig: {
-            tickFormat: d => formatPopulation(d),
-            title: "Share"
-          },
-          shapeConfig: {
-            label: false
-          },
-          tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPopulation(d.share)], ["Location", d => d.Geography]]}
-        }}
-        />
+        {wageDistributionDataAvailable
+          ? <BarChart config={{
+            data: wageDistributionData,
+            discrete: "x",
+            height: 400,
+            legend: false,
+            groupBy: "Household Income Bucket",
+            x: "Household Income Bucket",
+            y: "share",
+            time: "Year",
+            xSort: (a, b) => a["ID Household Income Bucket"] - b["ID Household Income Bucket"],
+            xConfig: {
+              tickFormat: d => rangeFormatter(d),
+              title: "Household Income Bucket"
+            },
+            yConfig: {
+              tickFormat: d => formatPopulation(d),
+              title: "Share"
+            },
+            shapeConfig: {
+              label: false
+            },
+            tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPopulation(d.share)], ["Location", d => d.Geography]]}
+          }}
+          /> : <div></div>}
       </SectionColumns>
     );
   }
@@ -75,8 +81,8 @@ WageDistribution.defaultProps = {
 };
 
 WageDistribution.need = [
-  fetchData("wageDistributionData", "https://acs.datausa.io/api/data?measures=Household Income&drilldowns=Household Income Bucket&Geography=<id>&Year=all", d => d.data),
-  fetchData("wageGinidata", "https://acs.datausa.io/api/data?measures=Wage GINI&Geography=<id>&Year=all", d => d.data)
+  fetchData("wageDistributionData", "https://quincy.datausa.io/api/data?measures=Household Income&drilldowns=Household Income Bucket&Geography=<id>&Year=all", d => d.data),
+  fetchData("wageGinidata", "https://quincy.datausa.io/api/data?measures=Wage GINI&Geography=<id>&Year=all", d => d.data)
 ];
 
 const mapStateToProps = state => ({

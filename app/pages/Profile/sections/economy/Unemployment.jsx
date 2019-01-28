9334088b984c7de584eaedec04310cdab41ebc4f
Rashmi <baheti.rash123@gmail.com>
2019-01-28 15:39:33 -0500

    Unemployment Rate: handles data for any tract/zip level that might not be available

--- app/pages/Profile/sections/economy/Unemployment.jsx
+++ app/pages/Profile/sections/economy/Unemployment.jsx
@@ -17,35 +17,44 @@ class Unemployment extends SectionColumns {
   render() {
 
     const {employmentStatus, workExperience, unemploymentRate} = this.props;
+    
+    const employmentStatusAvailable = employmentStatus.length !== 0;
+    const workExperienceAvailable = workExperience.length !== 0;
     const isUnemploymentRateAvailableForCurrentLocation = unemploymentRate.source[0].substitutions.length === 0;
 
     // Find full-time work share for male and female population.
-    const filteredWorkExperienceData = workExperience.filter(d => d["Work Experience"] === "Worked Full Time, Year-round");
-    nest()
-      .key(d => d.Year)
-      .entries(filteredWorkExperienceData)
-      .forEach(group => {
-        const total = sum(group.values, d => d["Poverty by Work Experience"]);
-        group.values.forEach(d => d.share = d["Poverty by Work Experience"] / total * 100);
-      });
-    const getMaleFullTimeData = filteredWorkExperienceData.filter(d => d.Sex === "Male");
-    const getFemaleFullTimeData = filteredWorkExperienceData.filter(d => d.Sex === "Female");
+    let getFemaleFullTimeData, getMaleFullTimeData;
+    if (workExperienceAvailable) {
+      const filteredWorkExperienceData = workExperience.filter(d => d["Work Experience"] === "Worked Full Time, Year-round");
+      nest()
+        .key(d => d.Year)
+        .entries(filteredWorkExperienceData)
+        .forEach(group => {
+          const total = sum(group.values, d => d["Poverty by Work Experience"]);
+          group.values.forEach(d => d.share = d["Poverty by Work Experience"] / total * 100);
+        });
+      getMaleFullTimeData = filteredWorkExperienceData.filter(d => d.Sex === "Male");
+      getFemaleFullTimeData = filteredWorkExperienceData.filter(d => d.Sex === "Female");
+    }
 
     // Find share for employmentStatus.
-    const unemployedData = employmentStatus.filter(d => d["Employment Status"] === "Unemployed");
     const recentYearUnemploymentData = {};
-    nest()
-      .key(d => d.Year)
-      .entries(unemployedData)
-      .forEach(group => {
-        const total = sum(group.values, d => d["Population by Employment Status"]);
-        group.values.forEach(d => d.share = d["Population by Employment Status"] / total * 100);
-        group.key >= unemployedData[0].Year ? Object.assign(recentYearUnemploymentData, group) : {};
-      });
-    const getMaleUnemploymemtData = recentYearUnemploymentData.values.filter(d => d.Sex === "Male");
-    const getTopMaleUnemploymemtData = getMaleUnemploymemtData.sort((a, b) => b.share - a.share)[0];
-    const getFemaleUnemploymemtData = recentYearUnemploymentData.values.filter(d => d.Sex === "Female");
-    const getTopFemaleUnemploymemtData = getFemaleUnemploymemtData.sort((a, b) => b.share - a.share)[0];
+    let getTopFemaleUnemploymemtData, getTopMaleUnemploymemtData, unemployedData;
+    if (employmentStatusAvailable) {
+      unemployedData = employmentStatus.filter(d => d["Employment Status"] === "Unemployed");
+      nest()
+        .key(d => d.Year)
+        .entries(unemployedData)
+        .forEach(group => {
+          const total = sum(group.values, d => d["Population by Employment Status"]);
+          group.values.forEach(d => d.share = d["Population by Employment Status"] / total * 100);
+          group.key >= unemployedData[0].Year ? Object.assign(recentYearUnemploymentData, group) : {};
+        });
+      const getMaleUnemploymemtData = recentYearUnemploymentData.values.filter(d => d.Sex === "Male");
+      getTopMaleUnemploymemtData = getMaleUnemploymemtData.sort((a, b) => b.share - a.share)[0];
+      const getFemaleUnemploymemtData = recentYearUnemploymentData.values.filter(d => d.Sex === "Female");
+      getTopFemaleUnemploymemtData = getFemaleUnemploymemtData.sort((a, b) => b.share - a.share)[0];
+    }
 
     const recentYearUnemploymentRate = {};
     nest()
@@ -62,45 +71,46 @@ class Unemployment extends SectionColumns {
           {isUnemploymentRateAvailableForCurrentLocation ? <div></div> : <div className="disclaimer">Showing unemployment rate data for {unemploymentRate.data[0].Geography}.</div>}
           <Stat
             title="Male Working Full Time"
-            year={getMaleFullTimeData[0].Year}
-            value={formatPercentage(getMaleFullTimeData[0].share)}
+            year={workExperienceAvailable ? getMaleFullTimeData[0].Year : ""}
+            value={workExperienceAvailable ? formatPercentage(getMaleFullTimeData[0].share) : "N/A"}
           />
           <Stat
             title="Female Working Full Time"
-            year={getFemaleFullTimeData[0].Year}
-            value={formatPercentage(getFemaleFullTimeData[0].share)}
+            year={workExperienceAvailable ? getFemaleFullTimeData[0].Year : ""}
+            value={workExperienceAvailable ? formatPercentage(getFemaleFullTimeData[0].share) : "N/A"}
           />
 
           <p>
-            In {getMaleFullTimeData[0].Year}, the percentage of the working population in {getMaleFullTimeData[0].Geography} that worked full-time for men and women was {formatPercentage(getMaleFullTimeData[0].share)} and {formatPercentage(getFemaleFullTimeData[0].share)}, respectively.
-            The most common unemployed age group for men was {getTopMaleUnemploymemtData.Age.toLowerCase()} ({formatPercentage(getTopMaleUnemploymemtData.share)}), and the most common female unemployed age group for women was {getTopFemaleUnemploymemtData.Age.toLowerCase()} ({formatPercentage(getTopFemaleUnemploymemtData.share)}).  
+            {workExperienceAvailable ? <span>In {getMaleFullTimeData[0].Year}, the percentage of the working population in {getMaleFullTimeData[0].Geography} that worked full-time for men and women was {formatPercentage(getMaleFullTimeData[0].share)} and {formatPercentage(getFemaleFullTimeData[0].share)}, respectively.</span> : ""}
+            {employmentStatusAvailable ? <span> The most common unemployed age group for men was {getTopMaleUnemploymemtData.Age.toLowerCase()} ({formatPercentage(getTopMaleUnemploymemtData.share)}), and the most common female unemployed age group for women was {getTopFemaleUnemploymemtData.Age.toLowerCase()} ({formatPercentage(getTopFemaleUnemploymemtData.share)}).</span> : ""}
             In {recentYearUnemploymentRate.values[0].Year}, the overall unemploymemt rate in {recentYearUnemploymentRate.values[0].Geography} was {formatPercentage(recentYearUnemploymentRate.values[0]["Unemployment Rate"])}.
           </p>
-          <p>The following charts show the unemployment rate over time both overall and by age and gender.</p>
+          <p>The following charts show the unemployment rate over time both{workExperienceAvailable ? <span> overall and by age and gender.</span> : "."}</p>
           
           {/* Barchart to show population by age and gender over the years for selected geography. */}
-          <BarChart config={{
-            data: unemployedData,
-            discrete: "x",
-            height: 300,
-            groupBy: ["Employment Status", "Sex"],
-            x: "Age",
-            y: "share",
-            time: "Year",
-            xSort: (a, b) => a["ID Age"] - b["ID Age"],
-            xConfig: {
-              tickFormat: d => rangeFormatter(d)
-            },
-            yConfig: {
-              tickFormat: d => formatPercentage(d),
-              title: "Share"
-            },
-            shapeConfig: {
-              label: false
-            },
-            tooltipConfig: {tbody: [["Year", d => d.Year], ["Age", d => rangeFormatter(d.Age)], ["Share", d => formatPercentage(d.share)], ["Location", d => d.Geography]]}
-          }}
-          />
+          {workExperienceAvailable 
+            ? <BarChart config={{
+              data: unemployedData,
+              discrete: "x",
+              height: 300,
+              groupBy: ["Employment Status", "Sex"],
+              x: "Age",
+              y: "share",
+              time: "Year",
+              xSort: (a, b) => a["ID Age"] - b["ID Age"],
+              xConfig: {
+                tickFormat: d => rangeFormatter(d)
+              },
+              yConfig: {
+                tickFormat: d => formatPercentage(d),
+                title: "Share"
+              },
+              shapeConfig: {
+                label: false
+              },
+              tooltipConfig: {tbody: [["Year", d => d.Year], ["Age", d => rangeFormatter(d.Age)], ["Share", d => formatPercentage(d.share)], ["Location", d => d.Geography]]}
+            }}
+            /> : <div></div>}
         </article>
 
         {/* Lineplot to show total population over the years for selected geography. */}

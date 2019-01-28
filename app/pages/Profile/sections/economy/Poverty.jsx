b3565bdf32a3a17d40a2d7be3baaa8f85db23b0a
Rashmi <baheti.rash123@gmail.com>
2019-01-28 15:08:13 -0500

    handles data for any tract/zip level that might not be available

--- app/pages/Profile/sections/economy/Poverty.jsx
+++ app/pages/Profile/sections/economy/Poverty.jsx
@@ -18,46 +18,47 @@ class Poverty extends SectionColumns {
 
     const {povertyByRace, povertyByAgeAndGender} = this.props;
 
-    const filterOutTotalRaceData = povertyByRace.filter(d => d.Race !== "Total");
+    const povertyByRaceAvailable = povertyByRace.length !== 0;
+    const povertyByAgeAndGenderAvailable = povertyByAgeAndGender.length !== 0;
 
     // Get the Poverty by Race data.
     const recentYearPovertyByRaceData = {};
-    nest()
-      .key(d => d.Year)
-      .entries(filterOutTotalRaceData)
-      .forEach(group => {
-        const total = sum(group.values, d => d["Poverty Population"]);
-        group.values.forEach(d => d.share = d["Poverty Population"] / total * 100);
-        group.key >= filterOutTotalRaceData[0].Year ? Object.assign(recentYearPovertyByRaceData, group) : {};
-      });
-    const filterDataBelowPovertyByRace = filterOutTotalRaceData.filter(d => d["ID Poverty Status"] === 0);
-
-    // Find top stats for Poverty by Race
-    const filterRecentYearPovertyByRace = recentYearPovertyByRaceData.values.filter(d => d["ID Poverty Status"] === 0).sort((a, b) => b.share - a.share);
-    const topPovertyByRace = filterRecentYearPovertyByRace[0];
+    let filterDataBelowPovertyByRace, topPovertyByRace;
+    if (povertyByRaceAvailable) {
+      const filterOutTotalRaceData = povertyByRace.filter(d => d.Race !== "Total");
+      nest()
+        .key(d => d.Year)
+        .entries(filterOutTotalRaceData)
+        .forEach(group => {
+          const total = sum(group.values, d => d["Poverty Population"]);
+          group.values.forEach(d => d.share = d["Poverty Population"] / total * 100);
+          group.key >= filterOutTotalRaceData[0].Year ? Object.assign(recentYearPovertyByRaceData, group) : {};
+        });
+      filterDataBelowPovertyByRace = filterOutTotalRaceData.filter(d => d["ID Poverty Status"] === 0);
+      // Find top stats for Poverty by Race
+      topPovertyByRace = recentYearPovertyByRaceData.values.filter(d => d["ID Poverty Status"] === 0).sort((a, b) => b.share - a.share)[0];
+    }
 
     // Get data for Poverty by Age and Gender.
-    const belowPovertyLevelByAgeAndGender = povertyByAgeAndGender.filter(d => d["ID Poverty Status"] === 0);
+    let belowPovertyLevelByAgeAndGender, topFemalePovertyData, topMalePovertyData;
     const recentYearPovertyByAgeAndGender = {};
-    nest()
-      .key(d => d.Year)
-      .entries(belowPovertyLevelByAgeAndGender)
-      .forEach(group => {
-        const total = sum(group.values, d => d["Poverty Population"]);
-        group.values.forEach(d => d.share = d["Poverty Population"] / total * 100);
-        group.key >= belowPovertyLevelByAgeAndGender[0].Year ? Object.assign(recentYearPovertyByAgeAndGender, group) : {};
-      });
-
-    // Find top stats for povetry by Age and Gender.
-    const recentYearPovertyByAgeAndGenderFiltered = recentYearPovertyByAgeAndGender.values.filter(d => d["ID Poverty Status"] === 0);
-
-    // Find top male poverty data.
-    const malePovertyData = recentYearPovertyByAgeAndGenderFiltered.filter(d => d.Gender === "Male").sort((a, b) => b.share - a.share);
-    const topMalePovertyData = malePovertyData[0];
-
-    // Find top female poverty data.
-    const femalePovertyData = recentYearPovertyByAgeAndGenderFiltered.filter(d => d.Gender === "Female").sort((a, b) => b.share - a.share);
-    const topFemalePovertyData = femalePovertyData[0];
+    if (povertyByAgeAndGenderAvailable) {
+      belowPovertyLevelByAgeAndGender = povertyByAgeAndGender.filter(d => d["ID Poverty Status"] === 0);
+      nest()
+        .key(d => d.Year)
+        .entries(belowPovertyLevelByAgeAndGender)
+        .forEach(group => {
+          const total = sum(group.values, d => d["Poverty Population"]);
+          group.values.forEach(d => d.share = d["Poverty Population"] / total * 100);
+          group.key >= belowPovertyLevelByAgeAndGender[0].Year ? Object.assign(recentYearPovertyByAgeAndGender, group) : {};
+        });
+      // Find top stats for povetry by Age and Gender.
+      const recentYearPovertyByAgeAndGenderFiltered = recentYearPovertyByAgeAndGender.values.filter(d => d["ID Poverty Status"] === 0);
+      // Find top male poverty data.
+      topMalePovertyData = recentYearPovertyByAgeAndGenderFiltered.filter(d => d.Gender === "Male").sort((a, b) => b.share - a.share)[0];
+      // Find top female poverty data.
+      topFemalePovertyData = recentYearPovertyByAgeAndGenderFiltered.filter(d => d.Gender === "Female").sort((a, b) => b.share - a.share)[0];
+    }
 
     return (
       <SectionColumns>
@@ -65,71 +66,73 @@ class Poverty extends SectionColumns {
         <article>
           <Stat
             title="Most common race"
-            year={topPovertyByRace.Year}
-            value={topPovertyByRace.Race}
-            qualifier={formatPopulation(topPovertyByRace.share)}
+            year={povertyByRaceAvailable ? topPovertyByRace.Year : ""}
+            value={povertyByRaceAvailable ? topPovertyByRace.Race : "N/A"}
+            qualifier={povertyByRaceAvailable ? formatPopulation(topPovertyByRace.share) : ""}
           />
           <Stat
             title="Most common male age"
-            year={topMalePovertyData.Year}
-            value={topMalePovertyData.Age}
-            qualifier={formatPopulation(topMalePovertyData.share)}
+            year={povertyByAgeAndGenderAvailable ? topMalePovertyData.Year : ""}
+            value={povertyByAgeAndGenderAvailable ? topMalePovertyData.Age : "N/A"}
+            qualifier={povertyByAgeAndGenderAvailable ? formatPopulation(topMalePovertyData.share) : ""}
           />
           <Stat
             title="Most common female age"
-            year={topFemalePovertyData.Year}
-            value={topFemalePovertyData.Age}
-            qualifier={formatPopulation(topFemalePovertyData.share)}
+            year={povertyByAgeAndGenderAvailable ? topFemalePovertyData.Year : ""}
+            value={povertyByAgeAndGenderAvailable ? topFemalePovertyData.Age : "N/A"}
+            qualifier={povertyByAgeAndGenderAvailable ? formatPopulation(topFemalePovertyData.share) : ""}
           />
-          <p>In {topPovertyByRace.Year}, most common male age in poverty was {topMalePovertyData.Age.toLowerCase()} ({formatPopulation(topMalePovertyData.share)}) while most common female age was {topFemalePovertyData.Age.toLowerCase()} ({formatPopulation(topFemalePovertyData.share)}) in {topFemalePovertyData.Geography}. The chart on the right shows male and female age distribution in poverty.</p>
-          <p>In {topPovertyByRace.Year}, the majority race in poverty was {topPovertyByRace.Race} ({formatPopulation(topPovertyByRace.share)}) of the total population in {topPovertyByRace.Geography}. The following chart shows the population in poverty by race.</p>
+          {povertyByAgeAndGenderAvailable ? <p>In {topMalePovertyData.Year}, most common male age in poverty was {topMalePovertyData.Age.toLowerCase()} ({formatPopulation(topMalePovertyData.share)}) while most common female age was {topFemalePovertyData.Age.toLowerCase()} ({formatPopulation(topFemalePovertyData.share)}) in {topFemalePovertyData.Geography}. The chart on the right shows male and female age distribution in poverty. </p> : ""}
+          {povertyByRaceAvailable ? <p>In {topPovertyByRace.Year}, the majority race in poverty was {topPovertyByRace.Race} ({formatPopulation(topPovertyByRace.share)}) of the total population in {topPovertyByRace.Geography}. The following chart shows the population in poverty by race.</p> : ""}
 
-          <BarChart config={{
-            data: filterDataBelowPovertyByRace,
-            discrete: "y",
-            height: 300,
-            groupBy: "Race",
-            legend: false,
-            y: "Race",
-            x: "share",
+          {povertyByRaceAvailable
+            ? <BarChart config={{
+              data: filterDataBelowPovertyByRace,
+              discrete: "y",
+              height: 300,
+              groupBy: "Race",
+              legend: false,
+              y: "Race",
+              x: "share",
+              time: "Year",
+              yConfig: {
+                ticks: [],
+                title: "Population Below Poverty by Race"
+              },
+              xConfig: {
+                tickFormat: d => formatPopulation(d),
+                title: "Share"
+              },
+              tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPopulation(d.share)], ["Location", d => d.Geography]]}
+            }}
+            /> : <div></div>}
+        </article>
+
+        {povertyByAgeAndGenderAvailable
+          ? <BarChart config={{
+            data: belowPovertyLevelByAgeAndGender,
+            discrete: "x",
+            height: 400,
+            groupBy: "Gender",
+            x: "Age",
+            y: "share",
             time: "Year",
-            yConfig: {
-              ticks: [],
-              title: "Population Below Poverty by Race"
-            },
+            xSort: (a, b) => a["ID Age"] - b["ID Age"],
             xConfig: {
+              labelRotation: false,
+              tickFormat: d => rangeFormatter(d),
+              title: "Population Below Poverty by Age and Gender"
+            },
+            yConfig: {
               tickFormat: d => formatPopulation(d),
               title: "Share"
             },
-            tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPopulation(d.share)], ["Location", d => d.Geography]]}
+            shapeConfig: {
+              label: false
+            },
+            tooltipConfig: {tbody: [["Year", d => d.Year], ["Age", d => d.Age], ["Share", d => formatPopulation(d.share)], ["Location", d => d.Geography]]}
           }}
-          />
-        </article>
-
-        <BarChart config={{
-          data: belowPovertyLevelByAgeAndGender,
-          discrete: "x",
-          height: 400,
-          groupBy: "Gender",
-          x: "Age",
-          y: "share",
-          time: "Year",
-          xSort: (a, b) => a["ID Age"] - b["ID Age"],
-          xConfig: {
-            labelRotation: false,
-            tickFormat: d => rangeFormatter(d),
-            title: "Population Below Poverty by Age and Gender"
-          },
-          yConfig: {
-            tickFormat: d => formatPopulation(d),
-            title: "Share"
-          },
-          shapeConfig: {
-            label: false
-          },
-          tooltipConfig: {tbody: [["Year", d => d.Year], ["Age", d => d.Age], ["Share", d => formatPopulation(d.share)], ["Location", d => d.Geography]]}
-        }}
-        />
+          /> : <div></div>}
       </SectionColumns>
     );
   }

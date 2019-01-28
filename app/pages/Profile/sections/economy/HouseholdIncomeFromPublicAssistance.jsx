fa2ad5ed8dd0dbb5810c383f03e9ef72151aeddd
Rashmi <baheti.rash123@gmail.com>
2019-01-28 14:05:50 -0500

    handles data for any tract/zip level that might not be available

--- app/pages/Profile/sections/economy/HouseholdIncomeFromPublicAssistance.jsx
+++ app/pages/Profile/sections/economy/HouseholdIncomeFromPublicAssistance.jsx
@@ -17,84 +17,93 @@ class HouseholdIncomeFromPublicAssistance extends SectionColumns {
 
     const {publicAssistanceData, householdSnapData} = this.props;
 
+    const publicAssistanceDataAvailable = publicAssistanceData.length !== 0;
+    const householdSnapDataAvailable = householdSnapData.length !== 0;
+
     // Format data for publicAssistanceData
     const recentYearPublicAssistanceData = {};
-    nest()
-      .key(d => d.Year)
-      .entries(publicAssistanceData)
-      .forEach(group => {
-        const total = sum(group.values, d => d["Food-Stamp Population"]);
-        group.values.forEach(d => d.share = d["Food-Stamp Population"] / total * 100);
-        group.key >= publicAssistanceData[0].Year ? Object.assign(recentYearPublicAssistanceData, group) : {};
-      });
+    let topPublicAssistanceData;
+    if (publicAssistanceDataAvailable) {
+      nest()
+        .key(d => d.Year)
+        .entries(publicAssistanceData)
+        .forEach(group => {
+          const total = sum(group.values, d => d["Food-Stamp Population"]);
+          group.values.forEach(d => d.share = d["Food-Stamp Population"] / total * 100);
+          group.key >= publicAssistanceData[0].Year ? Object.assign(recentYearPublicAssistanceData, group) : {};
+        });
+      // Find top recent year data for publicAssistanceData
+      topPublicAssistanceData = recentYearPublicAssistanceData.values.filter(d => d["ID Public Assistance or Snap"] === 0).sort((a, b) => b.share - a.share)[0];
+    }
 
-    // Find top recent year data for publicAssistanceData
-    const filteredData = recentYearPublicAssistanceData.values.filter(d => d["ID Public Assistance or Snap"] === 0).sort((a, b) => b.share - a.share);
-    const topPublicAssistanceData = filteredData[0];
-
-    // Find share for eacd data in householdSnapData.
+    // Find share for each data in householdSnapData.
     const recentYearHouseholdSnapData = {};
-    nest()
-      .key(d => d.Year)
-      .entries(householdSnapData)
-      .forEach(group => {
-        const total = sum(group.values, d => d["SNAP Receipts"]);
-        group.values.forEach(d => d.share = d["SNAP Receipts"] / total * 100);
-        group.key >= householdSnapData[0].Year ? Object.assign(recentYearHouseholdSnapData, group) : {};
+    let filterSnapRecievedData, topRecentYearHouseholdSnapData, totalTopHouseoldShare = 0;
+    if (householdSnapDataAvailable) {
+      nest()
+        .key(d => d.Year)
+        .entries(householdSnapData)
+        .forEach(group => {
+          const total = sum(group.values, d => d["SNAP Receipts"]);
+          group.values.forEach(d => d.share = d["SNAP Receipts"] / total * 100);
+          group.key >= householdSnapData[0].Year ? Object.assign(recentYearHouseholdSnapData, group) : {};
+        });
+      filterSnapRecievedData = householdSnapData.filter(d => d["ID Snap Receipt"] === 0);
+      // Get top stats for householdSnapData.
+      const filteredTopRecentYearHouseholdSnapData = recentYearHouseholdSnapData.values.filter(d => d["ID Snap Receipt"] === 0);
+      topRecentYearHouseholdSnapData = filteredTopRecentYearHouseholdSnapData.sort((a, b) => b.share - a.share)[0];
+      totalTopHouseoldShare = 0;
+      filteredTopRecentYearHouseholdSnapData.forEach(d => {
+        if (d["Number of workers"] === topRecentYearHouseholdSnapData["Number of workers"]) totalTopHouseoldShare += d.share;
       });
-    const filterSnapRecievedData = householdSnapData.filter(d => d["ID Snap Receipt"] === 0);
-
-    // Get top stats for householdSnapData.
-    const filteredTopRecentYearHouseholdSnapData = recentYearHouseholdSnapData.values.filter(d => d["ID Snap Receipt"] === 0);
-    const topRecentYearHouseholdSnapData = filteredTopRecentYearHouseholdSnapData.sort((a, b) => b.share - a.share)[0];
-    let totalTopHouseoldShare = 0;
-    filteredTopRecentYearHouseholdSnapData.forEach(d => {
-      if (d["Number of workers"] === topRecentYearHouseholdSnapData["Number of workers"]) totalTopHouseoldShare += d.share;
-    });
-
+    }
+    
     return (
       <SectionColumns>
         <SectionTitle>Household Income From Public Assistance</SectionTitle>
         <article>
           <Stat
-            title={`Population ${topPublicAssistanceData["Public Assistance or Snap"]}`}
-            year={topPublicAssistanceData.Year}
-            value={`${formatPercentage(topPublicAssistanceData.share)}`}
+            title={"Population With Cash Public Assistance Or Food Stamps/SNAP"}
+            year={publicAssistanceDataAvailable ? topPublicAssistanceData.Year : ""}
+            value={publicAssistanceDataAvailable ? `${formatPercentage(topPublicAssistanceData.share)}` : "N/A"}
           />
           <Stat
             title={"most common number of workers per household"}
-            year={topRecentYearHouseholdSnapData.Year}
-            value={topRecentYearHouseholdSnapData["Number of workers"]}
-            qualifier={formatPercentage(totalTopHouseoldShare)}
+            year={householdSnapDataAvailable ? topRecentYearHouseholdSnapData.Year : ""}
+            value={householdSnapDataAvailable ? topRecentYearHouseholdSnapData["Number of workers"] : "N/A"}
+            qualifier={householdSnapDataAvailable ? formatPercentage(totalTopHouseoldShare) : ""}
           />
-
-          <p>In {topPublicAssistanceData.Year}, {formatPercentage(topPublicAssistanceData.share)} of all population in {topPublicAssistanceData.Geography} got public assistance or food stamps in cash. The most common number of workers per household  on public assistance is {topRecentYearHouseholdSnapData["Number of workers"].toLowerCase()} ({formatPercentage(totalTopHouseoldShare)}).</p>
-          <p>The following chart shows the number of workers per household on public assistance.</p>
+          <p>
+            {publicAssistanceDataAvailable ? <span>In {topPublicAssistanceData.Year}, {formatPercentage(topPublicAssistanceData.share)} of all population in {topPublicAssistanceData.Geography} got public assistance or food stamps in cash. </span> : ""}
+            {householdSnapDataAvailable ? <span>The most common number of workers per household  on public assistance is {topRecentYearHouseholdSnapData["Number of workers"].toLowerCase()} ({formatPercentage(totalTopHouseoldShare)}).</span> : ""}
+          </p>
+          {householdSnapDataAvailable ? <p>The following chart shows the number of workers per household on public assistance.</p> : ""}
         </article>
 
-        <BarChart config={{
-          data: filterSnapRecievedData,
-          discrete: "x",
-          height: 400,
-          stacked: true,
-          legend: false,
-          label: d => `${d["Family type"]}`,
-          groupBy: d => `${d["Family type"]}`,
-          x: d => d["Number of workers"],
-          y: "share",
-          time: "Year",
-          xSort: (a, b) => a["ID Number of workers"] - b["ID Number of workers"],
-          xConfig: {labelRotation: false},
-          yConfig: {
-            tickFormat: d => formatPercentage(d),
-            title: "Share"
-          },
-          shapeConfig: {
-            label: false
-          },
-          tooltipConfig: {tbody: [["Year", d => d.Year], ["Workers", d => d["Number of workers"]], ["Share", d => formatPercentage(d.share)], ["Location", d => d.Geography]]}
-        }}
-        />
+        {householdSnapDataAvailable 
+          ? <BarChart config={{
+            data: filterSnapRecievedData,
+            discrete: "x",
+            height: 400,
+            stacked: true,
+            legend: false,
+            label: d => `${d["Family type"]}`,
+            groupBy: d => `${d["Family type"]}`,
+            x: d => d["Number of workers"],
+            y: "share",
+            time: "Year",
+            xSort: (a, b) => a["ID Number of workers"] - b["ID Number of workers"],
+            xConfig: {labelRotation: false},
+            yConfig: {
+              tickFormat: d => formatPercentage(d),
+              title: "Share"
+            },
+            shapeConfig: {
+              label: false
+            },
+            tooltipConfig: {tbody: [["Year", d => d.Year], ["Workers", d => d["Number of workers"]], ["Share", d => formatPercentage(d.share)], ["Location", d => d.Geography]]}
+          }}
+          /> : <div></div>}
       </SectionColumns>
     );
   }

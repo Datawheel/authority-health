208076c037278b55878f74ae5d2f0543e9249dd9
Rashmi <baheti.rash123@gmail.com>
2019-02-01 16:58:33 -0500

    adds new key for cubes which have "Year" dimension as "End Year"

--- app/utils/stats.js
+++ app/utils/stats.js
@@ -1,17 +1,17 @@
 module.exports = {
   healthTopics: [
-    {measure: "Life Expectancy", depth: ["County", "Tract"], latestYear: "", cube: "NVSS - Life Expectancy"},
-    {measure: "Heart Disease", depth: ["County", "Zip Region"], latestYear: "2016", cube: "MiBRFS - All Years"},
+    {measure: "Life Expectancy", depth: ["County", "Tract"], latestYear: "2015", cube: "NVSS - Life Expectancy", yearDimension: "End Year"},
+    {measure: "Heart Disease", depth: ["County", "Zip Region"], latestYear: "2016", cube: "MiBRFS - All Years", yearDimension: "End Year"},
     {measure: "Coronary Heart Disease", depth: ["Place", "Tract"], latestYear: "2015", cube: "500 Cities"},
     {measure: "High Blood Pressure", depth: ["Place", "Tract"], latestYear: "2015", cube: "500 Cities"},
     {measure: "High Cholesterol", depth: ["Place", "Tract"], latestYear: "2015", cube: "500 Cities"},
     {measure: "COPD", depth: ["Place", "Tract"], latestYear: "2015", cube: "500 Cities"},
-    {measure: "Current Asthma", depth: ["County", "Zip Region"], latestYear: "2016", cube: "MiBRFS - All Years"},
+    {measure: "Current Asthma", depth: ["County", "Zip Region"], latestYear: "2016", cube: "MiBRFS - All Years", yearDimension: "End Year"},
     {measure: "Diabetes", depth: ["Place", "Tract"], latestYear: "2015", cube: "500 Cities"},
     {measure: "Obesity", depth: ["Place", "Tract"], latestYear: "2015", cube: "500 Cities"},
-    {measure: "Ever Depressive", depth: ["County", "Zip Region"], latestYear: "2016", cube: "MiBRFS - All Years"},
-    {measure: "Poor Mental Health 14 Or More Days", depth: ["County", "Zip Region"], latestYear: "2016", cube: "MiBRFS - All Years"},
-    {measure: "Monthly Alcohol Consumption", depth: ["County", "Zip Region"], latestYear: "2016", cube: "MiBRFS - All Years"},
+    {measure: "Ever Depressive", depth: ["County", "Zip Region"], latestYear: "2016", cube: "MiBRFS - All Years", yearDimension: "End Year"},
+    {measure: "Poor Mental Health 14 Or More Days", depth: ["County", "Zip Region"], latestYear: "2016", cube: "MiBRFS - All Years", yearDimension: "End Year"},
+    {measure: "Monthly Alcohol Consumption", depth: ["County", "Zip Region"], latestYear: "2016", cube: "MiBRFS - All Years", yearDimension: "End Year"},
     {measure: "Binge Drinking", depth: ["Place", "Tract"], latestYear: "2015", cube: "500 Cities"},
     {measure: "Current Smoking", depth: ["Place", "Tract"], latestYear: "2015", cube: "500 Cities"}
   ],

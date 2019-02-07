57cd36246d53d75e8547f220de361587a68a0fd7
Dave Landry <landry.dave@gmail.com>
2019-02-07 12:56:29 -0500

    gives MainNav dark background in vizbuilder

--- app/components/MainNav.jsx
+++ app/components/MainNav.jsx
@@ -1,20 +1,22 @@
 import React, {Component} from "react";
+import PropTypes from "prop-types";
 import {Link} from "react-router";
 import "./MainNav.css";
 
-export default class MainNav extends Component {
+class MainNav extends Component {
 
   render() {
+    const {router} = this.context;
     const search = false;
+    const dark = ["charts"].includes(router.location.pathname);
     return (
-      <nav className="main-nav" role="nav">
+      <nav className={ `main-nav ${ dark ? "dark" : "" }` } role="nav">
         <div className="main-nav-inner">
           <Link className="main-nav-logo-link" to="/">
             <img className="main-nav-logo-img" src="/images/authority-health-logo.png" srcSet="/images/authority-health-logo.svg 1x" alt=""/>
             <span className="u-visually-hidden">AuthorityHealth</span>
           </Link>
 
-          {/* TODO: true everywhere but home page */}
           {/* TODO: replace with actual search component */}
           {search && <span>search</span>}
 
@@ -24,7 +26,7 @@ export default class MainNav extends Component {
             </li>
 
             <li className="main-nav-item">
-              <Link to="/map" className="main-nav-link">Map</Link>
+              <Link to="/charts" className="main-nav-link">Chart Builder</Link>
             </li>
 
             <li className="main-nav-item">
@@ -36,3 +38,9 @@ export default class MainNav extends Component {
     );
   }
 }
+
+MainNav.contextTypes = {
+  router: PropTypes.object
+};
+
+export default MainNav;

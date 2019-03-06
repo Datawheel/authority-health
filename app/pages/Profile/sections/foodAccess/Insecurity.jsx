d8d0e8a3f317319f49cd501029ddcb1597076686
Rashmi <baheti.rash123@gmail.com>
2019-03-06 14:36:31 -0500

    adds title "Insecurity"
    - adds insecurity definition
    - adds text to explain stats

--- app/pages/Profile/sections/foodAccess/Insecurity.jsx
+++ app/pages/Profile/sections/foodAccess/Insecurity.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
-
-import {fetchData, SectionColumns} from "@datawheel/canon-core";
-
+import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 import Stat from "components/Stat";
 
 class Insecurity extends SectionColumns {
@@ -12,10 +10,12 @@ class Insecurity extends SectionColumns {
     const isInsecurityRateDataAvailableForCurrentGeography = insecurityRate.source[0].substitutions.length === 0;
     const childInsecurity = insecurityRate.data[0];
     const adultInsecurity = insecurityRate.data[1];
+    const adultInsecurityRate = adultInsecurity["Food Insecurity Rate"] - childInsecurity["Food Insecurity Rate"];
 
     return (
       <div className="section-title-stat-inner">
         <SectionColumns>
+          <SectionTitle>Insecurity</SectionTitle>
           <article>
             {isInsecurityRateDataAvailableForCurrentGeography ? <div></div> : <div className="disclaimer">Showing data for {insecurityRate.data[0].Geography}.</div>}
             <Stat
@@ -23,14 +23,15 @@ class Insecurity extends SectionColumns {
               year={childInsecurity.Year}
               value={`${childInsecurity["Food Insecurity Rate"]}%`}
             />
-          </article>
-          <article>
             <Stat
               title={"Adult Insecurity"}
               year={adultInsecurity.Year}
-              value={`${adultInsecurity["Food Insecurity Rate"] - childInsecurity["Food Insecurity Rate"]}%`}
+              value={`${adultInsecurityRate}%`}
             />
+            <p>Food insecurity refers to <a href="https://www.ers.usda.gov/topics/food-nutrition-assistance/food-security-in-the-us.aspx">USDA’s measure</a> of lack of access, at times, to enough food for an active, healthy life for all household members and limited or uncertain availability of nutritionally adequate foods.</p>
+            <p>In {childInsecurity.Year}, the food insecurity rate was {childInsecurity["Food Insecurity Rate"]}% of all children and {adultInsecurityRate}% of all adults in {childInsecurity.Geography}.</p>
           </article>
+          <div></div> {/* // adds empty div to for alignment of above text */}
         </SectionColumns>
       </div>
     );

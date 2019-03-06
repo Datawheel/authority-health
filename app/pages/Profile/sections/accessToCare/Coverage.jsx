66897a1681f24a6417d7bf461ae7881c83f21b99
Rashmi <baheti.rash123@gmail.com>
2019-03-06 16:53:24 -0500

    adds Geomap for coverage data for places in Wayne County

--- app/pages/Profile/sections/accessToCare/Coverage.jsx
+++ app/pages/Profile/sections/accessToCare/Coverage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import {connect} from "react-redux";
 import {sum} from "d3-array";
 import {nest} from "d3-collection";
-import {BarChart} from "d3plus-react";
+import {BarChart, Geomap} from "d3plus-react";
 import {formatAbbreviate} from "d3plus-format";
 import {titleCase} from "d3plus-text";
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
 import Contact from "components/Contact";
 import rangeFormatter from "utils/rangeFormatter";
+import places from "utils/places";
 import Stat from "components/Stat";
 
 const formatPercentage = d => `${formatAbbreviate(d)}%`;
@@ -27,7 +28,8 @@ const formatCoverageData = coverageData => {
           ageGroup.values.forEach(d => total !== 0 ? d.share = d["Population by Insurance Coverage"] / total * 100 : d.share = 0);
         });
     });
-  return coverageData;
+  const filteredRecentYearData = coverageData.filter(d => d["ID Health Insurance Coverage Status"] === 0);
+  return filteredRecentYearData;
 };
 
 class Coverage extends SectionColumns {
@@ -44,8 +46,7 @@ class Coverage extends SectionColumns {
     let ageGroupYear, geoId, maleCoverageData, topFemaleAgeGroup, topFemaleShare, topMaleAgeGroup, topMaleShare;
     if (coverageDataAvailable) {
       const recentYearCoverageData = formatCoverageData(coverageData.data);
-      const filteredRecentYearData = recentYearCoverageData.filter(d => d["ID Health Insurance Coverage Status"] === 0);
-      const femaleCoverageData = filteredRecentYearData.filter(d => d.Sex === "Female").sort((a, b) => b.share - a.share);
+      const femaleCoverageData = recentYearCoverageData.filter(d => d.Sex === "Female").sort((a, b) => b.share - a.share);
       topFemaleAgeGroup = rangeFormatter(femaleCoverageData[0].Age);
       topFemaleShare = formatPercentage(femaleCoverageData[0].share);
 
@@ -67,42 +68,68 @@ class Coverage extends SectionColumns {
                 title="Most covered male group"
                 year={ageGroupYear}
                 value={topMaleAgeGroup}
-                qualifier={topMaleShare}
+                qualifier={`${topMaleShare} of total population within ${topMaleAgeGroup} years age group`}
               />
               <Stat
                 title="Most covered female group"
                 year={ageGroupYear}
                 value={topFemaleAgeGroup}
-                qualifier={topFemaleShare}
+                qualifier={`${topFemaleShare} of total population within ${topFemaleAgeGroup} years age group`}
               />
             </div>
 
             <p>In {ageGroupYear}, the age groups most likely to have health care coverage in {maleCoverageData[0].Geography} are {topMaleAgeGroup} and {topFemaleAgeGroup} years, for men and women respectively.</p>
+            <p>The map here shows share of population with health insurance coverage for places in Wayne County.</p>
             <p>The following chart shows the male and female age groups with health insurance coverage in {maleCoverageData[0].Geography}.</p>
             <Contact slug={this.props.slug} />
+
+            <BarChart config={{
+              data: `/api/data?measures=Population by Insurance Coverage&drilldowns=Health Insurance Coverage Status,Sex,Age&Geography=${geoId}&Year=all`,
+              discrete: "x",
+              height: 400,
+              groupBy: "Sex",
+              x: "Age",
+              y: "share",
+              time: "ID Year",
+              xSort: (a, b) => a["ID Age"] - b["ID Age"],
+              xConfig: {
+                labelRotation: false,
+                tickFormat: d => rangeFormatter(d),
+                title: "Population with Coverage"
+              },
+              yConfig: {tickFormat: d => formatPercentage(d)},
+              shapeConfig: {
+                label: false
+              },
+              tooltipConfig: {tbody: [["Year", d => d.Year], ["Age", d => d.Age], ["Share", d => formatPercentage(d.share)], [titleCase(meta.level), d => d.Geography]]}
+            }}
+            dataFormat={resp => formatCoverageData(resp.data)}
+            />
           </article>
 
-          <BarChart config={{
-            data: `/api/data?measures=Population by Insurance Coverage&drilldowns=Health Insurance Coverage Status,Sex,Age&Geography=${geoId}&Year=all`,
-            discrete: "x",
+          <Geomap config={{
+            data: "/api/data?measures=Population by Insurance Coverage&drilldowns=Health Insurance Coverage Status,Place&Geography=05000US26163:children&Year=all",
+            groupBy: "ID Place",
+            colorScale: "share",
+            title: "Population by Coverage",
+            colorScaleConfig: {axisConfig: {tickFormat: d => formatPercentage(d)}},
+            time: "Year",
+            label: d => d.Place,
             height: 400,
-            groupBy: "Sex",
-            x: "Age",
-            y: "share",
-            time: "ID Year",
-            xSort: (a, b) => a["ID Age"] - b["ID Age"],
-            xConfig: {
-              labelRotation: false,
-              tickFormat: d => rangeFormatter(d),
-              title: "Population with Coverage"
-            },
-            yConfig: {tickFormat: d => formatPercentage(d)},
-            shapeConfig: {
-              label: false
-            },
-            tooltipConfig: {tbody: [["Year", d => d.Year], ["Age", d => d.Age], ["Share", d => formatPercentage(d.share)], [titleCase(meta.level), d => d.Geography]]}
+            tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPercentage(d.share)]]},
+            topojson: "/topojson/place.json",
+            topojsonFilter: d => places.includes(d.id)
           }}
-          dataFormat={resp => formatCoverageData(resp.data)}
+          dataFormat={resp => {
+            nest()
+              .key(d => d.Year)
+              .entries(resp.data)
+              .forEach(group => {
+                const total = sum(group.values, d => d["Population by Insurance Coverage"]);
+                group.values.forEach(d => total !== 0 ? d.share = d["Population by Insurance Coverage"] / total * 100 : d.share = 0);
+              });
+            return resp.data.filter(d => d["ID Health Insurance Coverage Status"] === 0);
+          }}
           />
         </SectionColumns>
       );

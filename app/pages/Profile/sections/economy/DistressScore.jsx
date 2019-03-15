c5161c49e7085f5d85799ca29a175995f0e52999
Rashmi <baheti.rash123@gmail.com>
2019-03-14 20:04:18 -0400

    adds definition and seven component metrics of the DCI

--- app/pages/Profile/sections/economy/DistressScore.jsx
+++ app/pages/Profile/sections/economy/DistressScore.jsx
@@ -26,9 +26,20 @@ class DistressScore extends SectionColumns {
             title="Zip code with highest Distress Score"
             year={topDistressScoreData.Year}
             value={topDistressScoreData.Zip}
-            qualifier={`${formatAbbreviate(topDistressScoreData["Distress Score"])} percentile`}
+            qualifier={`(${formatAbbreviate(topDistressScoreData["Distress Score"])} percentile)`}
           />
-          <p>The maximum distress score was observed in the zip code {topDistressScoreData.Zip} with {formatAbbreviate(topDistressScoreData["Distress Score"])} percentile in the year {topDistressScoreData.Year}.</p>
+          <p>In {topDistressScoreData.Year}, the maximum distress score was observed in the zip code {topDistressScoreData.Zip} with {formatAbbreviate(topDistressScoreData["Distress Score"])} percentile with 0 percentile being least distressed (desired outcome) and 100 percentile being most distressed (unfavorable outcome).</p>
+          <p>The Distressed Communities Index (DCI) combines seven complementary economic indicators into a single holistic and comparative measure of community well-being. {}
+          The seven component metrics of the DCI are:</p>
+          <ol>
+            <li><b>No high school diploma:</b> Percent of the 25+ population without a high school diploma or equivalent</li>
+            <li><b>Housing vacancy rate:</b> Percent of habitable housing that is unoccupied, excluding properties that are for seasonal, recreational, or occasional use</li>
+            <li><b>Adults not working:</b> Percent of the prime-age population (25-64) not currently in work</li>
+            <li><b>Poverty rate:</b> Percent of the population living under the poverty line</li>
+            <li><b>Median income ratio:</b> Median household income as a percent of the state’s median household income (to account for cost of living differences across states)</li>
+            <li><b>Change in employment:</b> Percent change in the number of jobs</li>
+            <li><b>Change in establishments:</b> Percent change in the number of business establishments</li>
+          </ol>
           <p>The following map shows the distress score percentile for each zip code in Wayne County.</p>
           <Contact slug={this.props.slug} />
         </article>

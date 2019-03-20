e408db99b861595a36b3290cfa3a39feaf95a087
Rashmi <baheti.rash123@gmail.com>
2019-03-20 16:38:16 -0400

    adds zip region definition

--- app/components/ZipRegionDefinition.jsx
+++ app/components/ZipRegionDefinition.jsx
@@ -14,7 +14,7 @@ class ZipRegionDefinition extends Component {
 
     const zipRegionDefinition = 
       <p>
-        Zip region definition here.
+        Based on geographical contiguity and knowledge of Detroit and Wayne County communities, the 73 zip codes were combined into 27 communities to address sample size issues. Variation between measures by community was also taken into account, to avoid watering down estimates by grouping geographies with disparate estimates. In six cases, the zip code had a large enough sample to stand on its own; in others, between two and seven zip codes were combined.
       </p>;
 
     return (

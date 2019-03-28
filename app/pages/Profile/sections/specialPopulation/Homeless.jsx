fb358838a6936235fccf0b3ed39c8ed4d40ca421
Rashmi <baheti.rash123@gmail.com>
2019-03-28 16:20:53 -0400

    splits a grouped barchart into 2 horizontal barcharts

--- app/pages/Profile/sections/specialPopulation/Homeless.jsx
+++ app/pages/Profile/sections/specialPopulation/Homeless.jsx
@@ -18,29 +18,28 @@ const definitions = [
 
 const formatPercentage = d => `${formatAbbreviate(d)}%`;
 
-const formatTypesOfShelteredAndUnshelteredData = (data, shelteredHomelessData = false) => {
-  if (shelteredHomelessData) {
-    nest()
-      .key(d => d.Year)
-      .entries(data)
-      .forEach(group => {
-        const total = sum(group.values, d => d["Sheltered Homeless Population"]);
-        group.values.forEach(d => d.share = d["Sheltered Homeless Population"] / total * 100);
-      });
-    const topShelteredHomelessTypes = data.sort((a, b) => b.share - a.share)[0];
-    return [data, topShelteredHomelessTypes];
-  }
-  else {
-    nest()
-      .key(d => d.Year)
-      .entries(data)
-      .forEach(group => {
-        const total = sum(group.values, d => d["Unsheltered Homeless Population"]);
-        group.values.forEach(d => d.share = d["Unsheltered Homeless Population"] / total * 100);
-      });
-    const topUnshelteredHomelessTypes = data.sort((a, b) => b.share - a.share)[0];
-    return [data, topUnshelteredHomelessTypes];
-  }
+const formatShelteredHomelessCategories = data => {
+  nest()
+    .key(d => d.Year)
+    .entries(data)
+    .forEach(group => {
+      const total = sum(group.values, d => d["Sheltered Homeless Population"]);
+      group.values.forEach(d => d.shelteredShare = d["Sheltered Homeless Population"] / total * 100);
+    });
+  const topShelteredHomelessTypes = data.sort((a, b) => b.shelteredShare - a.shelteredShare)[0];
+  return [data, topShelteredHomelessTypes];
+};
+
+const formatUnshelteredHomelessCategories = data => {
+  nest()
+    .key(d => d.Year)
+    .entries(data)
+    .forEach(group => {
+      const total = sum(group.values, d => d["Unsheltered Homeless Population"]);
+      group.values.forEach(d => d.unshelteredShare = d["Unsheltered Homeless Population"] / total * 100);
+    });
+  const topUnshelteredHomelessTypes = data.sort((a, b) => b.unshelteredShare - a.unshelteredShare)[0];
+  return [data, topUnshelteredHomelessTypes];
 };
 
 const formatTypesOfHomeless = typesOfHomeless => {
@@ -66,15 +65,6 @@ const formatTypesOfHomeless = typesOfHomeless => {
 
 class Homeless extends SectionColumns {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      meta: this.props.meta,
-      dropdownValue: "Sheltered Homeless Population",
-      typesOfUnshelteredHomeless: []
-    };
-  }
-
   render() {
 
     const {meta, typesOfShelteredAndUnshelteredHomeless, totalHomelessData, wayneCountyPopulation} = this.props;
@@ -82,15 +72,15 @@ class Homeless extends SectionColumns {
     // Check if the data is available for current profile or if it falls back to the parent geography.
     const isHomelessDataAvailableForCurrentGeography = totalHomelessData.source[0].substitutions.length === 0;
 
-    const topShelteredHomelessTypes = formatTypesOfShelteredAndUnshelteredData(typesOfShelteredAndUnshelteredHomeless, true)[1];
-    const topUnshelteredHomelessTypes = formatTypesOfShelteredAndUnshelteredData(typesOfShelteredAndUnshelteredHomeless)[1];
+    const topShelteredHomelessCategory = formatShelteredHomelessCategories(typesOfShelteredAndUnshelteredHomeless)[1];
+    const topUnshelteredHomelessCategory = formatUnshelteredHomelessCategories(typesOfShelteredAndUnshelteredHomeless)[1];
     const totalHomelessPopulation = (totalHomelessData.data[0]["Sheltered Homeless Population"] + totalHomelessData.data[0]["Unsheltered Homeless Population"]) / wayneCountyPopulation[0].Population * 100;
 
     return (
       <SectionColumns>
         <SectionTitle>Homeless</SectionTitle>
         <article>
-          {isHomelessDataAvailableForCurrentGeography ? <div></div> : <div className="disclaimer">data is shown for {topShelteredHomelessTypes.Geography}</div>}
+          {isHomelessDataAvailableForCurrentGeography ? <div></div> : <div className="disclaimer">data is shown for {topShelteredHomelessCategory.Geography}</div>}
           <Stat
             title={"Homeless rate"}
             year={totalHomelessData.data[0].Year}
@@ -98,20 +88,20 @@ class Homeless extends SectionColumns {
           />
           <Stat
             title={"Most common Sheltered demographic"}
-            year={topShelteredHomelessTypes.Year}
-            value={topShelteredHomelessTypes.Category}
-            qualifier={formatPercentage(topShelteredHomelessTypes.share)}
+            year={topShelteredHomelessCategory.Year}
+            value={topShelteredHomelessCategory.Category}
+            qualifier={formatPercentage(topShelteredHomelessCategory.shelteredShare)}
           />
           <Stat
             title={"Most common Unsheltered demographic"}
-            year={topUnshelteredHomelessTypes.Year}
-            value={topUnshelteredHomelessTypes.Category}
-            qualifier={formatPercentage(topUnshelteredHomelessTypes.share)}
+            year={topUnshelteredHomelessCategory.Year}
+            value={topUnshelteredHomelessCategory.Category}
+            qualifier={formatPercentage(topUnshelteredHomelessCategory.unshelteredShare)}
           />
 
           <p>In {totalHomelessData.data[0].Year}, {formatPercentage(totalHomelessPopulation)} of the population in {totalHomelessData.data[0].Geography} was homeless. { }
-          The most common sheltered demographic was {topShelteredHomelessTypes.Category.toLowerCase()} ({formatPercentage(topShelteredHomelessTypes.share)}) { }
-          and unsheltered demographic was {topUnshelteredHomelessTypes.Category.toLowerCase()} ({formatPercentage(topUnshelteredHomelessTypes.share)}).</p>
+          The most common sheltered demographic was {topShelteredHomelessCategory.Category.toLowerCase()} ({formatPercentage(topShelteredHomelessCategory.shelteredShare)}) { }
+          and unsheltered demographic was {topUnshelteredHomelessCategory.Category.toLowerCase()} ({formatPercentage(topUnshelteredHomelessCategory.unshelteredShare)}).</p>
 
           <p>Following charts shows different categories and types of sheltered and unsheltered homeless population in {totalHomelessData.data[0].Geography} and percentages for each one of them.</p>
 
@@ -139,28 +129,57 @@ class Homeless extends SectionColumns {
           <Contact slug={this.props.slug} />
         </article>
 
+        {/* <div>  */}
         <BarChart config={{
-          data: `/api/data?measures=Sheltered Homeless Population,Unsheltered Homeless Population&drilldowns=Category&Geography=${meta.id}&Year=all`,
-          discrete: "x",
-          // stacked: true,
+          data: `/api/data?measures=Sheltered Homeless Population&drilldowns=Category&Geography=${meta.id}&Year=all`,
+          discrete: "y",
           legend: false,
-          groupBy: "HomelessType",
-          x: "Category",
-          y: "share",
+          groupBy: "Category",
+          label: d => `Sheltered Homeless: ${d.Category}`,
+          x: "shelteredShare",
+          y: "Category",
           time: "Year",
-          xConfig: {labelRotation: false},
-          yConfig: {
+          xConfig: {
             tickFormat: d => formatPercentage(d),
             title: "Share"
           },
-          title: "Categories in Sheltered & Unsheltered Population",
+          yConfig: {
+            labelRotation: false
+          },
+          title: d => `Categories in Sheltered Homeless Population in ${d[0].Geography}`,
           shapeConfig: {
             label: false
           },
-          tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPercentage(d.share)], ["County", d => d.Geography]]}
+          tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPercentage(d.shelteredShare)], ["County", d => d.Geography]]}
         }}
-        dataFormat={resp => formatTypesOfHomeless(resp.data)}
+        dataFormat={resp => formatShelteredHomelessCategories(resp.data)[0]}
         />
+        
+        <BarChart config={{
+          data: `/api/data?measures=Unsheltered Homeless Population&drilldowns=Category&Geography=${meta.id}&Year=all`,
+          discrete: "y",
+          legend: false,
+          groupBy: "Category",
+          label: d => `Unsheltered Homeless: ${d.Category}`,
+          x: "unshelteredShare",
+          y: "Category",
+          time: "Year",
+          xConfig: {
+            tickFormat: d => formatPercentage(d),
+            title: "Share"
+          },
+          yConfig: {
+            labelRotation: false
+          },
+          title: d => `Categories in Unsheltered Homeless Population in ${d[0].Geography}`,
+          shapeConfig: {
+            label: false
+          },
+          tooltipConfig: {tbody: [["Year", d => d.Year], ["Share", d => formatPercentage(d.unshelteredShare)], ["County", d => d.Geography]]}
+        }}
+        dataFormat={resp => formatUnshelteredHomelessCategories(resp.data)[0]}
+        />
+        {/* </div> */}
       </SectionColumns>
     );
   }

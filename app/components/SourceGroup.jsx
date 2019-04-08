4b72a7b28ffa8dd0664b1ea2e8438fb19fb345a7
Rashmi <baheti.rash123@gmail.com>
2019-04-08 10:08:18 -0400

    removes unused data in SourceGroup

--- app/components/SourceGroup.jsx
+++ app/components/SourceGroup.jsx
@@ -15,29 +15,19 @@ export default class SourceGroup extends Component {
         { sources.map((source, i) => {
 
           const {
-            dataset_description: datasetDesc,
             dataset_link: datasetLink,
-            dataset_name: dataset,
             source_description: orgDesc,
-            source_link: orgLink,
             source_name: org
           } = source;
 
           const orgName = org && `the ${org.replace(/^(T|t)he\s/g, "")}`;
-          const datasetName = dataset && `${dataset}`;
 
           return <span key={i} className="source-item">
             { i && i === sources.length - 1 ? <span> and</span> : null }
             { org && <span>&nbsp;</span> }
             { org && <Tooltip content={orgDesc} className={orgDesc ? "active" : ""} tooltipClassName="source-group-tooltip" disabled={!orgDesc}>
-              {/* { orgLink ? <a className="link" href={orgLink} target="_blank" rel="noopener noreferrer" dangerouslySetInnerHTML={{__html: orgName}} /> : <span dangerouslySetInnerHTML={{__html: orgName}} /> } */}
-              { datasetLink ? <a className="link" href={datasetLink} target="_blank" rel="noopener noreferrer" dangerouslySetInnerHTML={{__html: orgName}} /> : <span dangerouslySetInnerHTML={{__html: orgName}} /> }
-              {/* { datasetLink ? <a className="link" href={datasetLink} target="_blank" rel="noopener noreferrer" dangerouslySetInnerHTML={{__html: datasetName}} /> : <span dangerouslySetInnerHTML={{__html: datasetName}} /> } */}
+              { datasetLink && orgName ? <a className="link" href={datasetLink} target="_blank" rel="noopener noreferrer" dangerouslySetInnerHTML={{__html: orgName}} /> : <span dangerouslySetInnerHTML={{__html: orgName}} /> }
             </Tooltip> }
-            {/* { dataset && <span>&nbsp;</span> }
-            { dataset && <Tooltip content={datasetDesc} className={datasetDesc ? "active" : ""} disabled={!datasetDesc}>
-              { datasetLink ? <a className="link" href={datasetLink} target="_blank" rel="noopener noreferrer" dangerouslySetInnerHTML={{__html: datasetName}} /> : <span dangerouslySetInnerHTML={{__html: datasetName}} /> }
-            </Tooltip> } */}
             { i < sources.length - 1 && <span>,</span> }
             {i === sources.length - 1 && <span>.</span>}
           </span>;

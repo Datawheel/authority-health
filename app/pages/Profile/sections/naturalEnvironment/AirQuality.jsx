2e77099915d30c300e19b193779231834e641e08
Rashmi <baheti.rash123@gmail.com>
2019-04-11 17:14:21 -0400

    adds dropdown for 3 charts in AirQuality

--- app/pages/Profile/sections/naturalEnvironment/AirQuality.jsx
+++ app/pages/Profile/sections/naturalEnvironment/AirQuality.jsx
@@ -4,6 +4,7 @@ import {connect} from "react-redux";
 import {LinePlot} from "d3plus-react";
 import {formatAbbreviate} from "d3plus-format";
 import {titleCase} from "d3plus-text";
+import axios from "axios";
 
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
@@ -19,12 +20,44 @@ class AirQuality extends SectionColumns {
 
   constructor(props) {
     super(props);
-    this.state = {sources: []};
+    this.state = {
+      sources: [],
+      meta: this.props.meta,
+      dropdownValue: "Air Quality"
+    };
+  }
+
+  // Handler function for dropdown onChange event.
+  handleChange = event => {
+    const dropdownValue = event.target.value;
+    const {meta} = this.state;
+    if (dropdownValue === "Air Pollutants") {
+      axios.get(`/api/data?measures=Air Pollutant Days&drilldowns=Pollutant&Geography=${meta.id}&Year=latest`)
+        .then(resp => {
+          this.setState({
+            airPollutants: resp.data.data,
+            dropdownValue
+          });
+        });
+    }
+    else if (dropdownValue === "Median Air Quality Index") {
+      axios.get(`/api/data?measures=Median AQI&Geography=${meta.id}&Year=latest`)
+        .then(resp => {
+          this.setState({
+            airQualityMedianAQIs: resp.data.data,
+            dropdownValue
+          });
+        });
+    }
+    else this.setState({dropdownValue});
   }
 
   render() {
 
     const {meta, airQualityDays, airQualityMedianAQIs, airPollutants} = this.props;
+    const {dropdownValue} = this.state;
+
+    const dropdownList = ["Air Quality", "Air Pollutants", "Median Air Quality Index"];
 
     // Check if the data is available for current profile or if it falls back to the parent geography.
     const isAirQualityDaysAvailableForCurrentGeography = airQualityDays.source[0].substitutions.length === 0;
@@ -41,79 +74,59 @@ class AirQuality extends SectionColumns {
       <SectionColumns>
         <SectionTitle>Air Quality</SectionTitle>
         <article>
-          <Stat
-            title={"Days with good quality"}
-            year={topRecentYearAirQualityDays.Year}
-            value={formatPercentage(topRecentYearAirQualityDays.share)}
-            qualifier={`${topRecentYearAirQualityDays["Air Quality Days"]} of 90 days measured`}
-          />
-          <Stat
-            title={"Most common air pollutant"}
-            year={topRecentYearAirPollutant.Year}
-            value={topRecentYearAirPollutant.Pollutant}
-            qualifier={`${topRecentYearAirPollutant["Air Pollutant Days"]} days`}
-          />
-          <Stat
-            title={"Median Air Quality Index"}
-            year={airQualityMedianAQIs[0].Year}
-            value={airQualityMedianAQIs[0]["Median AQI"]}
-          />
+          <label className="pt-label pt-inline" htmlFor="store-access-dropdown">
+            Show data for
+            <select id="store-access-dropdown" onChange={this.handleChange}>
+              {dropdownList.map(item => <option key={item} value={item}>{item}</option>)}
+            </select>
+          </label>
 
-          <p>{topRecentYearAirQualityDays["Air Quality Days"]} of 90 days measured were good quality air in {topRecentYearAirQualityDays.Year}. The most common air pollutants was {topRecentYearAirPollutant.Pollutant} ({topRecentYearAirPollutant.Year}) and the median AQI was {airQualityMedianAQIs[0]["Median AQI"]} in {airQualityMedianAQIs[0].Geography}.</p>
+          {dropdownValue === "Air Quality" && 
+          <div>
+            <Stat
+              title={"Days with good quality"}
+              year={topRecentYearAirQualityDays.Year}
+              value={formatPercentage(topRecentYearAirQualityDays.share)}
+              qualifier={`${topRecentYearAirQualityDays["Air Quality Days"]} of 90 days measured`}
+            />
+            <p>{topRecentYearAirQualityDays["Air Quality Days"]} of 90 days measured were good quality air in {topRecentYearAirQualityDays.Year}.</p>
+          </div>}
+
+          {dropdownValue === "Air Pollutants" &&
+          <div>
+            <Stat
+              title={"Most common air pollutant"}
+              year={topRecentYearAirPollutant.Year}
+              value={topRecentYearAirPollutant.Pollutant}
+              qualifier={`${topRecentYearAirPollutant["Air Pollutant Days"]} days`}
+            />
+            <p>The most common air pollutants was {topRecentYearAirPollutant.Pollutant} in {topRecentYearAirPollutant.Year}.</p>
+          </div>}
+          {dropdownValue === "Median Air Quality Index" &&
+          <div>
+            <Stat
+              title={"Median Air Quality Index"}
+              year={airQualityMedianAQIs[0].Year}
+              value={airQualityMedianAQIs[0]["Median AQI"]}
+            />
+            <p>The median air quality index was {airQualityMedianAQIs[0]["Median AQI"]} in {airQualityMedianAQIs[0].Geography}.</p>
+          </div>}
+
+          <p>The chart here shows the {dropdownValue.toLocaleLowerCase()} over years in Wayne County.</p>
 
           {!isAirQualityDaysAvailableForCurrentGeography &&
             <Disclaimer>Data is shown for {airQualityDays.data[0].Geography}</Disclaimer>
           }
           <SourceGroup sources={this.state.sources} />
           <Contact slug={this.props.slug} />
-
-          {/* Lineplot to show air pollutants over the years. */}
-          <LinePlot config={{
-            data: `/api/data?measures=Air Pollutant Days&drilldowns=Pollutant&Geography=${meta.id}&Year=all`,
-            discrete: "x",
-            height: 200,
-            title: d => `Air Pollutants Over Years in ${d[0].Geography}`,
-            legend: false,
-            groupBy: "Pollutant",
-            x: "Year",
-            y: "Air Pollutant Days",
-            yConfig: {
-              title: "Testing Days"
-            },
-            tooltipConfig: {tbody: [["Year", d => d.Year], ["Air Pollutant Days", d => d["Air Pollutant Days"]], ["County", d => d.Geography]]}
-          }}
-          dataFormat={resp => {
-            this.setState({sources: updateSource(resp.source, this.state.sources)});
-            return resp.data;
-          }}
-          />
-
-          {/* Lineplot to show Median AQI stats over the years in the Waye county. */}
-          <LinePlot config={{
-            data: `/api/data?measures=Median AQI&Geography=${meta.id}&Year=all`,
-            discrete: "x",
-            height: 200,
-            title: d => `Median AQI Over Years in ${d[0].Geography}`,
-            legend: false,
-            groupBy: "Geography",
-            x: "Year",
-            y: "Median AQI",
-            yConfig: {
-              title: "Median AQI"
-            },
-            tooltipConfig: {tbody: [["Year", d => d.Year], ["Median AQI", d => d["Median AQI"]], ["County", d => d.Geography]]}
-          }}
-          dataFormat={resp => {
-            this.setState({sources: updateSource(resp.source, this.state.sources)});
-            return resp.data;
-          }}
-          />
         </article>
 
         {/* Lineplot to show air quality days over the years. */}
+        {dropdownValue === "Air Quality" && 
         <LinePlot config={{
           data: `/api/data?measures=Air Quality Days&drilldowns=Category&Geography=${meta.id}&Year=all`,
           discrete: "x",
+          height: 400,
           title: d => `Air Quality Over Years in ${d[0].Geography}`,
           legend: false,
           label: d => titleCase(d.Category),
@@ -129,7 +142,52 @@ class AirQuality extends SectionColumns {
           this.setState({sources: updateSource(resp.source, this.state.sources)});
           return resp.data;
         }}
+        />}
+
+        {/* Lineplot to show air pollutants over the years. */}
+        {dropdownValue === "Air Pollutants" && 
+          <LinePlot config={{
+            data: `/api/data?measures=Air Pollutant Days&drilldowns=Pollutant&Geography=${meta.id}&Year=all`,
+            discrete: "x",
+            height: 400,
+            title: d => `Air Pollutants Over Years in ${d[0].Geography}`,
+            legend: false,
+            groupBy: "Pollutant",
+            x: "Year",
+            y: "Air Pollutant Days",
+            yConfig: {
+              title: "Testing Days"
+            },
+            tooltipConfig: {tbody: [["Year", d => d.Year], ["Air Pollutant Days", d => d["Air Pollutant Days"]], ["County", d => d.Geography]]}
+          }}
+          dataFormat={resp => {
+            this.setState({sources: updateSource(resp.source, this.state.sources)});
+            return resp.data;
+          }}
+          />}
+
+        {/* Lineplot to show Median AQI stats over the years in the Waye county. */}
+        {dropdownValue === "Median Air Quality Index" && 
+        <LinePlot config={{
+          data: `/api/data?measures=Median AQI&Geography=${meta.id}&Year=all`,
+          discrete: "x",
+          height: 400,
+          title: d => `Median AQI Over Years in ${d[0].Geography}`,
+          legend: false,
+          groupBy: "Geography",
+          x: "Year",
+          y: "Median AQI",
+          yConfig: {
+            title: "Median AQI"
+          },
+          tooltipConfig: {tbody: [["Year", d => d.Year], ["Median AQI", d => d["Median AQI"]], ["County", d => d.Geography]]}
+        }}
+        dataFormat={resp => {
+          this.setState({sources: updateSource(resp.source, this.state.sources)});
+          return resp.data;
+        }}
         />
+        }
       </SectionColumns>
     );
   }

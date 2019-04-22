94e905895ce8a71dfccf3d05e12f4ca26753765f
Dave Landry <landry.dave@gmail.com>
2019-04-22 17:13:42 -0400

    updates cache script to go in hierarchy order

--- scripts/cache.js
+++ scripts/cache.js
@@ -3,15 +3,20 @@
 const axios = require("axios");
 
 const domain = "https://data.authorityhealth.org/";
+const levels = ["County", "Place", "Zip", "Tract"];
 
 /** */
 async function run() {
 
-  const attrs = await axios.get(`${domain}api/search/?limit=1000`)
+  let attrs = await axios.get(`${domain}api/search/?limit=1000`)
     .then(resp => resp.data);
 
+  attrs = attrs.sort((a, b) => levels.indexOf(a.hierarchy) - levels.indexOf(b.hierarchy));
+
   for (let i = 0; i < attrs.length; i++) {
-    const {id} = attrs[i];
+    const {id, name} = attrs[i];
+    console.log("");
+    console.log(name);
     const url = `${domain}profile/${id}`;
     console.log(url);
     const resp = await axios.get(url);

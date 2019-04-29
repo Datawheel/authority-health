8ac13b6a50a050147172fc621ff7e3b080c7f137
James Ferrell <hello@jamesferrell.me>
2019-04-29 16:51:57 -0400

    adds select menu SubNav for small screens

--- app/components/Subnav.jsx
+++ app/components/Subnav.jsx
@@ -1,14 +1,38 @@
 import React, {Component} from "react";
+import PropTypes from "prop-types";
 import {Icon} from "@blueprintjs/core";
 import {AnchorLink} from "@datawheel/canon-core";
 import {PROFILE_SECTIONS} from "utils/consts.js";
 import "./Subnav.css";
 
-export default class Subnav extends Component {
+class Subnav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentSection: "home"
+    };
+  }
+
+  selectSection(e) {
+    const {router} = this.context;
+    const hash = e.target.value;
+    router.push({
+      ...router.location,
+      state: "HASH",
+      hash: `#${hash}`
+    });
+    this.setState({currentSection: hash});
+
+    // this.context.router.replace(`/profile/16000US2622000#${e.target.value}`);
+  }
+
   render() {
+    const {currentSection} = this.state;
+
     return (
       <nav className="subnav">
-        <ul className="subnav-list u-list-reset">
+        {/* list of icons (big screens) */}
+        <ul className="subnav-list u-list-reset u-hide-below-md">
           {PROFILE_SECTIONS.map(section =>
             <li className="subnav-item" key={`${section.link}-anchor-link`}>
               <AnchorLink to={section.link} className="subnav-link link title">
@@ -18,7 +42,28 @@ export default class Subnav extends Component {
             </li>
           )}
         </ul>
+        {/* select menu (small screens) */}
+        <label className="subnav-select-label font-md">
+          <span className="subnav-select-text">Scroll to section </span>
+          <select
+            className="subnav-select u-hide-above-md"
+            onChange={e => this.selectSection(e)}
+            value={currentSection}
+          >
+            {PROFILE_SECTIONS.map(section =>
+              <option key={`${section.link}-option-link`} value={section.link}>
+                {section.title}
+              </option>
+            )}
+          </select>
+        </label>
       </nav>
     );
   }
 }
+
+Subnav.contextTypes = {
+  router: PropTypes.object
+};
+
+export default Subnav;

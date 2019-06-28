ac2777f087e548efca12297952f8fed7a3074293
Rashmi <baheti.rash123@gmail.com>
2019-06-28 09:37:58 -0700

    corrects adult food insecurity calculation

--- app/pages/Profile/sections/foodAccess/FoodInsecurity.jsx
+++ app/pages/Profile/sections/foodAccess/FoodInsecurity.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
+import {formatAbbreviate} from "d3plus-format";
 import {fetchData, SectionColumns, SectionTitle} from "@datawheel/canon-core";
 
 import Disclaimer from "components/Disclaimer";
@@ -25,9 +26,18 @@ class FoodInsecurity extends SectionColumns {
     const {insecurityRate} = this.props;
     const isInsecurityRateDataAvailableForCurrentGeography = insecurityRate.source[0].substitutions.length === 0;
     const childInsecurity = insecurityRate.data[0];
+
+    // Find adult insecurity with the help of following calculations:
+    // subtract child insecure population from overall insecure population to get adult insecure population (280,290)
+    // subtract child total population from overall total population to get adult total population (1,340,199)
+    // adult insecure rate = adult insecure population / adult total population * 100 (280,290 /1,340,199 * 100 = 20.91%)
+    const adultInsecurePopulation = insecurityRate.data[1]["Estimated Food Insecure Population"] - insecurityRate.data[0]["Estimated Food Insecure Population"];
+    const adultTotalPopulation = insecurityRate.data[1].Population - insecurityRate.data[0].Population;
+
+    const adultInsecurityRate = formatAbbreviate(adultInsecurePopulation / adultTotalPopulation * 100);
     const adultInsecurity = insecurityRate.data[1];
+
     const location = insecurityRate.data[0].Geography;
-    const adultInsecurityRate = adultInsecurity["Food Insecurity Rate"] - childInsecurity["Food Insecurity Rate"];
 
     return (
       <div className="section-title-stat-inner">
@@ -64,7 +74,7 @@ FoodInsecurity.defaultProps = {
 };
 
 FoodInsecurity.need = [
-  fetchData("insecurityRate", "/api/data?measures=Food Insecurity Rate&drilldowns=Category&Geography=<id>&Year=latest")
+  fetchData("insecurityRate", "/api/data?measures=Food Insecurity Rate,Population,Estimated Food Insecure Population&drilldowns=Category&Geography=<id>&Year=latest")
 ];
 
 const mapStateToProps = state => ({

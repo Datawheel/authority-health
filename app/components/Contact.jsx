70dcd2448e252a701077447d547463b1d0d33354
Dave Landry <landry.dave@gmail.com>
2020-03-31 16:10:47 -0400

    adds new TIC community resources mapping

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,26 +1,13 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import contacts from "utils/contacts";
-import {nest} from "d3-collection";
 import "./Contact.css";
 
-import {Button, Dialog, Icon} from "@blueprintjs/core";
+import {Button, Dialog} from "@blueprintjs/core";
 import {Tooltip2} from "@blueprintjs/labs";
 
 const title = "Community Resources";
 const icon = "geosearch";
-
-const phoneParse = p => {
-  const countryCode = p.indexOf("1") === 0 ? "1" : false;
-  if (countryCode) p = p.slice(1);
-  const split = p.match(/([A-z0-9]{10})[x]*([0-9]*)\s*\(*([^)]*)\)*/);
-  let number = split[1];
-  const extension = split[2];
-  const subtitle = split[3];
-  const h = p.toLowerCase().includes("wellnow") ? 7 : 6;
-  number = `${countryCode ? "1-" : ""}${number.slice(0, 3)}-${number.slice(3, h)}-${number.slice(h)}`;
-  return {extension, number, subtitle};
-};
 
 class Contact extends Component {
 
@@ -35,75 +22,37 @@ class Contact extends Component {
 
   render() {
 
-    const {locations, slug} = this.props;
+    const {meta, stats, slug} = this.props;
     const {open} = this.state;
     const toggle = this.toggleDialog.bind(this);
 
-    const data = contacts.filter(d => d.slug === slug && (!d.location || locations.includes(d.location)));
+    const data = contacts.find(d => d.slug === slug);
 
-    if (!data.length) return null;
-
-    const agencies = nest()
-      .key(d => d.agency)
-      .entries(data);
+    if (!data) return null;
+    const topic = slug
+      .replace(/\-/g, " ")
+      .replace(" by demographic", "")
+      .replace(" prevalence", "")
+      .replace("occurance by ", "")
+      .replace("cancer site", "cancer")
+      .replace(" demographics", "")
+      .replace("health center", "health centers")
+      .replace("homeless", "homelessness")
+      .replace("disability status", "disabilities");
+    const tooltip = `Community resources for ${topic} can be found here.`;
+    let city = "Detroit";
+    if (meta.level === "place") city = meta.name;
+    if (meta.level === "tract") city = stats.tractToPlace[meta.id] || city;
+    if (meta.level === "zip") city = stats.zipToPlace[meta.id] || city;
 
     return (
       <div className="contact section-popover-button">
-        <Tooltip2 className="contact-container" tooltipClassName="contact-tooltip" content={ `Community resources that relate to ${slug.replace(/\-/g, " ")} can be found here.` }>
+        <Tooltip2 className="contact-container" tooltipClassName="contact-tooltip" content={ tooltip }>
           <Button className="contact-button pt-minimal font-xxs" text={title} iconName={icon} onClick={toggle} />
         </Tooltip2>
         <Dialog className="contact-dialog" title={title} iconName={icon} isOpen={open} onClose={toggle}>
           <div className="pt-dialog-body">
-            <ul className="agency-list">
-              { agencies.map(group => {
-                const agency = group.key;
-                const contacts = group.values;
-                return <li key={agency}>
-                  <div className="contact-agency title font-sm">{ agency }</div>
-                  <ul className="contact-list">
-                    { contacts.map((d, i) => {
-                      const {address1, address2, contact, email, subAgency, title} = d;
-
-                      let phone = d.phone;
-                      if (!(phone instanceof Array)) phone = [];
-                      phone = phone.map(phoneParse);
-
-                      let fax = d.fax;
-                      if (!(fax instanceof Array)) fax = [];
-                      fax = fax.map(phoneParse);
-
-                      return <li className="contact font-sm" key={i}>
-                        { contact && <div className="contact-name"><b>{ contact }</b></div> }
-                        { title && <div className="contact-title font-xs"><i>{ title }</i></div> }
-                        { subAgency && <div className="contact-subAgency font-xs"><i dangerouslySetInnerHTML={{__html: subAgency}} /></div> }
-                        { address1 && address2 && <div className="contact-address font-xs has-icon">
-                          <Icon iconName="office" />
-                          <span>{ address1 }<br />{ address2 }</span>
-                        </div> }
-                        { email && <div className="contact-email font-xs has-icon">
-                          <Icon iconName="desktop" />
-                          <a href={`mailto:${email}`} target="_blank" rel="noopener noreferrer">{ email }</a>
-                        </div> }
-                        { phone.map((p, i) => {
-                          const {number, extension, subtitle} = p;
-                          return <div key={i} className="contact-phone font-xs has-icon">
-                            <Icon iconName="phone" />
-                            <span>{ number }{ extension ? ` x ${extension}` : null }{ subtitle ? ` (${subtitle})` : null }</span>
-                          </div>;
-                        }) }
-                        { fax.map((p, i) => {
-                          const {number, extension, subtitle} = p;
-                          return <div key={i} className="contact-fax font-xs has-icon">
-                            <Icon iconName="document" />
-                            <span>{ number }{ extension ? ` x ${extension}` : null }{ subtitle ? ` (${subtitle} fax)` : " (fax)" }</span>
-                          </div>;
-                        }) }
-                      </li>;
-                    }) }
-                  </ul>
-                </li>;
-              }) }
-            </ul>
+            <iframe className="tic-iframe" src={data.embed.replace("Detroit", city)} />
           </div>
         </Dialog>
       </div>
@@ -112,5 +61,6 @@ class Contact extends Component {
 }
 
 export default connect(state => ({
-  locations: state.data.topStats.locations
+  meta: state.data.meta,
+  stats: state.data.topStats
 }))(Contact);
